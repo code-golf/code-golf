495b433f502fa3fb73ca700d922dbbb72a6ff696
James Raspass <jraspass@gmail.com>
2017-05-17 08:26:21 +0100

    print leaderboards on the homepage

--- db.go
+++ db.go
@@ -2,19 +2,24 @@ package main
 
 import (
 	"database/sql"
+	"net/http"
 	"os"
+	"strconv"
 
 	_ "github.com/lib/pq"
 )
 
-var dbDSN = os.Getenv("DB_DSN")
+var db *sql.DB
 
-func addSolution(userID int, lang, code string ) {
-	db, err := sql.Open("postgres", dbDSN)
-	if err != nil {
+func init() {
+	var err error
+
+	if db, err = sql.Open("postgres", os.Getenv("DB_DSN")); err != nil {
 		panic(err)
 	}
+}
 
+func addSolution(userID int, lang, code string) {
 	if _, err := db.Exec(
 		`INSERT INTO solutions(user_id, lang, code) VALUES($1, $2, $3)
 		 ON CONFLICT ON CONSTRAINT solutions_pkey DO UPDATE SET code = $3`,
@@ -25,13 +30,53 @@ func addSolution(userID int, lang, code string ) {
 }
 
 func addUser(id int, login string) {
-	if db, err := sql.Open("postgres", dbDSN); err != nil {
-		panic(err)
-	} else if _, err := db.Exec(
+	if _, err := db.Exec(
 		`INSERT INTO users VALUES($1, $2)
 		 ON CONFLICT(id) DO UPDATE SET login = $2`,
 		id, login,
 	); err != nil {
+		panic(err)
+	}
+}
+
+func printLeaderboards(w http.ResponseWriter) {
+	rows, err := db.Query(
+		`SELECT login, lang, length(code)
+		   FROM solutions
+		   JOIN users on user_id = id`,
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	w.Write([]byte("<table>"))
+
+	i := 0
+
+	for rows.Next() {
+		var login, lang string
+		var length int
+
+		if err := rows.Scan(&login, &lang, &length); err != nil {
+			panic(err)
+		}
+
+		i++
+
+		w.Write([]byte(
+			"<tr><td>" + strconv.Itoa(i)
+			"<td>" + strconv.Itoa(length) +
+			"<td>" + lang +
+			"<td>" + login,
+		))
+	}
+
+	w.Write([]byte("</table>"))
+
+	if err := rows.Err(); err != nil {
 		panic(err)
 	}
 }
