afff853f0f01f679486ae4df0c7494efa949c35f
James Raspass <jraspass@gmail.com>
2017-07-22 21:12:59 +0100

    Add a bit more preamble

--- preambles.go
+++ preambles.go
@@ -8,15 +8,19 @@ var intros = map[string]string{
 }
 
 var preambles = map[string]string{
-	"99-bottles-of-beer": "",
+	"99-bottles-of-beer": `<h1>99 Bottles of Beer</h1>
+
+    <p>Print the lyrics to the song 99 Bottles of Beer.</p>`,
 	"fizz-buzz": `<h1>Fizz Buzz</h1>
 
-    <p>Write a program that prints the numbers from <b>1</b> to <b>100</b> (inclusive), each on their own line.</p>
+    <p>Print the numbers from <b>1</b> to <b>100</b> (inclusive), each on their own line.</p>
 
     <p>If, however, the number is a multiple of <b>three</b> then print <b>Fizz</b> instead, and if the number is a multiple of <b>five</b> then print <b>Buzz</b>.</p>
 
     <p>For numbers which are multiples of <b>both three and five</b> then print <b>FizzBuzz</b>.</p>`,
-	"pascals-triangle": "",
+	"pascals-triangle": `<h1>Pascal's Triangle</h1>
+
+    <p>Prints the first <b>20 rows</b> of Pascal's triangle.</p>`,
 	"π": `<h1>π</h1>
 
     <p>Print π (Pi) to the first 1,000 decimal places.</p>`,
