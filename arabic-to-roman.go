5988e8ed4893c8fc44371a4d121c201b90c0c6c0
James Raspass <jraspass@gmail.com>
2017-07-27 21:24:05 +0100

    Fix arabic to roman

--- arabic-to-roman.go
+++ arabic-to-roman.go
@@ -3,7 +3,8 @@ package main
 var roman0 = []string{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}
 var roman1 = []string{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"}
 var roman2 = []string{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}
+var roman3 = []string{"", "M", "MM", "MMM"}
 
 func arabicToRoman(n int) string {
-	return roman2[n%1000/100] + roman1[n%100/10] + roman0[n%10]
+	return roman3[n%10000/1000] + roman2[n%1000/100] + roman1[n%100/10] + roman0[n%10]
 }
