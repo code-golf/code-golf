7475f08f95093524450fa0447b871717d5704191
James Raspass <jraspass@gmail.com>
2017-09-16 14:57:43 +0100

    Add the fibonacci hole

--- answers.go
+++ answers.go
@@ -300,6 +300,37 @@ Take one down and pass it around, no more bottles of beer on the wall.
 
 No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.`,
+	"fibonacci": `0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+144
+233
+377
+610
+987
+1597
+2584
+4181
+6765
+10946
+17711
+28657
+46368
+75025
+121393
+196418
+317811
+514229
+832040`,
 	"fizz-buzz": `1
 2
 Fizz
