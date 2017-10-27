2021b1bf8d1ef630773fbdee78ebeb218b5aea93
James Raspass <jraspass@gmail.com>
2017-10-27 22:09:21 +0100

    Flesh out the user page a bit more

--- routes/user.go
+++ routes/user.go
@@ -8,16 +8,68 @@ import (
 )
 
 func user(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	var one int
+	user := ps[0].Value
+
+	var html []byte
 
 	switch err := db.QueryRow(
-		"SELECT 1 FROM users WHERE login = $1", ps[0].Value,
-	).Scan(&one); err {
+		`WITH leaderboard AS (
+		  SELECT DISTINCT ON (hole, user_id)
+		         hole,
+		         submitted,
+		         LENGTH(code) strokes,
+		         user_id
+		    FROM solutions
+		ORDER BY hole, user_id, LENGTH(code), submitted
+		), scored_leaderboard AS (
+		  SELECT hole,
+		         ROUND(
+		             (
+		                 (SELECT COUNT(distinct user_id) FROM solutions WHERE hole = l.hole)
+		                 -
+		                 RANK() OVER (PARTITION BY hole ORDER BY strokes)
+		                 +
+		                 1
+		             )
+		             *
+		             (
+		                 100.0
+		                 /
+		                 (SELECT COUNT(distinct user_id) FROM solutions WHERE hole = l.hole)
+		             )
+		         ) score,
+		         strokes,
+		         submitted,
+		         user_id
+		    FROM leaderboard l
+		), summed_leaderboard AS (
+		  SELECT user_id,
+		         SUM(score),
+		         COUNT(*),
+		         MAX(submitted)
+		    FROM scored_leaderboard
+		GROUP BY user_id
+		) SELECT CONCAT(
+		             login,
+		             '?s=100"><h1>',
+		             login,
+		             '</h1><table><tr><td>',
+		             TO_CHAR(sum, 'FM9,999'),
+		             '<td>points<tr><td>',
+		             count,
+		             '<td>holes</table><hr>'
+		         )
+		    FROM summed_leaderboard
+		    JOIN users ON id = user_id
+		   WHERE login = $1`,
+		user,
+	).Scan(&html); err {
 	case sql.ErrNoRows:
 		print404(w, r)
 	case nil:
 		printHeader(w, r, 200)
-		w.Write([]byte("<article><h1>" + ps[0].Value + "</h1>"))
+		w.Write([]byte("<link rel=stylesheet href=" + userCssPath + `><article><img src="//avatars.githubusercontent.com/`))
+		w.Write(html)
 	default:
 		panic(err)
 	}
