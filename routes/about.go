1618397148ebfd9bf7d4e9af2299c8ed4bdb9c6b
James Raspass <jraspass@gmail.com>
2017-11-25 12:10:51 +0000

    Tweak /about layout slightly

--- routes/about.go
+++ routes/about.go
@@ -9,7 +9,7 @@ import (
 func about(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	printHeader(w, r, 200)
 
-	const html = "<article><h1>About</h1>" +
+	const html = "<article id=about><h1>About</h1>" +
 		"<p>Code Golf is written in <a href=//golang.org>Go</a> " +
 		"and is <a href=//github.com/JRaspass/code-golf>open source</a>, " +
 		"patches welcome!</p><h2>Versions</h2><table>" + versionTable
