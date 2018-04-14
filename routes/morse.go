25d4b671bbb56dd64680fa9811359309aae1ca44
James Raspass <jraspass@gmail.com>
2018-04-14 14:04:45 +0100

    Randomise the Morse holes more
    This prevents people hardcoding the expected output.
    
    Fixes #22

--- routes/morse.go
+++ routes/morse.go
@@ -1,7 +1,9 @@
 package routes
 
-// The 123 quick brown foxes jump over the 456 lazy dogs
-import "math/rand"
+import (
+	"math/rand"
+	"strings"
+)
 
 var morseMap = map[rune]string{
 	'A': "▄ ▄▄▄",
@@ -44,7 +46,7 @@ var morseMap = map[rune]string{
 }
 
 func morse(reverse bool) (args []string, out string) {
-	digits := []byte{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
+	digits := []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
 
 	// Shuffle the digits.
 	for i := range digits {
@@ -52,25 +54,24 @@ func morse(reverse bool) (args []string, out string) {
 		digits[i], digits[j] = digits[j], digits[i]
 	}
 
-	// It would look weird if the numbers started with zero.
-	if digits[0] == '0' || digits[5] == '0' {
-		digits[0], digits[1] = digits[1], digits[0]
-		digits[5], digits[6] = digits[6], digits[5]
+	words := []string{
+		"BUD",
+		"FOR",
+		"JIGS",
+		"NYMPH",
+		"QUICK",
+		"VEX",
+		"WALTZ",
+		strings.Join(digits, ""),
 	}
 
-	args = []string{"THE "}
-
-	for _, digit := range digits[:5] {
-		args[0] += string(digit)
+	// Shuffle the words.
+	for i := range words {
+		j := rand.Intn(i + 1)
+		words[i], words[j] = words[j], words[i]
 	}
 
-	args[0] += " QUICK BROWN FOXES JUMP OVER THE "
-
-	for _, digit := range digits[5:] {
-		args[0] += string(digit)
-	}
-
-	args[0] += " LAZY DOGS"
+	args = []string{strings.Join(words, " ")}
 
 	for _, char := range args[0] {
 		out += morseMap[char] + "   "
