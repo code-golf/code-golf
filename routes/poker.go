f4bc8178d11b3924295ee0243ca8371767d21578
Eric Roshan-Eisner <ede@alum.mit.edu>
2018-07-27 14:14:04 -0700

    Generate random poker hands (#54)
    Should be able to generate all possible hands, and ensures that ace-wrapping edge cases turn up.
    
    Fixes #53

--- routes/poker.go
+++ routes/poker.go
@@ -1,40 +1,173 @@
 package routes
 
-import "math/rand"
+import (
+	"math/rand"
+	"reflect"
+	"sort"
+)
 
-var handTypes = []string{
-	"High Card",
-	"Pair",
-	"Two Pair",
-	"Three of a Kind",
-	"Straight",
-	"Flush",
-	"Full House",
-	"Four of a Kind",
-	"Straight Flush",
-	"Royal Flush",
+func cardRune(number, suit int) rune {
+	if number > 10 {
+		// Skip over the unused Knight face card
+		number++
+	}
+	return 0x1f0a1 + 16*rune(suit) + rune(number)
+}
+
+func straightCheck(numbers []int) bool {
+	sort.Ints(numbers)
+	if reflect.DeepEqual(numbers, []int{0, 9, 10, 11, 12}) {
+		return true
+	}
+	return numbers[1]-numbers[0] == 1 &&
+		numbers[2]-numbers[1] == 1 &&
+		numbers[3]-numbers[2] == 1 &&
+		numbers[4]-numbers[3] == 1
+}
+
+type Hand struct {
+	Type  string
+	Cards []rune
 }
 
 func poker() (args []string, out string) {
-	hands := []struct {
-		Type  uint8
-		Cards []rune
-	}{
-		// All the royal flushes.
-		{9, []rune{'🂪', '🂫', '🂭', '🂮', '🂡'}},
-		{9, []rune{'🂺', '🂻', '🂽', '🂾', '🂱'}},
-		{9, []rune{'🃊', '🃋', '🃍', '🃎', '🃁'}},
-		{9, []rune{'🃚', '🃛', '🃝', '🃞', '🃑'}},
-		// TODO Needs more random.
-		{8, []rune{'🃘', '🃗', '🃖', '🃕', '🃔'}},
-		{7, []rune{'🂻', '🃋', '🂫', '🃛', '🃇'}},
-		{6, []rune{'🂺', '🃊', '🂪', '🃙', '🃉'}},
-		{5, []rune{'🂤', '🂫', '🂨', '🂢', '🂩'}},
-		{4, []rune{'🃙', '🃈', '🂧', '🃆', '🂵'}},
-		{3, []rune{'🃗', '🃇', '🂧', '🃞', '🃃'}},
-		{2, []rune{'🃔', '🂤', '🃓', '🃃', '🃝'}},
-		{1, []rune{'🂱', '🃁', '🃘', '🂤', '🂷'}},
-		{0, []rune{'🃃', '🃛', '🂨', '🂴', '🂢'}},
+	var hands []Hand
+	const handCount = 3
+
+	// High card
+	for i := 0; i < handCount; i++ {
+		cards := rand.Perm(13)
+		if straightCheck(cards[:5]) {
+			i--
+			continue
+		}
+		suits := rand.Perm(4)
+		hands = append(hands, Hand{"High Card", []rune{
+			cardRune(cards[0], suits[0]),
+			cardRune(cards[1], suits[1]), // Avoid flush
+			cardRune(cards[2], rand.Intn(4)),
+			cardRune(cards[3], rand.Intn(4)),
+			cardRune(cards[4], rand.Intn(4)),
+		}})
+	}
+
+	// Pair
+	for i := 0; i < handCount; i++ {
+		cards := rand.Perm(13)
+		suits := rand.Perm(4)
+		hands = append(hands, Hand{"Pair", []rune{
+			cardRune(cards[0], suits[0]),
+			cardRune(cards[0], suits[1]),
+			cardRune(cards[1], rand.Intn(4)),
+			cardRune(cards[2], rand.Intn(4)),
+			cardRune(cards[3], rand.Intn(4)),
+		}})
+	}
+
+	// Two Pair
+	for i := 0; i < handCount; i++ {
+		cards := rand.Perm(13)
+		suit1 := rand.Perm(4)
+		suit2 := rand.Perm(4)
+		hands = append(hands, Hand{"Two Pair", []rune{
+			cardRune(cards[0], suit1[0]),
+			cardRune(cards[0], suit1[1]),
+			cardRune(cards[1], suit2[0]),
+			cardRune(cards[1], suit2[1]),
+			cardRune(cards[2], rand.Intn(4)),
+		}})
+	}
+
+	// Three of a Kind
+	for i := 0; i < handCount; i++ {
+		cards := rand.Perm(13)
+		suits := rand.Perm(4)
+		hands = append(hands, Hand{"Three of a Kind", []rune{
+			cardRune(cards[0], suits[0]),
+			cardRune(cards[0], suits[1]),
+			cardRune(cards[0], suits[2]),
+			cardRune(cards[1], rand.Intn(4)),
+			cardRune(cards[2], rand.Intn(4)),
+		}})
+	}
+
+	// Four of a Kind
+	for i := 0; i < handCount; i++ {
+		cards := rand.Perm(13)
+		hands = append(hands, Hand{"Four of a Kind", []rune{
+			cardRune(cards[0], 0),
+			cardRune(cards[0], 1),
+			cardRune(cards[0], 2),
+			cardRune(cards[0], 3),
+			cardRune(cards[1], rand.Intn(4)),
+		}})
+	}
+
+	// Full House
+	for i := 0; i < handCount; i++ {
+		cards := rand.Perm(13)
+		suit1 := rand.Perm(4)
+		suit2 := rand.Perm(4)
+		hands = append(hands, Hand{"Full House", []rune{
+			cardRune(cards[0], suit1[0]),
+			cardRune(cards[0], suit1[1]),
+			cardRune(cards[0], suit1[2]),
+			cardRune(cards[1], suit2[0]),
+			cardRune(cards[1], suit2[1]),
+		}})
+	}
+
+	// Flush
+	for i := 0; i < handCount; i++ {
+		cards := rand.Perm(13)
+		if straightCheck(cards[:5]) {
+			i--
+			continue
+		}
+		suit := rand.Intn(4)
+		hands = append(hands, Hand{"Flush", []rune{
+			cardRune(cards[0], suit),
+			cardRune(cards[1], suit),
+			cardRune(cards[2], suit),
+			cardRune(cards[3], suit),
+			cardRune(cards[4], suit),
+		}})
+	}
+
+	// Straight
+	lowCards := rand.Perm(9)
+	lowCards[0] = 0 // Ensure at least one low ace
+	lowCards[1] = 9 // Ensure at least one high ace
+	for _, lowCard := range lowCards[:handCount] {
+		suits := rand.Perm(4)
+		hands = append(hands, Hand{"Straight", []rune{
+			cardRune(lowCard, suits[0]),
+			cardRune(lowCard+1, suits[1]), // Avoid flush
+			cardRune(lowCard+2, rand.Intn(4)),
+			cardRune(lowCard+3, rand.Intn(4)),
+			cardRune((lowCard+4)%13, rand.Intn(4)),
+		}})
+	}
+
+	// Straight Flush
+	lowCards = rand.Perm(9)
+	lowCards[0] = 0 // Ensure at least one low ace
+	for _, lowCard := range lowCards[:handCount] {
+		suit := rand.Intn(4)
+		var hand []rune
+		for card := lowCard; card < lowCard+5; card++ {
+			hand = append(hand, cardRune(card%13, suit))
+		}
+		hands = append(hands, Hand{"Straight Flush", hand})
+	}
+
+	// Royal Flush
+	for suit := 0; suit < 4; suit++ {
+		var hand []rune
+		for card := 9; card < 14; card++ {
+			hand = append(hand, cardRune(card%13, suit))
+		}
+		hands = append(hands, Hand{"Royal Flush", hand})
 	}
 
 	// Shuffle the hands.
@@ -52,7 +185,7 @@ func poker() (args []string, out string) {
 
 		args = append(args, string(hand.Cards))
 
-		out += handTypes[hand.Type] + "\n"
+		out += hand.Type + "\n"
 	}
 
 	// Drop the trailing newline.
