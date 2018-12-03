2f44c3fd86e4e6419ddfcf64077ff83c385c592e
James Raspass <jraspass@gmail.com>
2018-12-03 00:35:43 +0000

    Increase seven-segment length to reduce brute force

--- routes/seven-segment.go
+++ routes/seven-segment.go
@@ -13,7 +13,7 @@ var segments = [][]string{
 }
 
 func sevenSegment() (input, output string) {
-	digits := []byte{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
+	digits := []byte{'0', '0', '1', '1', '2', '2', '3', '3', '4', '4', '5', '5', '6', '6', '7', '7', '8', '8', '9', '9'}
 
 	// Shuffle the digits
 	for i := range digits {
