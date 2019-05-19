0fc53fd186bb7045270624b95e75dd972ef90cd4
James Raspass <jraspass@gmail.com>
2019-05-19 20:00:14 +0100

    Bump Julia & PHP

--- routes/versions.go
+++ routes/versions.go
@@ -7,11 +7,11 @@ const versionTable = "" +
 	"<tr><th class=haskell>Haskell<td>Glasgow Haskell Compiler 8.4.3<td><a href=//www.haskell.org/ghc/></a>" +
 	"<tr><th class=j>J<td>8.07.07<td><a href=http://jsoftware.com></a>" +
 	"<tr><th class=javascript>JavaScript<td>V8 7.4.51<td><a href=//v8.dev></a>" +
-	"<tr><th class=julia>Julia<td>1.1.0<td><a href=//julialang.org></a>" +
+	"<tr><th class=julia>Julia<td>1.1.1<td><a href=//julialang.org></a>" +
 	"<tr><th class=lisp>Lisp<td>GNU CLISP 2.49.92<td><a href=//clisp.sourceforge.io></a>" +
 	"<tr><th class=lua>Lua<td>5.3.5<td><a href=//www.lua.org></a>" +
 	"<tr><th class=perl>Perl<td>5.28.2<td><a href=//www.perl.org></a>" +
 	"<tr><th class=perl6>Perl 6<td>Rakudo 2019.03 on MoarVM 2019.03 implementing Perl 6.d<td><a href=//perl6.org></a>" +
-	"<tr><th class=php>PHP<td>7.3.4<td><a href=//secure.php.net></a>" +
+	"<tr><th class=php>PHP<td>7.3.5<td><a href=//secure.php.net></a>" +
 	"<tr><th class=python>Python<td>3.7.3<td><a href=//www.python.org></a>" +
 	"<tr><th class=ruby>Ruby<td>2.6.3<td><a href=//www.ruby-lang.org></a>"
