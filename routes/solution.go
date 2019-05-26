f8d74283af6967d61200c87ee18d30cd51cc340e
James Raspass <jraspass@gmail.com>
2019-05-26 01:38:41 +0100

    Bump Nim
    Fixes #86

--- routes/solution.go
+++ routes/solution.go
@@ -228,7 +228,7 @@ func runCode(hole, lang, code string, args []string, userID int) (string, string
 		cmd.Args = []string{"/usr/bin/run-julia", "/tmp/code.jl"}
 	case "nim":
 		cmd.Args = []string{
-			"/usr/bin/nim", "--cc:tcc", "--hint[Processing]:off",
+			"/usr/bin/nim", "--cc:tcc", "--hint.Processing:off",
 			"--nimcache:/tmp", "--verbosity:0", "-o:/tmp/code", "-r", "c", "-",
 		}
 	case "perl6":
