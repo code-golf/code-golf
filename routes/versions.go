255cbd4a1812fb574ed9f8321afc7d82c1861d48
James Raspass <jraspass@gmail.com>
2019-09-29 16:32:29 +0100

    Bump Nim, PHP, Ruby

--- routes/versions.go
+++ routes/versions.go
@@ -10,9 +10,9 @@ const versionTable = "" +
 	"<tr><th class=julia>Julia<td>1.2.0<td><a href=//julialang.org></a>" +
 	"<tr><th class=lisp>Lisp<td>GNU CLISP 2.49.92<td><a href=//clisp.sourceforge.io></a>" +
 	"<tr><th class=lua>Lua<td>5.3.5<td><a href=//www.lua.org></a>" +
-	"<tr><th class=nim>Nim<td>0.20.2<td><a href=//nim-lang.org></a>" +
+	"<tr><th class=nim>Nim<td>1.0.0<td><a href=//nim-lang.org></a>" +
 	"<tr><th class=perl>Perl<td>5.30.0<td><a href=//www.perl.org></a>" +
 	"<tr><th class=perl6>Perl 6<td>Rakudo 2019.07.1 on MoarVM 2019.07.1 implementing Perl 6.d<td><a href=//perl6.org></a>" +
-	"<tr><th class=php>PHP<td>7.3.8<td><a href=//secure.php.net></a>" +
+	"<tr><th class=php>PHP<td>7.3.10<td><a href=//secure.php.net></a>" +
 	"<tr><th class=python>Python<td>3.7.4<td><a href=//www.python.org></a>" +
-	"<tr><th class=ruby>Ruby<td>2.6.3<td><a href=//www.ruby-lang.org></a>"
+	"<tr><th class=ruby>Ruby<td>2.6.4<td><a href=//www.ruby-lang.org></a>"
