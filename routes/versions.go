83b66728b73fa82482cb08b6c2454bd28f30d1a1
James Raspass <jraspass@gmail.com>
2019-11-28 16:15:41 +0000

    Bump Julia, Nim, & PHP

--- routes/versions.go
+++ routes/versions.go
@@ -7,12 +7,12 @@ const versionTable = "" +
 	"<tr><th class=haskell>Haskell<td>Glasgow Haskell Compiler 8.4.3<td class=wide><a href=//www.haskell.org/ghc/>website</a>" +
 	"<tr><th class=j>J<td>8.07.07<td class=wide><a href=http://jsoftware.com>website</a>" +
 	"<tr><th class=javascript>JavaScript<td>V8 7.4.51<td class=wide><a href=//v8.dev>website</a>" +
-	"<tr><th class=julia>Julia<td>1.2.0<td class=wide><a href=//julialang.org>website</a>" +
+	"<tr><th class=julia>Julia<td>1.3.0<td class=wide><a href=//julialang.org>website</a>" +
 	"<tr><th class=lisp>Lisp<td>GNU CLISP 2.49.92<td class=wide><a href=//clisp.sourceforge.io>website</a>" +
 	"<tr><th class=lua>Lua<td>5.3.5<td class=wide><a href=//www.lua.org>website</a>" +
-	"<tr><th class=nim>Nim<td>1.0.2<td class=wide><a href=//nim-lang.org>website</a>" +
+	"<tr><th class=nim>Nim<td>1.0.4<td class=wide><a href=//nim-lang.org>website</a>" +
 	"<tr><th class=perl>Perl<td>5.30.1<td class=wide><a href=//www.perl.org>website</a>" +
 	"<tr><th class=perl6>Perl 6<td>Rakudo 2019.07.1 on MoarVM 2019.07.1 implementing Perl 6.d<td class=wide><a href=//perl6.org>website</a>" +
-	"<tr><th class=php>PHP<td>7.3.11<td class=wide><a href=//secure.php.net>website</a>" +
+	"<tr><th class=php>PHP<td>7.4.0<td class=wide><a href=//secure.php.net>website</a>" +
 	"<tr><th class=python>Python<td>3.8.0<td class=wide><a href=//www.python.org>website</a>" +
 	"<tr><th class=ruby>Ruby<td>2.6.5<td class=wide><a href=//www.ruby-lang.org>website</a>"
