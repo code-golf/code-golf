26d351537c33d68cd429c10fc9b895bac8f6eaed
James Raspass <jraspass@gmail.com>
2019-12-19 06:23:48 +0000

    Bump dev DB to match prod
    I wonder what shiny 12 features we can use now, generated columns?

--- db/0.schema.sql
+++ db/0.schema.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 11.5
--- Dumped by pg_dump version 11.5
+-- Dumped from database version 12.1
+-- Dumped by pg_dump version 12.1
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -109,7 +109,7 @@ CREATE TYPE public.trophy AS ENUM (
 
 SET default_tablespace = '';
 
-SET default_with_oids = false;
+SET default_table_access_method = heap;
 
 --
 -- Name: ideas; Type: TABLE; Schema: public; Owner: -
