2acd6070a5c4e204122be9d51eec95b6ab4e9782
James Raspass <jraspass@gmail.com>
2020-01-26 14:13:24 +0000

    Bump Haskell, Julia & PHP
    Also bump the base alpine image of the langs.

--- routes/versions.go
+++ routes/versions.go
@@ -4,14 +4,15 @@ const versionTable = "" +
 	"<tr><th class=bash>Bash<td>5.0.0<td class=wide><a href=//www.gnu.org/software/bash/>website</a>" +
 	"<tr><th class=brainfuck>Brainfuck<td>1.0.0<td class=wide><a href=//github.com/code-golf/code-golf/tree/master/langs/brainfuck/>website</a>" +
 	"<tr><th class=c>C<td>Tiny C Compiler 0.9.27<td class=wide><a href=//bellard.org/tcc/>website</a>" +
-	"<tr><th class=haskell>Haskell<td>Glasgow Haskell Compiler 8.4.3<td class=wide><a href=//www.haskell.org/ghc/>website</a>" +
+	"<tr><th class=haskell>Haskell<td>Glasgow Haskell Compiler 8.6.5<td class=wide><a href=//www.haskell.org/ghc/>website</a>" +
 	"<tr><th class=j>J<td>8.07.07<td class=wide><a href=http://jsoftware.com>website</a>" +
 	"<tr><th class=javascript>JavaScript<td>V8 7.4.51<td class=wide><a href=//v8.dev>website</a>" +
+	"<tr><th class=julia>Julia<td>1.3.1<td class=wide><a href=//julialang.org>website</a>" +
 	"<tr><th class=lisp>Lisp<td>GNU CLISP 2.49.92<td class=wide><a href=//clisp.sourceforge.io>website</a>" +
 	"<tr><th class=lua>Lua<td>5.3.5<td class=wide><a href=//www.lua.org>website</a>" +
 	"<tr><th class=nim>Nim<td>1.0.4<td class=wide><a href=//nim-lang.org>website</a>" +
 	"<tr><th class=perl>Perl<td>5.30.1<td class=wide><a href=//www.perl.org>website</a>" +
-	"<tr><th class=php>PHP<td>7.4.1<td class=wide><a href=//secure.php.net>website</a>" +
+	"<tr><th class=php>PHP<td>7.4.2<td class=wide><a href=//secure.php.net>website</a>" +
 	"<tr><th class=python>Python<td>3.8.1<td class=wide><a href=//www.python.org>website</a>" +
 	"<tr><th class=raku>Raku<td>Rakudo 2019.11 on MoarVM 2019.11 implementing Perl 6.d<td class=wide><a href=//raku.org>website</a>" +
 	"<tr><th class=ruby>Ruby<td>2.7.0<td class=wide><a href=//www.ruby-lang.org>website</a>" +
