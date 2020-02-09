1b5385735d38e62de7c6f43f1fa1a3bf2d677b36
Brian Collins <bri.l.collins@gmail.com>
2020-02-09 15:08:30 -0500

    Recent Solutions: Show 1st-3rd place medals for those ranks.
    Also show a decreasing chart emoji, if stroke count was lower than previous at that rank at the time of submission.

--- routes/recent.go
+++ routes/recent.go
@@ -8,9 +8,42 @@ import (
 // Recent serves GET /recent
 func Recent(w http.ResponseWriter, r *http.Request) {
 	rows, err := db(r).Query(
-		` SELECT hole, lang, login, LENGTH(code), submitted
-		    FROM solutions JOIN users ON id = user_id
-		ORDER BY submitted DESC LIMIT 100`,
+		`WITH solution_lengths AS (
+        SELECT hole,
+               lang,
+               login,
+               LENGTH(code) strokes,
+               submitted
+          FROM solutions
+          JOIN users on user_id = id
+         WHERE NOT failing
+     )  SELECT t1.hole,
+               t1.lang,
+               login,
+               t1.strokes,
+               rank,
+               COUNT(*) - 1 tie_count,
+               t1.submitted
+          FROM solution_lengths AS t1
+    INNER JOIN (
+        SELECT RANK() OVER (PARTITION BY hole, lang ORDER BY strokes) rank,
+               hole,
+               lang,
+               strokes,
+               submitted
+          FROM solution_lengths
+    ) AS t2
+            ON t1.hole = t2.hole
+           AND t1.lang = t2.lang
+           AND t1.strokes = t2.strokes
+           AND t2.submitted <= t1.submitted
+      GROUP BY t1.hole,
+               t1.lang,
+               login,
+               t1.strokes,
+               t1.submitted,
+               rank
+      ORDER BY t1.submitted DESC LIMIT 100`,
 	)
 	if err != nil {
 		panic(err)
@@ -23,6 +56,8 @@ func Recent(w http.ResponseWriter, r *http.Request) {
 		Lang      Lang
 		Login     string
 		Strokes   int
+		Rank      int
+		TieCount  int
 		Submitted time.Time
 	}
 
@@ -37,6 +72,8 @@ func Recent(w http.ResponseWriter, r *http.Request) {
 			&langID,
 			&r.Login,
 			&r.Strokes,
+			&r.Rank,
+			&r.TieCount,
 			&r.Submitted,
 		); err != nil {
 			panic(err)
