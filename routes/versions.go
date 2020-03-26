d835d7ef8efb060c40edf86234ad079ff8b35595
James Raspass <jraspass@gmail.com>
2020-03-26 12:06:13 +0000

    Bump Julia & Swift

--- routes/versions.go
+++ routes/versions.go
@@ -7,7 +7,7 @@ const versionTable = "" +
 	"<tr><th class=haskell>Haskell<td>Glasgow Haskell Compiler 8.6.5<td class=wide><a href=//www.haskell.org/ghc/>website</a>" +
 	"<tr><th class=j>J<td>8.07.07<td class=wide><a href=http://jsoftware.com>website</a>" +
 	"<tr><th class=javascript>JavaScript<td>V8 7.4.51<td class=wide><a href=//v8.dev>website</a>" +
-	"<tr><th class=julia>Julia<td>1.3.1<td class=wide><a href=//julialang.org>website</a>" +
+	"<tr><th class=julia>Julia<td>1.4.0<td class=wide><a href=//julialang.org>website</a>" +
 	"<tr><th class=lisp>Lisp<td>GNU CLISP 2.49.92<td class=wide><a href=//clisp.sourceforge.io>website</a>" +
 	"<tr><th class=lua>Lua<td>5.4.0<td class=wide><a href=//www.lua.org>website</a>" +
 	"<tr><th class=nim>Nim<td>1.0.6<td class=wide><a href=//nim-lang.org>website</a>" +
@@ -17,4 +17,4 @@ const versionTable = "" +
 	"<tr><th class=raku>Raku<td>Rakudo 2020.02.1 on MoarVM 2020.02.1 implementing Raku 6.d<td class=wide><a href=//raku.org>website</a>" +
 	"<tr><th class=ruby>Ruby<td>2.7.0<td class=wide><a href=//www.ruby-lang.org>website</a>" +
 	"<tr><th class=rust>Rust<td>1.42.0<td class=wide><a href=//www.rust-lang.org>website</a>" +
-	"<tr><th class=swift>Swift<td>5.1.5<td class=wide><a href=//swift.org>website</a>"
+	"<tr><th class=swift>Swift<td>5.2<td class=wide><a href=//swift.org>website</a>"
