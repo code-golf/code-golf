8b8f0e7d6478ed12d55ad71df1ddf38067615fd4
James Raspass <jraspass@gmail.com>
2020-04-14 11:34:18 +0100

    Re-wrap schema.sql

--- db/0.schema.sql
+++ db/0.schema.sql
@@ -4,13 +4,13 @@ CREATE TYPE hole AS ENUM (
     '12-days-of-christmas', '99-bottles-of-beer', 'abundant-numbers',
     'arabic-to-roman', 'brainfuck', 'christmas-trees', 'cubes', 'diamonds',
     'divisors', 'emirp-numbers', 'evil-numbers', 'fibonacci', 'fizz-buzz',
-    'happy-numbers', 'leap-years', 'lucky-tickets', 'morse-decoder', 'morse-encoder',
-    'niven-numbers', 'odious-numbers', 'ordinal-numbers', 'pangram-grep',
-    'pascals-triangle', 'pernicious-numbers', 'poker', 'prime-numbers',
-    'quine', 'rock-paper-scissors-spock-lizard', 'roman-to-arabic',
-    'rule-110', 'seven-segment', 'sierpiński-triangle', 'spelling-numbers',
-    'sudoku', 'ten-pin-bowling', 'united-states', 'λ', 'π', 'τ', 'φ', '√2',
-    '𝑒'
+    'happy-numbers', 'leap-years', 'lucky-tickets', 'morse-decoder',
+    'morse-encoder', 'niven-numbers', 'odious-numbers', 'ordinal-numbers',
+    'pangram-grep', 'pascals-triangle', 'pernicious-numbers', 'poker',
+    'prime-numbers', 'quine', 'rock-paper-scissors-spock-lizard',
+    'roman-to-arabic', 'rule-110', 'seven-segment', 'sierpiński-triangle',
+    'spelling-numbers', 'sudoku', 'ten-pin-bowling', 'united-states', 'λ',
+    'π', 'τ', 'φ', '√2', '𝑒'
 );
 
 CREATE TYPE lang AS ENUM (
