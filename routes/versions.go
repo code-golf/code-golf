3d98e464c2fdb71d3522be9bf34c9e7b009d67a6
James Raspass <jraspass@gmail.com>
2020-04-18 12:18:35 +0100

    Bump Julia, PHP, & Swift

--- routes/versions.go
+++ routes/versions.go
@@ -9,14 +9,14 @@ const versionTable = "" +
 	"<tr><th class=haskell>Haskell<td>Glasgow Haskell Compiler 8.6.5<td class=wide><a href=//www.haskell.org/ghc/>website</a>" +
 	"<tr><th class=j>J<td>8.07.07<td class=wide><a href=//www.jsoftware.com>website</a>" +
 	"<tr><th class=javascript>JavaScript<td>V8 7.4.51<td class=wide><a href=//v8.dev>website</a>" +
-	"<tr><th class=julia>Julia<td>1.4.0<td class=wide><a href=//julialang.org>website</a>" +
+	"<tr><th class=julia>Julia<td>1.4.1<td class=wide><a href=//julialang.org>website</a>" +
 	"<tr><th class=lisp>Lisp<td>GNU CLISP 2.49.92<td class=wide><a href=//clisp.sourceforge.io>website</a>" +
 	"<tr><th class=lua>Lua<td>5.4.0<td class=wide><a href=//www.lua.org>website</a>" +
 	"<tr><th class=nim>Nim<td>1.0.6<td class=wide><a href=//nim-lang.org>website</a>" +
 	"<tr><th class=perl>Perl<td>5.30.2<td class=wide><a href=//www.perl.org>website</a>" +
-	"<tr><th class=php>PHP<td>7.4.4<td class=wide><a href=//secure.php.net>website</a>" +
+	"<tr><th class=php>PHP<td>7.4.5<td class=wide><a href=//secure.php.net>website</a>" +
 	"<tr><th class=python>Python<td>3.8.2<td class=wide><a href=//www.python.org>website</a>" +
 	"<tr><th class=raku>Raku<td>Rakudo 2020.02.1 on MoarVM 2020.02.1 implementing Raku 6.d<td class=wide><a href=//raku.org>website</a>" +
 	"<tr><th class=ruby>Ruby<td>2.7.1<td class=wide><a href=//www.ruby-lang.org>website</a>" +
 	"<tr><th class=rust>Rust<td>1.42.0<td class=wide><a href=//www.rust-lang.org>website</a>" +
-	"<tr><th class=swift>Swift<td>5.2.1<td class=wide><a href=//swift.org>website</a>"
+	"<tr><th class=swift>Swift<td>5.2.2<td class=wide><a href=//swift.org>website</a>"
