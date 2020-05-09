40b079c7aed79cb0beea592935e468b476221bb8
SirBogman <53135437+SirBogman@users.noreply.github.com>
2020-05-09 10:31:39 -0400

    C#, F#: don't use ln in dockerfiles. (#199)

--- hole/hole.go
+++ hole/hole.go
@@ -85,6 +85,8 @@ func Play(ctx context.Context, holeID, langID, code string) (score Scorecard) {
 		cmd.Args = []string{"/usr/bin/bash", "-s", "-"}
 	case "c":
 		cmd.Args = []string{"/usr/bin/tcc", "-run", "-"}
+	case "c-sharp", "f-sharp":
+		cmd.Args = []string{"/compiler/Compiler", "-"}
 	case "haskell", "javascript", "php":
 		cmd.Args = []string{"/usr/bin/" + langID, "--"}
 	case "j":
