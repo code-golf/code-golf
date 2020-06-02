baa7cd3bc99b080e2c20100829087aaa22ebdd5a
James Raspass <jraspass@gmail.com>
2020-06-02 04:04:12 +0100

    Bump Go, Julia, Perl, & Swift

--- routes/versions.go
+++ routes/versions.go
@@ -7,19 +7,19 @@ const versionTable = "" +
 	"<tr><th class=c-sharp>C#<td>C# 8.0 on .NET Core 3.1.3<td class=wide><a href=//docs.microsoft.com/en-us/dotnet/csharp/>website</a>" +
 	"<tr><th class=fortran>Fortran<td>9.3.0<td class=wide><a href=//gcc.gnu.org/fortran/>website</a>" +
 	"<tr><th class=f-sharp>F#<td>F# Compiler 10.8.0.0 for F# 4.7 on .NET Core 3.1.3<td class=wide><a href=//fsharp.org/>website</a>" +
-	"<tr><th class=go>Go<td>1.14.3<td class=wide><a href=//golang.org>website</a>" +
+	"<tr><th class=go>Go<td>1.14.4<td class=wide><a href=//golang.org>website</a>" +
 	"<tr><th class=haskell>Haskell<td>Glasgow Haskell Compiler 8.6.5<td class=wide><a href=//www.haskell.org/ghc/>website</a>" +
 	"<tr><th class=j>J<td>8.07.07<td class=wide><a href=//www.jsoftware.com>website</a>" +
 	"<tr><th class=java>Java<td>14.0.1+7<td class=wide><a href=//openjdk.java.net>website</a>" +
 	"<tr><th class=javascript>JavaScript<td>V8 7.4.51<td class=wide><a href=//v8.dev>website</a>" +
-	"<tr><th class=julia>Julia<td>1.4.1<td class=wide><a href=//julialang.org>website</a>" +
+	"<tr><th class=julia>Julia<td>1.4.2<td class=wide><a href=//julialang.org>website</a>" +
 	"<tr><th class=lisp>Lisp<td>GNU CLISP 2.49.92<td class=wide><a href=//clisp.sourceforge.io>website</a>" +
 	"<tr><th class=lua>Lua<td>5.4.0<td class=wide><a href=//www.lua.org>website</a>" +
 	"<tr><th class=nim>Nim<td>1.2.0<td class=wide><a href=//nim-lang.org>website</a>" +
-	"<tr><th class=perl>Perl<td>5.30.2<td class=wide><a href=//www.perl.org>website</a>" +
+	"<tr><th class=perl>Perl<td>5.30.3<td class=wide><a href=//www.perl.org>website</a>" +
 	"<tr><th class=php>PHP<td>7.4.6<td class=wide><a href=//secure.php.net>website</a>" +
 	"<tr><th class=python>Python<td>3.8.3<td class=wide><a href=//www.python.org>website</a>" +
 	"<tr><th class=raku>Raku<td>Rakudo 2020.05.1 on MoarVM 2020.05 implementing Raku 6.d<td class=wide><a href=//raku.org>website</a>" +
 	"<tr><th class=ruby>Ruby<td>2.7.1<td class=wide><a href=//www.ruby-lang.org>website</a>" +
 	"<tr><th class=rust>Rust<td>1.43.1<td class=wide><a href=//www.rust-lang.org>website</a>" +
-	"<tr><th class=swift>Swift<td>5.2.3<td class=wide><a href=//swift.org>website</a>"
+	"<tr><th class=swift>Swift<td>5.2.4<td class=wide><a href=//swift.org>website</a>"
