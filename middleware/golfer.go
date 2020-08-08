2a4a5b6890d92ef5a88cfd91dc62b6165bb4858f
James Raspass <jraspass@gmail.com>
2020-08-08 11:02:08 +0100

    Remove support for the old __Host-user cookie

--- middleware/golfer.go
+++ middleware/golfer.go
@@ -1,16 +1,9 @@
 package middleware
 
 import (
-	"crypto/hmac"
-	"crypto/sha256"
-	"crypto/subtle"
 	"database/sql"
-	"encoding/base64"
 	"errors"
 	"net/http"
-	"os"
-	"strconv"
-	"strings"
 	"time"
 
 	"github.com/code-golf/code-golf/golfer"
@@ -18,69 +11,9 @@ import (
 	"github.com/gofrs/uuid"
 )
 
-var hmacKey []byte
-
-func init() {
-	var err error
-	if hmacKey, err = base64.RawURLEncoding.DecodeString(os.Getenv("HMAC_KEY")); err != nil {
-		panic(err)
-	}
-}
-
 // GolferHandler adds the golfer to the context if logged in.
 func GolferHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if cookie, err := r.Cookie("__Host-user"); err == nil {
-			if i := strings.LastIndexByte(cookie.Value, ':'); i != -1 {
-				mac := hmac.New(sha256.New, hmacKey)
-				mac.Write([]byte(cookie.Value[:i]))
-
-				if subtle.ConstantTimeCompare(
-					[]byte(cookie.Value[i+1:]),
-					[]byte(base64.RawURLEncoding.EncodeToString(mac.Sum(nil))),
-				) == 1 {
-					var golfer golfer.Golfer
-
-					j := strings.IndexByte(cookie.Value, ':')
-					golfer.ID, _ = strconv.Atoi(cookie.Value[:j])
-					golfer.Name = cookie.Value[j+1 : i]
-
-					r = session.Set(r, "golfer", &golfer)
-
-					// Port them to the new cookie.
-					cookie := http.Cookie{
-						HttpOnly: true,
-						Name:     "__Host-session",
-						Path:     "/",
-						SameSite: http.SameSiteLaxMode,
-						Secure:   true,
-					}
-
-					if err := session.Database(r).QueryRow(
-						`WITH golfer AS (
-						    INSERT INTO users (id, login) VALUES ($1, $2)
-						    ON CONFLICT (id) DO UPDATE SET login = excluded.login
-						      RETURNING id
-						) INSERT INTO sessions (user_id) SELECT * FROM golfer RETURNING id`,
-						golfer.ID, golfer.Name,
-					).Scan(&cookie.Value); err != nil {
-						panic(err)
-					}
-
-					http.SetCookie(w, &cookie)
-
-					http.SetCookie(w, &http.Cookie{
-						HttpOnly: true,
-						MaxAge:   -1,
-						Name:     "__Host-user",
-						Path:     "/",
-						SameSite: http.SameSiteLaxMode,
-						Secure:   true,
-					})
-				}
-			}
-		}
-
 		if cookie, _ := r.Cookie("__Host-session"); cookie != nil {
 			var golfer golfer.Golfer
 
