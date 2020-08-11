c4a0d21c125c7fb47850507975d196162beafc3c
James Raspass <jraspass@gmail.com>
2020-08-11 05:47:16 +0100

    Move J version logic lower

--- langs/j/j.c
+++ langs/j/j.c
@@ -9,9 +9,15 @@ int main (int argc, char *argv[]) {
     if (fp == 0)
         return 1;
 
-    while ((nbytes = fread(buffer, sizeof(char), sizeof(buffer), stdin)) > 0)
-        if (fwrite(buffer, sizeof(char), nbytes, fp) != nbytes)
-            return 2;
+    if (argc > 1 && strcmp(argv[1], "-v") == 0) {
+        argv[1] = "/tmp/code.ijs";
+        fputs("echo JLIB", fp);
+    }
+    else {
+        while ((nbytes = fread(buffer, sizeof(char), sizeof(buffer), stdin)) > 0)
+            if (fwrite(buffer, sizeof(char), nbytes, fp) != nbytes)
+                return 2;
+    }
 
     fclose(fp);
 
