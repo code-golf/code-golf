bcc706a6797c786511159d905f7085edbafc6e23
Jo King <653testing@gmail.com>
2020-10-21 18:24:26 +1100

    Added random test cases to ten pin bowling (#308)
    * added random test cases to ten pin bowling
    
    * made lint happy
    
    * made the compiler happy

--- hole/ten-pin-bowling.go
+++ hole/ten-pin-bowling.go
@@ -2,6 +2,7 @@ package hole
 
 import (
 	"math/rand"
+	"strconv"
 	"strings"
 )
 
@@ -26,41 +27,124 @@ var games = [...]struct {
 	{[]rune("72 9- 81  X 9- 8/  X  X  X 9- "), "162"},
 	{[]rune("81 16 8/ 33  X 7- -7 9- 8- -- "), "83"},
 	{[]rune("9- -2 35  X  X  X  X 62 22 62 "), "143"},
+	{[]rune("9/ 5F 5- F2  X 5- 81  X F7 9/3"), "93"},
+}
+
+// Randomly create splits and fouls to make it more interesting.
+func randReplacements(gFrames []rune) []rune {
+	frames := make([]rune, len(gFrames))
+	copy(frames, gFrames)
+
+	for j, char := range frames {
+		var replacement rune
+
+		switch char {
+		case '0':
+			frames[j] = '-'
+			replacement = 'F'
+		case '-':
+			replacement = 'F'
+		case '5', '6', '7', '8':
+			// Only split on the first ball of the frame.
+			if j%3 == 0 {
+				replacement = '①' - '1' + char
+			} else {
+				continue
+			}
+		default:
+			continue
+		}
+
+		if rand.Intn(2) == 0 {
+			frames[j] = replacement
+		}
+	}
+	return frames
 }
 
 func tenPinBowling() ([]string, string) {
-	args := make([]string, len(games))
-	outs := make([]string, len(games))
+	extraCases := 22
+	args := make([]string, len(games)+extraCases)
+	outs := make([]string, len(games)+extraCases)
 
 	for i, game := range games {
-		frames := make([]rune, len(game.frames))
-		copy(frames, game.frames)
-
-		// Randomly create splits and fouls to make it more interesting.
-		for j, char := range frames {
-			var replacement rune
-
-			switch char {
-			case '-':
-				replacement = 'F'
-			case '5', '6', '7', '8':
-				// Only split on the first ball of the frame.
-				if j%3 == 0 {
-					replacement = '①' - '1' + char
-				} else {
-					continue
-				}
-			default:
-				continue
-			}
-
-			if rand.Intn(2) == 0 {
-				frames[j] = replacement
-			}
-		}
-
+		frames := randReplacements(game.frames)
 		args[i] = string(frames)
 		outs[i] = game.score
+	}
+
+	for i := 0; i < extraCases; i++ {
+		rolls := make([]int, 24)
+
+		// Generate some random rolls
+		for rollNum := 0; rollNum < 23; rollNum++ {
+			roll := 0
+			maxRoll := 10
+			if rollNum%2 == 1 {
+				maxRoll = 10 - rolls[rollNum-1]
+			}
+			// Roll with bias towards strikes/spares and misses
+			roll = rand.Intn(maxRoll+2) - 1
+			if roll > maxRoll {
+				roll = maxRoll
+			} else if roll < 0 {
+				roll = 0
+			}
+			rolls[rollNum] = roll
+		}
+
+		if rolls[18] != 10 {
+			rolls[21] = 0
+		}
+
+		// Now let's score the rolls and format the input
+		arg := ""
+		score := 0
+		for frame := 0; frame < 12; frame++ {
+			// Skip the bonus rolls if necessary
+			if frame > 9 {
+				if rolls[18]+rolls[19] != 10 || (frame == 11 && (rolls[18] != 10 || rolls[20] != 10)) {
+					arg += " "
+					break
+				}
+			} else if frame > 0 {
+				arg += " "
+				// Add the bonus scores if the last frame was a spare or strike
+				if rolls[frame*2-2]+rolls[frame*2-1] == 10 {
+					score += rolls[frame*2]
+
+					// If it was a strike specifically
+					if rolls[frame*2-2] == 10 {
+						score += rolls[frame*2+1]
+						// And if the one before that was also a strike
+						if frame > 1 && rolls[frame*2-4] == 10 {
+							score += rolls[frame*2]
+						}
+					}
+				}
+			}
+			score += rolls[frame*2] + rolls[frame*2+1]
+
+			if rolls[frame*2] == 10 {
+				if frame < 9 {
+					arg += " "
+				}
+				arg += "X"
+			} else {
+				arg += strconv.Itoa(rolls[frame*2])
+				if frame < 10 || (frame == 10 && rolls[18] == 10) {
+					if rolls[frame*2]+rolls[frame*2+1] == 10 {
+						arg += "/"
+					} else {
+						arg += strconv.Itoa(rolls[frame*2+1])
+					}
+				}
+			}
+		}
+		r := []rune(arg)
+		frames := randReplacements(r)
+		args[i+len(games)] = string(frames)
+		outs[i+len(games)] = strconv.Itoa(score)
 	}
 
 	rand.Shuffle(len(args), func(i, j int) {
