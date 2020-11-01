a7bf6edab625629f3c7f97ed03736f8399e40b41
James Raspass <jraspass@gmail.com>
2020-11-01 04:37:39 +0000

    Simplify fish container

--- hole/play.go
+++ hole/play.go
@@ -100,7 +100,7 @@ func Play(ctx context.Context, holeID, langID, code string) (score Scorecard) {
 	case "c-sharp", "f-sharp":
 		cmd.Args = []string{"/compiler/Compiler", "-"}
 	case "fish":
-		cmd.Args = []string{"/usr/bin/python3.9", "/usr/bin/fish", "-c", code, "-i"}
+		cmd.Args = []string{"/usr/bin/fish", "-c", code, "-i"}
 	case "haskell", "php":
 		cmd.Args = []string{"/usr/bin/" + langID, "--"}
 	case "j":
