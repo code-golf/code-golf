ffe16d0f260dcb20d941ae6b3a034ba2fc096032
alessandro <alessandro_melis@rocketmail.com>
2021-03-21 17:20:01 +0000

    exact solution, print only integer part

--- hole/ellipse-perimeters.go
+++ hole/ellipse-perimeters.go
@@ -8,12 +8,18 @@ import (
 )
 
 func perimeter(ai, bi int) (p float64) {
+	var n float64
 	a, b := float64(ai), float64(bi)
 	h := math.Pow(a-b, 2) / math.Pow(a+b, 2)
-	p = math.Pi * (a + b) * (1.0 + (3.0 * h / (10.0 + math.Sqrt(4.0-(3.0*h)))))
+	var bin float64
+	for ni := 0; ni < 100; ni++ {
+		n = float64(ni)
+		bin = math.Gamma(1.5) / (math.Gamma(1.0+n) * math.Gamma(1.5-n))
+		p += math.Pow(bin, 2) * math.Pow(h, n)
+	}
+	p *= math.Pi * (a + b)
 	return
 }
-
 func ellipsePerimeters() (args []string, out string) {
 	var outs []string
 
@@ -21,13 +27,13 @@ func ellipsePerimeters() (args []string, out string) {
 	var a, b int
 	var p float64
 	var ps string
-	for i := 0; i < 50; i++ {
-		a = rand.Intn(50) + 1
-		b = rand.Intn(50) + 1
+	for i := 0; i < 10; i++ {
+		a = rand.Intn(15) + 5
+		b = rand.Intn(5) + 1
 		args = append(args, strconv.Itoa(a)+" "+strconv.Itoa(b))
 
 		p = perimeter(a, b)
-		ps = strconv.FormatFloat(p, 'f', 40, 64)[0:11]
+		ps = strconv.FormatFloat(p, 'f', 40, 64)[0:2]
 		outs = append(outs, ps)
 	}
 
