db4bfbab89e22a149df89e63b3eea401c924c411
James Raspass <jraspass@gmail.com>
2021-04-25 23:39:11 +0100

    Add "Sudoku V2" hole

--- db/a-schema.sql
+++ db/a-schema.sql
@@ -13,8 +13,8 @@ CREATE TYPE hole AS ENUM (
     'pernicious-numbers', 'poker', 'prime-numbers', 'quine', 'recamán',
     'rock-paper-scissors-spock-lizard', 'roman-to-arabic', 'rule-110',
     'seven-segment', 'sierpiński-triangle', 'spelling-numbers', 'sudoku',
-    'ten-pin-bowling', 'tongue-twisters', 'united-states', 'vampire-numbers',
-    'λ', 'π', 'τ', 'φ', '√2', '𝑒'
+    'sudoku-v2', 'ten-pin-bowling', 'tongue-twisters', 'united-states',
+    'vampire-numbers', 'λ', 'π', 'τ', 'φ', '√2', '𝑒'
 );
 
 CREATE TYPE keymap AS ENUM ('default', 'vim');
