e3ade9f5d19adcad55b693acf929d1d102ee0e38
James Raspass <jraspass@gmail.com>
2021-05-03 21:17:00 +0100

    Make "Star Wars Opening Crawl" live

--- db/a-schema.sql
+++ db/a-schema.sql
@@ -12,9 +12,10 @@ CREATE TYPE hole AS ENUM (
     'odious-numbers', 'ordinal-numbers', 'pangram-grep', 'pascals-triangle',
     'pernicious-numbers', 'poker', 'prime-numbers', 'quine', 'recamán',
     'rock-paper-scissors-spock-lizard', 'roman-to-arabic', 'rule-110',
-    'seven-segment', 'sierpiński-triangle', 'spelling-numbers', 'sudoku',
-    'sudoku-v2', 'ten-pin-bowling', 'tongue-twisters', 'united-states',
-    'vampire-numbers', 'van-eck-sequence', 'λ', 'π', 'τ', 'φ', '√2', '𝑒'
+    'seven-segment', 'sierpiński-triangle', 'spelling-numbers',
+    'star-wars-opening-crawl', 'sudoku', 'sudoku-v2', 'ten-pin-bowling',
+    'tongue-twisters', 'united-states', 'vampire-numbers', 'van-eck-sequence',
+    'λ', 'π', 'τ', 'φ', '√2', '𝑒'
 );
 
 CREATE TYPE keymap AS ENUM ('default', 'vim');
