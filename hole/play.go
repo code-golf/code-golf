d5c10b54766ea713799bbbaffe04f5ead4da643b
James Raspass <jraspass@gmail.com>
2021-05-23 14:39:34 +0100

    Simplify asm run

--- hole/play.go
+++ hole/play.go
@@ -109,7 +109,7 @@ func Play(ctx context.Context, holeID, langID, code string) (score Scorecard) {
 	// Interpreter
 	switch langID {
 	case "assembly":
-		cmd.Args = []string{"/usr/bin/defasm", "-o", "/tmp/code", "-r"}
+		cmd.Args = []string{"/usr/bin/defasm", "-r"}
 	case "bash":
 		cmd.Args = []string{"/usr/bin/bash", "-s", "-"}
 	case "brainfuck":
