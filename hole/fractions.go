113ef75befe3138dd908a97b5e8016415f8386e4
H <76665736+MeWhenI@users.noreply.github.com>
2021-09-03 12:28:59 -0500

    Rework fractions.go
    Add larger fractions and more hardcoded test cases

--- hole/fractions.go
+++ hole/fractions.go
@@ -41,12 +41,61 @@ func isIrreducible(frac fract) bool {
 	return true
 }
 
-// generator of random fraction with non-zero denominator
-func fracGen() fract {
+// Generates a fraction with numerator and denominator both less than 16
+func smallFracGen() fract {
+	numer := rand.Intn(16)
+	denom := rand.Intn(15) + 1
+
+	// Choose a random scale factor so that the
+	// largest value in the fraction does not
+	// exceed 250
+	largerVal := numer
+	if denom > largerVal {
+		largerVal = denom
+	}
+	scale := rand.Intn(250/largerVal-1) + 1
+
 	return fract{
-		n: rand.Intn(16),
-		d: rand.Intn(15) + 1,
-		s: rand.Intn(15) + 1,
+		n: numer,
+		d: denom,
+		s: scale,
+	}
+}
+
+// Generates a fraction with at least one value greater than 15
+func largeFracGen() fract {
+	numer := rand.Intn(251)
+	denom := rand.Intn(250) + 1
+
+	// If neither value is greater than 15,
+	// reassign one chosen at random to a
+	// larger number
+	if numer <= 15 && denom <= 15 {
+		newVal := rand.Intn(235) + 16
+		chnged := rand.Intn(2)
+		if chnged == 0 {
+			numer = newVal
+		} else {
+			denom = newVal
+		}
+	}
+
+	// Choose a random scale factor so that the
+	// largest value in the fraction does not
+	// exceed 250
+	largerVal := numer
+	if denom > largerVal {
+		largerVal = denom
+	}
+	scale := 1
+	if largerVal <= 125 {
+		scale = rand.Intn(250/largerVal-1) + 1
+	}
+
+	return fract{
+		n: numer,
+		d: denom,
+		s: scale,
 	}
 }
 
@@ -55,57 +104,72 @@ func fractions() (args []string, out string) {
 
 	//// default cases
 	// hardcoded values
-	f1 := fract{n: 1, d: 1, s: 1}
-	f2 := fract{n: 1, d: 1, s: 10}
-	f3 := fract{n: 1, d: 2, s: 1}
-	f4 := fract{n: 1, d: 2, s: 10}
-	f5 := fract{n: 2, d: 1, s: 1}
-	f6 := fract{n: 2, d: 1, s: 10}
-	f7 := fract{n: 0, d: 1, s: 1}
-	f8 := fract{n: 15, d: 14, s: 15}
+	ra1 := rand.Intn(248) + 2
+	ra2 := rand.Intn(248) + 2
 
-	// 1 over 1
-	args = append(args, strconvunsimplifiedfrac(f1))
-	outs = append(outs, strconvsimplifiedfrac(f1))
+	hardcodes := []fract{
+		fract{n: 1, d: 1, s: 1},
+		fract{n: 1, d: 1, s: ra1},
+		fract{n: 1, d: 2, s: 1},
+		fract{n: 1, d: 2, s: 10},
+		fract{n: 1, d: 2, s: 100},
+		fract{n: 2, d: 1, s: 1},
+		fract{n: 2, d: 1, s: 100},
+		fract{n: 0, d: 1, s: 1},
+		fract{n: 0, d: 1, s: ra2},
+		fract{n: 15, d: 14, s: 15},
+		fract{n: 250, d: 249, s: 1},
+		fract{n: 127, d: 126, s: 2},
+		fract{n: 126, d: 127, s: 2},
+		fract{n: 51, d: 1, s: 5},
+		fract{n: 1, d: 51, s: 5},
+		fract{n: 249, d: 2, s: 1},
+		fract{n: 2, d: 249, s: 1},
+		fract{n: 15, d: 13, s: 9},
+		fract{n: 15, d: 13, s: 5},
+		fract{n: 13, d: 15, s: 9},
+		fract{n: 15, d: 11, s: 13},
+		fract{n: 11, d: 15, s: 13},
+		fract{n: 17, d: 19, s: 13},
+		fract{n: 19, d: 17, s: 13},
+	}
 
-	// 10 over 10
-	args = append(args, strconvunsimplifiedfrac(f2))
-	outs = append(outs, strconvsimplifiedfrac(f2))
+	for _, testCase := range hardcodes {
+		args = append(args, strconvunsimplifiedfrac(testCase))
+		outs = append(outs, strconvsimplifiedfrac(testCase))
+	}
 
-	// 1 over 2
-	args = append(args, strconvunsimplifiedfrac(f3))
-	outs = append(outs, strconvsimplifiedfrac(f3))
-
-	// 10 over 20
-	args = append(args, strconvunsimplifiedfrac(f4))
-	outs = append(outs, strconvsimplifiedfrac(f4))
-
-	// 2 over 1
-	args = append(args, strconvunsimplifiedfrac(f5))
-	outs = append(outs, strconvsimplifiedfrac(f5))
-
-	// 20 over 10
-	args = append(args, strconvunsimplifiedfrac(f6))
-	outs = append(outs, strconvsimplifiedfrac(f6))
-
-	// 0 over 1
-	args = append(args, strconvunsimplifiedfrac(f7))
-	outs = append(outs, strconvsimplifiedfrac(f7))
-
-	// 225 over 210
-	args = append(args, strconvunsimplifiedfrac(f8))
-	outs = append(outs, strconvsimplifiedfrac(f8))
-
-	//// generate 100 random cases
+	// generate 60 random cases with small reduced forms
 	cases := 0
-	for cases < 100 {
-		f1 = fracGen()
-		if isIrreducible(f1) {
-			args = append(args, strconvunsimplifiedfrac(f1))
-			outs = append(outs, strconvsimplifiedfrac(f1))
+	for cases < 60 {
+		f := smallFracGen()
+		if isIrreducible(f) {
+			args = append(args, strconvunsimplifiedfrac(f))
+			outs = append(outs, strconvsimplifiedfrac(f))
 			cases++
 		}
 	}
+
+	// generate 40 random cases with at least 1 large number in their reduced forms
+	// with at least 20 not being already simplified
+	simpCases := 0
+	unsimpCases := 0
+	for unsimpCases < 20 || unsimpCases+simpCases < 40 {
+		f := largeFracGen()
+		if isIrreducible(f) {
+			if simpCases < 20 && f.s == 1 {
+				args = append(args, strconvunsimplifiedfrac(f))
+				outs = append(outs, strconvsimplifiedfrac(f))
+				simpCases++
+			}
+			if f.s > 1 {
+				args = append(args, strconvunsimplifiedfrac(f))
+				outs = append(outs, strconvsimplifiedfrac(f))
+				unsimpCases++
+			}
+		}
+	}
+
 	// shuffle args and outputs in the same way
 	rand.Shuffle(len(args), func(i, j int) {
 		args[i], args[j] = args[j], args[i]
