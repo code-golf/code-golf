3c733376ddc21dbd411c9fb13158ac52013188ec
NewDefectus <36775845+NewDefectus@users.noreply.github.com>
2021-09-07 20:48:39 +0300

    Use gold linker for asm (#495)

--- hole/play.go
+++ hole/play.go
@@ -123,7 +123,7 @@ func Play(ctx context.Context, holeID, langID, code string) (score Scorecard) {
 			panic(err)
 		}
 
-		cmd.Args = []string{"/usr/bin/defasm", "--ldflags=-N", "--size-out=3", "-r"}
+		cmd.Args = []string{"/usr/bin/defasm", "-l", "ld.gold", "--ldflags=-N -u _start -T /usr/lib/defaultEntry.ld", "--size-out=3", "-r"}
 		cmd.ExtraFiles = []*os.File{asmBytesWrite}
 	case "bash":
 		cmd.Args = []string{"/usr/bin/bash", "-s", "-"}
