59321abf6fecb112ee4ec12c1ec9e69a57a74f1a
Frank Dragt <mailvoorfrank.dragt@gmail.com>
2021-09-18 13:21:02 +0200

    added lucky tickets cases by description in #179

--- hole/lucky-tickets.go
+++ hole/lucky-tickets.go
@@ -13,14 +13,24 @@ type ticket struct {
 
 var data = []ticket{
 	{8, 2, 70},
+	{14, 5, 454805755},
 	{6, 6, 4332},
+	{12, 7, 786588243},
 	{4, 8, 344},
 	{4, 9, 489},
+	{8, 9, 2306025},
 	{2, 10, 10},
 	{4, 10, 670},
 	{6, 10, 55252},
-	{14, 12, 39222848622984},
+	{8, 10, 4816030},
+	{12, 9, 12434998005},
+	{12, 10, 39581170420},
+	{12, 11, 112835748609},
+	{6, 13, 204763},
 	{4, 15, 2255},
+	{6, 15, 418503},
+	{8, 15, 82073295},
+	{10, 15, 16581420835},
 }
 
 func iPow(a, b int64) int64 {
@@ -49,11 +59,18 @@ func sumDigits(number int64, base int64) int64 {
 }
 
 func luckyTickets() ([]string, string) {
+	// make a random selection of 4 from the fixed cases
+	rand.Shuffle(len(data), func(i, j int) {
+		data[i], data[j] = data[j], data[i]
+	})
+	data = data[0:3]
 	tickets := make([]ticket, len(data))
+	// always add case 14 12
+	tickets = append(tickets, ticket{14, 12, 39222848622984})
 	copy(tickets, data)
 
 	// Randomly generate additional test cases.
-	for i := 0; i < 8; i++ {
+	for i := 0; i < 15; i++ {
 		digits := 2 + 2*rand.Intn(5)
 		base := 2 + rand.Intn(15)
 
