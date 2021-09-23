85db631a0a16207b99a3a6d1782b1d209f8f1b3c
Irratix <36267179+Irratix@users.noreply.github.com>
2021-09-24 01:57:06 +0200

    Add Lucky Tickets cases (#507)
    * added Lucky Tickets test cases
    
    * added lucky tickets cases by description in #179
    
    * fix slice of fixed tests
    
    * fixed abuse of global data

--- hole/lucky-tickets.go
+++ hole/lucky-tickets.go
@@ -13,11 +13,24 @@ type ticket struct {
 
 var data = []ticket{
 	{8, 2, 70},
+	{14, 5, 454805755},
+	{6, 6, 4332},
+	{12, 7, 786588243},
 	{4, 8, 344},
+	{4, 9, 489},
+	{8, 9, 2306025},
 	{2, 10, 10},
 	{4, 10, 670},
 	{6, 10, 55252},
-	{14, 12, 39222848622984},
+	{8, 10, 4816030},
+	{12, 9, 12434998005},
+	{12, 10, 39581170420},
+	{12, 11, 112835748609},
+	{6, 13, 204763},
+	{4, 15, 2255},
+	{6, 15, 418503},
+	{8, 15, 82073295},
+	{10, 15, 16581420835},
 }
 
 func iPow(a, b int64) int64 {
@@ -46,11 +59,16 @@ func sumDigits(number int64, base int64) int64 {
 }
 
 func luckyTickets() ([]string, string) {
-	tickets := make([]ticket, len(data))
-	copy(tickets, data)
+	var tickets []ticket
+	// always add case 14 12
+	tickets = append(tickets, ticket{14, 12, 39222848622984})
+	// add random sample of fixed cases
+	for _, test_case := range rand.Perm(len(data))[:4] {
+		tickets = append(tickets, data[test_case])
+	}
 
 	// Randomly generate additional test cases.
-	for i := 0; i < 5; i++ {
+	for i := 0; i < 15; i++ {
 		digits := 2 + 2*rand.Intn(5)
 		base := 2 + rand.Intn(15)
 
