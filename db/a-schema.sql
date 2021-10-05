3df5e001f9f73e4327d437a8f027e825c6e8e23b
James Raspass <jraspass@gmail.com>
2021-10-06 00:23:56 +0100

    Add "Smith Numbers" hole

--- db/a-schema.sql
+++ db/a-schema.sql
@@ -24,10 +24,10 @@ CREATE TYPE hole AS ENUM (
     'pangram-grep', 'pascals-triangle', 'pernicious-numbers', 'poker',
     'prime-numbers', 'qr-decoder', 'quine', 'recamán',
     'rock-paper-scissors-spock-lizard', 'roman-to-arabic', 'rule-110',
-    'seven-segment', 'sierpiński-triangle', 'spelling-numbers',
-    'star-wars-opening-crawl', 'sudoku', 'sudoku-v2', 'ten-pin-bowling',
-    'tongue-twisters', 'united-states', 'vampire-numbers', 'van-eck-sequence',
-    'λ', 'π', 'τ', 'φ', '√2', '𝑒'
+    'seven-segment', 'sierpiński-triangle', 'smith-numbers',
+    'spelling-numbers', 'star-wars-opening-crawl', 'sudoku', 'sudoku-v2',
+    'ten-pin-bowling', 'tongue-twisters', 'united-states', 'vampire-numbers',
+    'van-eck-sequence', 'λ', 'π', 'τ', 'φ', '√2', '𝑒'
 );
 
 CREATE TYPE keymap AS ENUM ('default', 'vim');
