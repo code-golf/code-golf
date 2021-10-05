1db6f5de257edbf920796e02384901f529848c59
James Raspass <jraspass@gmail.com>
2021-10-06 00:24:54 +0100

    Update feeds

--- routes/feed.go
+++ routes/feed.go
@@ -25,6 +25,7 @@ func init() {
 		sha, created, id string
 		hole             bool
 	}{
+		{"3df5e00", "2021-10-05 23:23:56", "smith-numbers", true},
 		{"428a369", "2021-09-19 19:58:44", "happy-numbers-long", true},
 		{"1978cdf", "2021-09-17 22:19:46", "qr-decoder", true},
 		{"6d99d16", "2021-09-03 18:00:28", "fractions", true},
