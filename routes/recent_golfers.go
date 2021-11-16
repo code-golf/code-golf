caf44d97fc41e62372ff3564a4f8bef975626b23
James Raspass <jraspass@gmail.com>
2021-11-16 22:15:42 +0000

    Add lang icons to /recent/golfers

--- routes/recent_golfers.go
+++ routes/recent_golfers.go
@@ -7,6 +7,7 @@ import (
 	"github.com/code-golf/code-golf/config"
 	"github.com/code-golf/code-golf/pager"
 	"github.com/code-golf/code-golf/session"
+	"github.com/lib/pq"
 )
 
 // RecentGolfers serves GET /recent/golfers
@@ -15,6 +16,7 @@ func RecentGolfers(w http.ResponseWriter, r *http.Request) {
 		Cheevos       int
 		Country, Name string
 		Date          time.Time
+		Langs         []string
 	}
 
 	data := struct {
@@ -23,17 +25,23 @@ func RecentGolfers(w http.ResponseWriter, r *http.Request) {
 	}{len(config.CheevoList), make([]golfer, 0, pager.PerPage)}
 
 	rows, err := session.Database(r).Query(
-		`WITH recent AS (
+		`WITH langs AS (
+		    SELECT user_id, array_agg(DISTINCT lang) langs
+		      FROM solutions
+		     WHERE NOT failing
+		  GROUP BY user_id
+		), recent AS (
 		    SELECT user_id, COUNT(*) cheevos, MIN(earned) date
 		      FROM trophies
 		  GROUP BY user_id
 		  ORDER BY date DESC
 		     LIMIT $1
-		) SELECT COALESCE(CASE WHEN show_country THEN country END, ''),
-		         cheevos, date, login
-		    FROM recent
-		    JOIN users ON id = user_id
-		ORDER BY date DESC`,
+		)  SELECT COALESCE(CASE WHEN show_country THEN country END, ''),
+		          cheevos, date, login, langs
+		     FROM recent
+		     JOIN users ON id = user_id
+		LEFT JOIN langs USING (user_id)
+		 ORDER BY date DESC`,
 		pager.PerPage,
 	)
 	if err != nil {
@@ -43,7 +51,13 @@ func RecentGolfers(w http.ResponseWriter, r *http.Request) {
 	for rows.Next() {
 		var g golfer
 
-		if err := rows.Scan(&g.Country, &g.Cheevos, &g.Date, &g.Name); err != nil {
+		if err := rows.Scan(
+			&g.Country,
+			&g.Cheevos,
+			&g.Date,
+			&g.Name,
+			pq.Array(&g.Langs),
+		); err != nil {
 			panic(err)
 		}
 
