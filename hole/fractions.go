b94e0e315f45909979ff2e051e7fd5e02b94295f
James Raspass <jraspass@gmail.com>
2021-11-24 01:12:58 +0000

    Massively simplify fractions.go

--- hole/fractions.go
+++ hole/fractions.go
@@ -1,186 +1,133 @@
 package hole
 
 import (
+	"fmt"
 	"math/rand"
-	"strconv"
 	"strings"
 )
 
-// a fraction (fract) is defined by its
-// numerator and denominator in simplified
-// form (n, d) and its scale factor (s)
-type fract struct{ n, d, s int }
+type fraction struct{ numerator, denominator, scale int }
 
-// most of this code is lifted from the
-// intersection hole
-func strconvunsimplifiedfrac(frac fract) (out string) {
-	var outs []string
-	outs = append(outs, strconv.Itoa(frac.n*frac.s))
-	outs = append(outs, strconv.Itoa(frac.d*frac.s))
-	return strings.Join(outs, "/")
-}
-
-func strconvsimplifiedfrac(frac fract) (out string) {
-	var outs []string
-	outs = append(outs, strconv.Itoa(frac.n))
-	outs = append(outs, strconv.Itoa(frac.d))
-	return strings.Join(outs, "/")
-}
-
-// if numerator and denominator are
-// divisible by a number greater than
-// 1 then the fraction is reducible
-func isIrreducible(frac fract) bool {
-	gcd := 2
-	for gcd <= frac.d {
-		if frac.n%gcd == 0 && frac.d%gcd == 0 {
+// If numerator and denominator are divisible by a number greater than 1 then
+// the fraction is reducible.
+func (f fraction) isIrreducible() bool {
+	for i := 2; i <= f.denominator; i++ {
+		if f.numerator%i == 0 && f.denominator%i == 0 {
 			return false
 		}
-		gcd++
 	}
 	return true
 }
 
 // Generates a fraction with numerator and denominator both less than 16
-func smallFracGen() fract {
-	numer := rand.Intn(16)
-	denom := rand.Intn(15) + 1
+func smallFracGen() fraction {
+	f := fraction{numerator: rand.Intn(16), denominator: rand.Intn(15) + 1}
 
-	// Choose a random scale factor so that the
-	// largest value in the fraction does not
-	// exceed 250
-	largerVal := numer
-	if denom > largerVal {
-		largerVal = denom
-	}
-	scale := rand.Intn(250/largerVal-1) + 1
+	// Choose a random scale factor so that the largest value in the fraction
+	// does not exceed 250.
+	max := maxint(f.numerator, f.denominator)
+	f.scale = rand.Intn(250/max-1) + 1
 
-	return fract{
-		n: numer,
-		d: denom,
-		s: scale,
-	}
+	return f
 }
 
 // Generates a fraction with at least one value greater than 15
-func largeFracGen() fract {
-	numer := rand.Intn(251)
-	denom := rand.Intn(250) + 1
+func largeFracGen() fraction {
+	f := fraction{numerator: rand.Intn(251), denominator: rand.Intn(250) + 1}
 
-	// If neither value is greater than 15,
-	// reassign one chosen at random to a
-	// larger number
-	if numer <= 15 && denom <= 15 {
+	// If neither value is greater than 15, reassign one chosen at random to a
+	// larger number.
+	if f.numerator <= 15 && f.denominator <= 15 {
 		newVal := rand.Intn(235) + 16
-		chnged := rand.Intn(2)
-		if chnged == 0 {
-			numer = newVal
+		if rand.Intn(2) == 0 {
+			f.numerator = newVal
 		} else {
-			denom = newVal
+			f.denominator = newVal
 		}
 	}
 
-	// Choose a random scale factor so that the
-	// largest value in the fraction does not
-	// exceed 250
-	largerVal := numer
-	if denom > largerVal {
-		largerVal = denom
-	}
-	scale := 1
-	if largerVal <= 125 {
-		scale = rand.Intn(250/largerVal-1) + 1
+	// Choose a random scale factor so that the largest value in the fraction
+	// does not exceed 250.
+	f.scale = 1
+	if max := maxint(f.numerator, f.denominator); max <= 125 {
+		f.scale = rand.Intn(250/max-1) + 1
 	}
 
-	return fract{
-		n: numer,
-		d: denom,
-		s: scale,
-	}
+	return f
 }
 
-func fractions() (args []string, out string) {
-	var outs []string
-
-	//// default cases
-	// hardcoded values
+func fractions() ([]string, string) {
+	// Default cases.
 	ra1 := rand.Intn(249) + 2
 	ra2 := rand.Intn(249) + 2
 	ra3 := rand.Intn(249) + 2
 	ra4 := rand.Intn(151) + 100
 
-	hardcodes := []fract{
-		fract{n: 1, d: 1, s: 1},
-		fract{n: 1, d: 1, s: ra1},
-		fract{n: 1, d: ra3, s: 1},
-		fract{n: ra3, d: 1, s: 1},
-		fract{n: 1, d: ra4, s: 1},
-		fract{n: ra4, d: 1, s: 1},
-		fract{n: 1, d: 2, s: 1},
-		fract{n: 1, d: 2, s: 10},
-		fract{n: 1, d: 2, s: 100},
-		fract{n: 2, d: 1, s: 1},
-		fract{n: 2, d: 1, s: 100},
-		fract{n: 0, d: 1, s: 1},
-		fract{n: 0, d: 1, s: ra2},
-		fract{n: 15, d: 14, s: 15},
-		fract{n: 250, d: 249, s: 1},
-		fract{n: 125, d: 124, s: 2},
-		fract{n: 124, d: 125, s: 2},
-		fract{n: 50, d: 1, s: 5},
-		fract{n: 1, d: 50, s: 5},
-		fract{n: 249, d: 2, s: 1},
-		fract{n: 2, d: 249, s: 1},
-		fract{n: 15, d: 13, s: 9},
-		fract{n: 15, d: 13, s: 5},
-		fract{n: 13, d: 15, s: 9},
-		fract{n: 15, d: 11, s: 13},
-		fract{n: 11, d: 15, s: 13},
-		fract{n: 17, d: 19, s: 13},
-		fract{n: 19, d: 17, s: 13},
+	fractions := []fraction{
+		{numerator: 1, denominator: 1, scale: 1},
+		{numerator: 1, denominator: 1, scale: ra1},
+		{numerator: 1, denominator: ra3, scale: 1},
+		{numerator: ra3, denominator: 1, scale: 1},
+		{numerator: 1, denominator: ra4, scale: 1},
+		{numerator: ra4, denominator: 1, scale: 1},
+		{numerator: 1, denominator: 2, scale: 1},
+		{numerator: 1, denominator: 2, scale: 10},
+		{numerator: 1, denominator: 2, scale: 100},
+		{numerator: 2, denominator: 1, scale: 1},
+		{numerator: 2, denominator: 1, scale: 100},
+		{numerator: 0, denominator: 1, scale: 1},
+		{numerator: 0, denominator: 1, scale: ra2},
+		{numerator: 15, denominator: 14, scale: 15},
+		{numerator: 250, denominator: 249, scale: 1},
+		{numerator: 125, denominator: 124, scale: 2},
+		{numerator: 124, denominator: 125, scale: 2},
+		{numerator: 50, denominator: 1, scale: 5},
+		{numerator: 1, denominator: 50, scale: 5},
+		{numerator: 249, denominator: 2, scale: 1},
+		{numerator: 2, denominator: 249, scale: 1},
+		{numerator: 15, denominator: 13, scale: 9},
+		{numerator: 15, denominator: 13, scale: 5},
+		{numerator: 13, denominator: 15, scale: 9},
+		{numerator: 15, denominator: 11, scale: 13},
+		{numerator: 11, denominator: 15, scale: 13},
+		{numerator: 17, denominator: 19, scale: 13},
+		{numerator: 19, denominator: 17, scale: 13},
 	}
 
-	for _, testCase := range hardcodes {
-		args = append(args, strconvunsimplifiedfrac(testCase))
-		outs = append(outs, strconvsimplifiedfrac(testCase))
-	}
-
-	// generate 60 random cases with small reduced forms
-	cases := 0
-	for cases < 60 {
-		f := smallFracGen()
-		if isIrreducible(f) {
-			args = append(args, strconvunsimplifiedfrac(f))
-			outs = append(outs, strconvsimplifiedfrac(f))
-			cases++
+	// Generate 60 random cases with small reduced forms.
+	for i := 0; i < 60; {
+		if f := smallFracGen(); f.isIrreducible() {
+			fractions = append(fractions, f)
+			i++
 		}
 	}
 
-	// generate 40 random cases with at least 1 large number in their reduced forms
-	// with at least 20 not being already simplified
-	simpCases := 0
-	unsimpCases := 0
+	// Generate 40 random cases with at least 1 large number in their reduced
+	// forms with at least 20 not being already simplified.
+	var simpCases, unsimpCases int
 	for unsimpCases < 20 || unsimpCases+simpCases < 40 {
-		f := largeFracGen()
-		if isIrreducible(f) {
-			if simpCases < 20 && f.s == 1 {
-				args = append(args, strconvunsimplifiedfrac(f))
-				outs = append(outs, strconvsimplifiedfrac(f))
+		if f := largeFracGen(); f.isIrreducible() {
+			if simpCases < 20 && f.scale == 1 {
+				fractions = append(fractions, f)
 				simpCases++
-			}
-			if f.s > 1 {
-				args = append(args, strconvunsimplifiedfrac(f))
-				outs = append(outs, strconvsimplifiedfrac(f))
+			} else if f.scale > 1 {
+				fractions = append(fractions, f)
 				unsimpCases++
 			}
 		}
 	}
 
-	// shuffle args and outputs in the same way
-	rand.Shuffle(len(args), func(i, j int) {
-		args[i], args[j] = args[j], args[i]
-		outs[i], outs[j] = outs[j], outs[i]
+	rand.Shuffle(len(fractions), func(i, j int) {
+		fractions[i], fractions[j] = fractions[j], fractions[i]
 	})
-	out = strings.Join(outs, "\n")
-	return
+
+	args := make([]string, len(fractions))
+	outs := make([]string, len(fractions))
+
+	for i, f := range fractions {
+		args[i] = fmt.Sprintf("%d/%d", f.numerator*f.scale, f.denominator*f.scale)
+		outs[i] = fmt.Sprintf("%d/%d", f.numerator, f.denominator)
+	}
+
+	return args, strings.Join(outs, "\n")
 }
