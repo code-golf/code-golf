cc7115314cf96df55ecf948a0b7d8fb55d9dd0d6
James Raspass <jraspass@gmail.com>
2022-01-10 19:49:55 +0000

    Update feeds

--- routes/feed.go
+++ routes/feed.go
@@ -25,6 +25,7 @@ func init() {
 		sha, created, id string
 		hole             bool
 	}{
+		{"1e59551", "2022-01-10 19:25:07", "isbn", true},
 		{"ab1f804", "2022-01-09 21:45:31", "k", false},
 		{"1628145", "2022-01-02 03:29:48", "catalans-constant", true},
 		{"e431f15", "2022-01-01 20:45:25", "cpp", false},
