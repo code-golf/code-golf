86f4d683cb1760e2671bb86a5abaac3396166b7d
H <76665736+MeWhenI@users.noreply.github.com>
2022-01-10 02:23:12 -0600

    Update isbn.go (#578)
    Guarantee some of the ISBN's end in X

--- hole/isbn.go
+++ hole/isbn.go
@@ -5,48 +5,59 @@ import (
 	"strconv"
 )
 
-func isbn() (args []string, out string) {
-	for i := 0; i < 20; i++ {
-		weightedDigitsSum := 0
-		weight := 10
+func check_digit(digits [9]int) (check int) {
+	weightedDigitsSum := 0
+	weight := 10
 
-		// First digit of ISBN, not sticking with traditional 1 or 0, can't
-		// let them exploit that.
-		firstDigit := rand.Intn(10)
-
-		weightedDigitsSum += firstDigit * weight
+	for _, digit := range digits {
+		weightedDigitsSum += digit * weight
 		weight--
+	}
+	check = (11 - (weightedDigitsSum % 11)) % 11
+	return
+}
 
-		arg := strconv.Itoa(firstDigit) + "-"
+func isbn() (args []string, out string) {
+	guaranteedTens := rand.Perm(100)
+	for i := 0; i < 100; i++ {
+		var digits [9]int
+
+		for j := 0; j < 9; j++ {
+			digits[j] = rand.Intn(10)
+		}
+
+		// Guarantee at least 5 arguments end with 'X'
+		if guaranteedTens[i] < 5 {
+			if digits[7] == 9 {
+				digits[7] = 8
+			}
+
+			for check_digit(digits) != 2 && check_digit(digits) != 10 {
+				digits[8] = (digits[8] + 1) % 10
+			}
+
+			if check_digit(digits) != 10 {
+				digits[7] += 1
+			}
+		}
+
+		arg := ""
 
 		// This here logic is for varying the second two parts of the ISBN.
 		// Sure, it's cosmetic, but it might mess some people up.
-		difference := 6 - rand.Intn(5)
-		for i := 0; i < difference; i++ {
-			publisherDigit := rand.Intn(10)
+		difference := 7 - rand.Intn(5)
+		for j, digit := range digits {
+			arg += strconv.Itoa(digit)
 
-			weightedDigitsSum += publisherDigit * weight
-			weight--
-
-			arg += strconv.Itoa(publisherDigit)
-		}
-
-		arg += "-"
-
-		difference = 8 - difference
-		for j := 0; j < difference; j++ {
-			titleDigit := rand.Intn(10)
-
-			weightedDigitsSum += titleDigit * weight
-			weight--
-
-			arg += strconv.Itoa(titleDigit)
+			if j == 0 || j == difference {
+				arg += "-"
+			}
 		}
 
 		arg += "-"
 		args = append(args, arg)
 
-		if check := (11 - (weightedDigitsSum % 11)) % 11; check == 10 {
+		if check := check_digit(digits); check == 10 {
 			out += arg + "X\n"
 		} else {
 			out += arg + strconv.Itoa(check) + "\n"
