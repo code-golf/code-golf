b564bacdc53c1d83df81f1fed620b9ab88e045f8
Sen Jiang <JayXon@Gmail.com>
2022-02-09 01:39:31 -0800

    Include numbers larger than 200 in ordinal numbers
    RIP %99

--- hole/ordinal-numbers.go
+++ hole/ordinal-numbers.go
@@ -9,6 +9,9 @@ import (
 
 func ordinalNumbers() (args []string, out string) {
 	for _, i := range rand.Perm(200) {
+		if i > 100 {
+			i += rand.Intn(20) * 100
+		}
 		s := strconv.Itoa(i)
 
 		args = append(args, s)
