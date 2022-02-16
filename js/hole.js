86572414fe0c3471f9ce3c9cbd98e05c3c914731
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2022-02-16 09:25:56 +0100

    use let instead of var

--- js/hole.js
+++ js/hole.js
@@ -402,11 +402,11 @@ function getScoring(str, index) {
 
 //https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript
 function copyToClipboard(text) {
-    var input = document.createElement('textarea');
+    let input = document.createElement('textarea');
     input.innerHTML = text;
     document.body.appendChild(input);
     input.select();
-    var result = document.execCommand('copy');
+    let result = document.execCommand('copy');
     document.body.removeChild(input);
     return result;
 }
