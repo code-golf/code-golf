1501782106f9d60b972745bf5b5813b473d240ec
James Raspass <jraspass@gmail.com>
2022-02-28 19:27:59 +0000

    Speedup the golfer holes page
    The main win comes from using the rankings mat view but switching to a
    single flat map certainly makes the code easier to follow.

--- routes/golfer_holes.go
+++ routes/golfer_holes.go
@@ -10,10 +10,22 @@ import (
 // GolferHoles serves GET /golfers/{golfer}/holes
 func GolferHoles(w http.ResponseWriter, r *http.Request) {
 	golfer := session.GolferInfo(r).Golfer
-	scoring := param(r, "scoring")
+	data := struct {
+		Holes    []*config.Hole
+		Langs    []*config.Lang
+		Ranks    map[string]int
+		Scoring  string
+		Scorings []string
+	}{
+		Holes:    config.HoleList,
+		Langs:    config.LangList,
+		Ranks:    map[string]int{},
+		Scoring:  param(r, "scoring"),
+		Scorings: []string{"bytes", "chars"},
+	}
 
-	if scoring != "bytes" && scoring != "chars" {
-		if scoring == "" {
+	if data.Scoring != "bytes" && data.Scoring != "chars" {
+		if data.Scoring == "" {
 			http.Redirect(w, r, "/golfers/"+golfer.Name+"/holes/chars", http.StatusPermanentRedirect)
 			return
 		}
@@ -22,47 +34,10 @@ func GolferHoles(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	data := struct {
-		Holes        []*config.Hole
-		Langs        []*config.Lang
-		Ranks        []map[string]map[string]int
-		ScoringIndex int
-		Scorings     []string
-	}{
-		Holes:    config.HoleList,
-		Langs:    config.LangList,
-		Ranks:    []map[string]map[string]int{{}, {}},
-		Scorings: []string{"bytes", "chars"},
-	}
-
-	if scoring == data.Scorings[1] {
-		data.ScoringIndex = 1
-	}
-
 	rows, err := session.Database(r).Query(
-		`WITH matrix AS (
-		  SELECT user_id,
-		         hole,
-		         lang,
-		         RANK() OVER (PARTITION BY hole, lang ORDER BY `+data.Scorings[0]+`)
-		    FROM solutions
-		   WHERE NOT failing
-		     AND scoring = $2
-		), other_matrix AS (
-		  SELECT user_id,
-		         hole,
-		         lang,
-		         RANK() OVER (PARTITION BY hole, lang ORDER BY `+data.Scorings[1]+`) other_rank
-		    FROM solutions
-		   WHERE NOT failing
-		     AND scoring = $3
-		) SELECT hole, lang, COALESCE(rank, 0), COALESCE(other_rank, 0)
-		    FROM matrix
-	   FULL JOIN other_matrix USING (user_id, hole, lang)
-		   WHERE user_id = $1`,
+		`SELECT hole::text || lang::text || scoring::text, rank
+		   FROM rankings WHERE user_id = $1`,
 		golfer.ID,
-		data.Scorings[0],
-		data.Scorings[1],
 	)
 	if err != nil {
 		panic(err)
@@ -71,20 +46,14 @@ func GolferHoles(w http.ResponseWriter, r *http.Request) {
 	defer rows.Close()
 
 	for rows.Next() {
-		var hole, lang string
-		var rank0, rank1 int
+		var key string
+		var rank int
 
-		if err := rows.Scan(&hole, &lang, &rank0, &rank1); err != nil {
+		if err := rows.Scan(&key, &rank); err != nil {
 			panic(err)
 		}
 
-		if data.Ranks[0][hole] == nil {
-			data.Ranks[0][hole] = map[string]int{}
-			data.Ranks[1][hole] = map[string]int{}
-		}
-
-		data.Ranks[0][hole][lang] = rank0
-		data.Ranks[1][hole][lang] = rank1
+		data.Ranks[key] = rank
 	}
 
 	if err := rows.Err(); err != nil {
