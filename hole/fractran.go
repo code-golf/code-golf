2f980b5f03073b6761964b9fed7a87e40b9302ff
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2022-03-09 16:24:15 +0100

    refactor+add more obfuscation, - test, noop test, move test,

--- hole/fractran.go
+++ hole/fractran.go
@@ -1,6 +1,7 @@
 package hole
 
 import (
+	"fmt"
 	"math"
 	"math/rand"
 	"strconv"
@@ -18,6 +19,7 @@ type Program = []Number
 var primes = []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47}
 
 func Mul(a Number, b Number) Number {
+	// Returns a*b.
 	prod := Number{}
 	for p, e := range a {
 		prod[p] = e
@@ -31,7 +33,17 @@ func Mul(a Number, b Number) Number {
 	return prod
 }
 
+func Pow(a Number, exp int) Number {
+	// Returns a^exp.
+	power := Number{}
+	for p, e := range a {
+		power[p] = e * exp
+	}
+	return power
+}
+
 func IsInteger(a Number) bool {
+	// Indicates whether a is whole.
 	for _, e := range a {
 		if e < 0 {
 			return false
@@ -41,6 +53,7 @@ func IsInteger(a Number) bool {
 }
 
 func ToFraction(a Number) (int, int) {
+	// Returns numerator and denominator of a.
 	n, d := 1, 1
 	for p, e := range a {
 		for e > 0 {
@@ -56,16 +69,19 @@ func ToFraction(a Number) (int, int) {
 }
 
 func ToString(a Number) string {
+	// Converts a to string.
 	n, d := ToFraction(a)
 	return strconv.Itoa(n) + "/" + strconv.Itoa(d)
 }
 
 func ToStringNumerator(a Number) string {
+	// Converts a to string assuming it's integer.
 	n, _ := ToFraction(a)
 	return strconv.Itoa(n)
 }
 
 func Run(n Number, program Program) Number {
+	// Interprets the given FRACTRAN program on the input n.
 	for {
 		halt := true
 		for _, x := range program {
@@ -73,7 +89,6 @@ func Run(n Number, program Program) Number {
 			if IsInteger(newN) {
 				n = newN
 				halt = false
-				// fmt.Println(n)
 				break
 			}
 		}
@@ -84,6 +99,8 @@ func Run(n Number, program Program) Number {
 }
 
 func randomVars(reserved int, vars ...*int) {
+	// Initializes the variables to random primes, skipping
+	// the given amount of reserved primes.
 	nums := []int{}
 	for i := reserved; i < len(primes); i++ {
 		nums = append(nums, primes[i])
@@ -96,8 +113,26 @@ func randomVars(reserved int, vars ...*int) {
 	}
 }
 
+func RandomFraction(unused []int) Number {
+	// Returns a random noninteger fraction which is `unused`-smooth.
+	result := Number{}
+	for i := 0; i < 3; i++ {
+		p := unused[rand.Intn(len(unused))]
+		for result[p] > 0 {
+			p = unused[rand.Intn(len(unused))]
+		}
+		result[p]--
+		p = unused[rand.Intn(len(unused))]
+		if result[p] >= 0 {
+			result[p]++
+		}
+	}
+	return result
+}
+
 func SwapProgram() Program {
-	// Given 2^a × 3^b × 5 returns 2^b × 3^a
+	// Returns the following (partially randomized) FRACTRAN program:
+	// Given 2^a × 3^b × 5 returns 2^b × 3^a.
 	var S1, T0, T1, A int
 	S0 := 5
 	randomVars(3, &S1, &T0, &T1, &A)
@@ -113,14 +148,24 @@ func SwapProgram() Program {
 }
 
 func AddProgram() Program {
-	// Given 2^a × 3^b returns 2^(a+b)
+	// Returns the following (partially randomized) FRACTRAN program:
+	// Given 2^a × 3^b returns 2^(a+b).
 	return Program{
 		{2: 1, 3: -1},
 	}
 }
 
+func SubtractProgram() Program {
+	// Returns the following (partially randomized) FRACTRAN program:
+	// Given 2^a × 3^b, a>b returns 2^(a-b).
+	return Program{
+		{2: -1, 3: -1},
+	}
+}
+
 func MinProgram() Program {
-	// Given 2^a × 3^b returns 2^min(a,b)
+	// Returns the following (partially randomized) FRACTRAN program:
+	// Given 2^a × 3^b returns 2^min(a,b).
 	var S0, S1, A int
 	randomVars(2, &S0, &S1, &A)
 	return Program{
@@ -135,7 +180,8 @@ func MinProgram() Program {
 }
 
 func MaxProgram() Program {
-	// Given 2^a × 3^b returns 2^max(a,b)
+	// Returns the following (partially randomized) FRACTRAN program:
+	// Given 2^a × 3^b returns 2^max(a,b).
 	var S0, S1, A int
 	randomVars(2, &S0, &S1, &A)
 	return Program{
@@ -148,8 +194,23 @@ func MaxProgram() Program {
 	}
 }
 
+func NoopProgram() Program {
+	// Returns the following (partially randomized) FRACTRAN program:
+	// Given 2^a × 3^a returns 2^a × 3^a.
+	return Program{}
+}
+
+func MoveProgram() Program {
+	// Returns the following (partially randomized) FRACTRAN program:
+	// Given 3^a returns 2^a.
+	return Program{
+		Number{3: -1, 2: 1},
+	}
+}
+
 func FibonacciProgram() Program {
-	// Given 3^a returns 2^Fibonacci(a)
+	// Returns the following (partially randomized) FRACTRAN program:
+	// Given 3^a returns 2^Fibonacci(a).
 	var S0, S1, T0, T1, U0, U1, B, C int
 	A := 2
 	randomVars(2, &S0, &S1, &T0, &T1, &U0, &U1, &B, &C)
@@ -170,10 +231,32 @@ func FibonacciProgram() Program {
 
 const MAXINT = int(^uint(0) >> 1)
 
+func IntMin(a int, b int) int {
+	// Returns the minimum of integers a,b.
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func IntPower(a int, exp int) int {
+	// Returns a^exp.
+	return int(math.Pow(float64(a), float64(exp)))
+}
+
+func MaxPower(a int, lim int) int {
+	// Returns the maximum r, such that a^r <= lim.
+	if a == 1 {
+		return 64
+	}
+	return int(math.Log(float64(lim)) / math.Log(float64(a)))
+}
+
 func RandomABPair(lim int) ABPair {
+	// Return a random pair (a,b), such that 2^a × 3^b <= lim.
 	pairs := []ABPair{}
-	for a := 0; float64(a) < math.Log2(float64(lim)); a++ {
-		for b := 0; float64(b) < math.Log(float64(lim)/math.Pow(2.0, float64(a)))/math.Log(3); b++ {
+	for a := 1; a < MaxPower(2, lim); a++ {
+		for b := 1; b < MaxPower(3, lim/IntPower(2, a)); b++ {
 			pairs = append(pairs, ABPair{a, b})
 		}
 	}
@@ -181,6 +264,7 @@ func RandomABPair(lim int) ABPair {
 }
 
 func insert(a Program, index int, value Number) Program {
+	// Returns a new slice, with `value` inserted into `a` at position `index`
 	if len(a) == index { // nil or empty slice or after last element
 		return append(a, value)
 	}
@@ -189,28 +273,57 @@ func insert(a Program, index int, value Number) Program {
 	return a
 }
 
-func Obfuscate(program Program) Program {
-	// Works by doubling random instructions
-	// TODO: replace with random powers of random instructions
-	result := Program{}
-	for i := 0; i < len(program); i++ {
-		result = append(result, program[i])
+func RandomPower(f Number) Number {
+	// Returns a random power of f, such that bot numerator and denominator fit into int64.
+	n, d := ToFraction(f)
+	lim := IntMin(MaxPower(n, MAXINT), MaxPower(d, MAXINT))
+	return Pow(f, rand.Intn(lim)+1)
+}
+
+func GetUnusedVars(program Program) []int {
+	usage := map[int]bool{}
+	for _, frac := range program {
+		for p, e := range frac {
+			if e != 0 {
+				usage[p] = true
+			}
+		}
 	}
-	for i := 0; i < 12; i++ {
-		r := rand.Intn(len(result))
-		result = insert(result, r, result[r])
+	result := []int{}
+	for i := 2; i < len(primes); i++ {
+		if !usage[primes[i]] {
+			result = append(result, primes[i])
+		}
 	}
 	return result
 }
 
-func min(a int, b int) int {
-	if a < b {
-		return a
+func Obfuscate(program Program) Program {
+	// Returns an equivalent program to the input one,
+	// by adding noop instructions.
+
+	// copy program
+	result := Program{}
+	for i := 0; i < len(program); i++ {
+		result = append(result, program[i])
 	}
-	return b
+
+	// add new (noop) instructions
+	unused := GetUnusedVars(result)
+	for i := 0; i < 12; i++ {
+		r := rand.Intn(len(result) + 1)
+		result = insert(result, r, RandomFraction(unused))
+	}
+	// clone instructions
+	for i := 0; i < 12; i++ {
+		r := rand.Intn(len(result))
+		result = insert(result, r, RandomPower(result[r]))
+	}
+	return result
 }
 
 func fractran() ([]string, string) {
+	// Generates inputs and corresponding answers for the tests.
 	type TestInput struct {
 		Input   Number
 		Program Program
@@ -226,12 +339,27 @@ func fractran() ([]string, string) {
 		expected = append(expected, Number{2: (pair.a + pair.b)})
 	}
 	for _, pair := range []ABPair{RandomABPair(MAXINT), {12, 8}} {
+		for pair.a < pair.b {
+			pair = RandomABPair(MAXINT)
+		}
+		tests = append(tests, TestInput{Number{2: pair.a, 3: pair.b}, SubtractProgram()})
+		expected = append(expected, Number{2: (pair.a - pair.b)})
+	}
+	for _, pair := range []ABPair{RandomABPair(MAXINT), {12, 8}} {
 		tests = append(tests, TestInput{Number{2: pair.a, 3: pair.b}, MinProgram()})
-		expected = append(expected, Number{2: min(pair.a, pair.b)})
+		expected = append(expected, Number{2: IntMin(pair.a, pair.b)})
 	}
 	for _, pair := range []ABPair{RandomABPair(MAXINT), {12, 8}} {
 		tests = append(tests, TestInput{Number{2: pair.a, 3: pair.b}, MaxProgram()})
-		expected = append(expected, Number{2: -min(-pair.a, -pair.b)})
+		expected = append(expected, Number{2: -IntMin(-pair.a, -pair.b)})
+	}
+	for _, pair := range []ABPair{RandomABPair(MAXINT), {12, 8}} {
+		tests = append(tests, TestInput{Number{2: pair.a, 3: pair.b}, NoopProgram()})
+		expected = append(expected, Number{2: pair.a, 3: pair.b})
+	}
+	for _, pair := range []ABPair{RandomABPair(MAXINT), {12, 8}} {
+		tests = append(tests, TestInput{Number{3: pair.b}, MoveProgram()})
+		expected = append(expected, Number{2: pair.b})
 	}
 	tests = append(tests, TestInput{Number{3: 7}, FibonacciProgram()})
 	expected = append(expected, Number{2: 13})
@@ -239,7 +367,12 @@ func fractran() ([]string, string) {
 	expected = append(expected, Number{2: 55})
 
 	for i := 0; i < len(tests); i++ {
+		tests[i].Program = Obfuscate(tests[i].Program)
+
 		if ToString(Run(tests[i].Input, tests[i].Program)) != ToString(expected[i]) {
+			fmt.Println(tests[i].Input)
+			fmt.Println(Run(tests[i].Input, tests[i].Program))
+			fmt.Println(expected[i])
 			panic("Assertion error." + strconv.Itoa(i))
 		}
 	}
