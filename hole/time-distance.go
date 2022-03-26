c8382ea6aef948ff7977c6bde474ecc789b1c020
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2022-03-26 09:15:49 +0100

    simplify time distance

--- hole/time-distance.go
+++ hole/time-distance.go
@@ -52,7 +52,7 @@ func formatDistance(secs int) string {
 func timeDistance() ([]string, string) {
 	const rep = 32
 
-	tests := []int{0}
+	inputs := []int{0}
 
 	unitsChosen := []int{1, 2, 3, 4, 5, 6, 7}
 	for i := 0; i <= rep; i++ {
@@ -62,36 +62,26 @@ func timeDistance() ([]string, string) {
 	for _, j := range unitsChosen {
 		secs := units[j].seconds
 		secsLarger := units[j-1].seconds
-		tests = append(tests, randInt(secs, secs*2-1))        // future singular
-		tests = append(tests, -randInt(secs, secs*2-1))       // past singular
-		tests = append(tests, randInt(2*secs, secsLarger-1))  // future plural
-		tests = append(tests, -randInt(2*secs, secsLarger-1)) // past plural
+		inputs = append(inputs, randInt(secs, secs*2-1))        // future singular
+		inputs = append(inputs, -randInt(secs, secs*2-1))       // past singular
+		inputs = append(inputs, randInt(2*secs, secsLarger-1))  // future plural
+		inputs = append(inputs, -randInt(2*secs, secsLarger-1)) // past plural
 		blimit := secs - 1
 		if blimit > 1000 {
 			blimit = 1000
 		}
 		a := randInt(2, 6)
 		b := randInt(-blimit, blimit)
-		tests = append(tests, a*secs+b) // future plural antiapproximation
+		inputs = append(inputs, a*secs+b) // future plural antiapproximation
 		a = -randInt(2, 6)
 		b = randInt(-blimit, blimit)
-		tests = append(tests, a*secs+b) // past plural antiapproximation
+		inputs = append(inputs, a*secs+b) // past plural antiapproximation
 	}
 
-	rand.Shuffle(len(tests), func(i, j int) {
-		tests[i], tests[j] = tests[j], tests[i]
-	})
-
-	args := make([]string, len(tests))
-	var answer strings.Builder
-
-	for i, secs := range tests {
-		args[i] = strconv.Itoa(secs)
-		if i > 0 {
-			answer.WriteByte('\n')
-		}
-		answer.WriteString(formatDistance(secs))
+	tests := make([]test, len(inputs))
+	for i, inp := range inputs {
+		tests[i] = test{strconv.Itoa(secs), formatDistance(secs)}
 	}
 
-	return args, answer.String()
+	return outputTests(shuffle(tests))
 }
