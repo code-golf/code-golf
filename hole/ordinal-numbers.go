5ffeb3997304b8f7753a5ebd64cda89ab1c65d3c
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2022-03-26 09:18:03 +0100

    fix ordinal

--- hole/ordinal-numbers.go
+++ hole/ordinal-numbers.go
@@ -9,12 +9,12 @@ import (
 func ordinalNumbers() ([]string, string) {
 	const count = 1000
 	tests := make([]test, count)
-    
-    for i := range count {
+
+	for i := range count {
 		tests[i] = test{
-            strconv.Itoa(i),
-            strconv.Itoa(i) + pretty.Ordinal(i)
-        }
+			strconv.Itoa(i),
+			strconv.Itoa(i) + pretty.Ordinal(i),
+		}
 	}
 
 	return outputTests(shuffle(tests))
