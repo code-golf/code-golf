1c7c5fafe0bb73c228cc14480cd98b4adffbd0fb
James Raspass <jraspass@gmail.com>
2022-05-08 21:57:48 +0100

    Make "Jacobi Symbol" live

--- db/a-schema.sql
+++ db/a-schema.sql
@@ -26,18 +26,19 @@ CREATE TYPE hole AS ENUM (
     'css-colors', 'cubes', 'diamonds', 'divisors', 'emirp-numbers', 'emojify',
     'evil-numbers', 'fibonacci', 'fizz-buzz', 'foo-fizz-buzz-bar',
     'fractions', 'happy-numbers', 'happy-numbers-long', 'hexdump',
-    'intersection', 'isbn', 'kolakoski-constant', 'kolakoski-sequence',
-    'leap-years', 'levenshtein-distance', 'leyland-numbers', 'look-and-say',
-    'lucky-numbers', 'lucky-tickets', 'morse-decoder', 'morse-encoder',
-    'musical-chords', 'niven-numbers', 'number-spiral', 'odious-numbers',
-    'ordinal-numbers', 'pangram-grep', 'pascals-triangle',
-    'pernicious-numbers', 'poker', 'prime-numbers', 'prime-numbers-long',
-    'qr-decoder', 'quine', 'recamán', 'reverse-polish-notation',
-    'rock-paper-scissors-spock-lizard', 'roman-to-arabic', 'rule-110',
-    'seven-segment', 'sierpiński-triangle', 'smith-numbers',
-    'spelling-numbers', 'star-wars-opening-crawl', 'sudoku', 'sudoku-v2',
-    'ten-pin-bowling', 'time-distance', 'tongue-twisters', 'united-states',
-    'vampire-numbers', 'van-eck-sequence', 'λ', 'π', 'τ', 'φ', '√2', '𝑒'
+    'intersection', 'isbn', 'jacobi-symbol', 'kolakoski-constant',
+    'kolakoski-sequence', 'leap-years', 'levenshtein-distance',
+    'leyland-numbers', 'look-and-say', 'lucky-numbers', 'lucky-tickets',
+    'morse-decoder', 'morse-encoder', 'musical-chords', 'niven-numbers',
+    'number-spiral', 'odious-numbers', 'ordinal-numbers', 'pangram-grep',
+    'pascals-triangle', 'pernicious-numbers', 'poker', 'prime-numbers',
+    'prime-numbers-long', 'qr-decoder', 'quine', 'recamán',
+    'reverse-polish-notation', 'rock-paper-scissors-spock-lizard',
+    'roman-to-arabic', 'rule-110', 'seven-segment', 'sierpiński-triangle',
+    'smith-numbers', 'spelling-numbers', 'star-wars-opening-crawl', 'sudoku',
+    'sudoku-v2', 'ten-pin-bowling', 'time-distance', 'tongue-twisters',
+    'united-states', 'vampire-numbers', 'van-eck-sequence', 'λ', 'π', 'τ',
+    'φ', '√2', '𝑒'
 );
 
 CREATE TYPE keymap AS ENUM ('default', 'vim');
