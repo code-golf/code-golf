9347d3325cc466b55b1375510cc835baae96381e
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2022-05-11 00:36:45 +0200

    simplify

--- hole/jacobi-symbol.go
+++ hole/jacobi-symbol.go
@@ -28,11 +28,13 @@ func randomPrime(max int) int {
 
 func jacobiSymbol() ([]string, string) {
 	const mult = 9
-	type input struct {
-		a, n int
-	}
-	inputs := []input{}
 	tests := []test{}
+	addTest := func(a, n int) {
+		tests = append(tests, test{
+			strconv.Itoa(a) + " " + strconv.Itoa(n),
+			strconv.Itoa(big.Jacobi(big.NewInt(int64(a)), big.NewInt(int64(n)))),
+		})
+	}
 
 	var a, n int
 
@@ -40,39 +42,38 @@ func jacobiSymbol() ([]string, string) {
 		// Random numbers, varying size, a<n
 		n = randomOdd(1 << (53 - 2*i))
 		a = randomNatural(n)
-		inputs = append(inputs, input{a, n})
+		addTest(a, n)
 		// Random numbers, varying size, a>n
 		a = randomNatural(1 << (53 - 2*i))
 		n = randomOdd(a)
-		inputs = append(inputs, input{a, n})
+		addTest(a, n)
 		// Semiprime n
 		a = randomNatural(1 << 53)
-		n1 := randomPrime(1 << 26)
-		n2 := randomPrime(1 << 26)
-		inputs = append(inputs, input{a, n1 * n2})
+		n = randomPrime(1<<26) * randomPrime(1<<26)
+		addTest(a, n)
 		// Prime n
 		a = randomNatural(1 << 53)
 		n = randomPrime(1 << 53)
-		inputs = append(inputs, input{a, n})
+		addTest(a, n)
 		// Common multiple
 		common := randomOdd(1 << 26)
 		a = randomNatural(1 << 27)
 		n = randomOdd(1 << 27)
-		inputs = append(inputs, input{a * common, n * common})
+		addTest(a*common, n*common)
 		// a multiple of n
 		a = randomNatural(1 << 26)
 		n = randomOdd(1 << 27)
-		inputs = append(inputs, input{a * n, n})
+		addTest(a*n, n)
 		// n multiple of a
 		a = randomOdd(1 << 26)
 		n = randomOdd(1 << 27)
-		inputs = append(inputs, input{a, n * a})
+		addTest(a, n*a)
 		// a is small
 		n = randomOdd(1 << 53)
-		inputs = append(inputs, input{i, n})
+		addTest(i, n)
 		// n is small
 		a = randomNatural(1 << 53)
-		inputs = append(inputs, input{a, 2*i + 1})
+		addTest(a, 2*i+1)
 
 	}
 	// Different residue classes
@@ -80,15 +81,8 @@ func jacobiSymbol() ([]string, string) {
 		for j := 1; j < 8; j += 2 {
 			n = randomInClass(1<<53, 8, j)
 			a = randomInClass(1<<53, 4, i)
-			inputs = append(inputs, input{a, n})
+			addTest(a, n)
 		}
-	}
-
-	for _, inp := range inputs {
-		tests = append(tests, test{
-			strconv.Itoa(inp.a) + " " + strconv.Itoa(inp.n),
-			strconv.Itoa(big.Jacobi(big.NewInt(int64(inp.a)), big.NewInt(int64(inp.n)))),
-		})
 	}
 	return outputTests(shuffle(tests))
 }
