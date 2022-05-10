6a4c9f57f32188878283985fc9e3ad12f67669fa
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2022-05-10 23:42:27 +0200

    rename variables to match description, more test cases

--- hole/jacobi-symbol.go
+++ hole/jacobi-symbol.go
@@ -27,47 +27,67 @@ func randomPrime(max int) int {
 }
 
 func jacobiSymbol() ([]string, string) {
-	const mult = 20
+	const mult = 9
 	type input struct {
-		n, k int
+		a, n int
 	}
-	inputs := []input{{221, 7594623}}
+	inputs := []input{}
 	tests := []test{}
 
-	for i := 0; i < mult; i++ { // Random numbers, varying size
-		k := randomOdd(1 << (53 - 2*i))
-		n := randomNatural(k)
-		inputs = append(inputs, input{n, k})
-	}
-	for i := 0; i < mult; i++ { // Semiprime
-		k := randomOdd(1 << 53)
+	var a, n int
+
+	for i := 0; i < mult; i++ {
+		// Random numbers, varying size, a<n
+		n = randomOdd(1 << (53 - 2*i))
+		a = randomNatural(n)
+		inputs = append(inputs, input{a, n})
+		// Random numbers, varying size, a>n
+		a = randomNatural(1 << (53 - 2*i))
+		n = randomOdd(a)
+		inputs = append(inputs, input{a, n})
+		// Semiprime n
+		a = randomNatural(1 << 53)
 		n1 := randomPrime(1 << 26)
-		n2 := randomPrime(k / n1)
-		inputs = append(inputs, input{n1 * n2, k})
-	}
-	for i := 0; i < mult; i++ { // Prime
-		k := randomOdd(1 << 53)
-		n := randomPrime(k)
-		inputs = append(inputs, input{n, k})
-	}
-	for i := 0; i < mult; i++ { // Common multiple
+		n2 := randomPrime(1 << 26)
+		inputs = append(inputs, input{a, n1 * n2})
+		// Prime n
+		n = randomPrime(1 << 53)
+		a = randomOdd(1 << 53)
+		inputs = append(inputs, input{a, n})
+		// Common multiple
 		common := randomOdd(1 << 26)
-		k := randomOdd(1 << 27)
-		n := randomNatural(k)
-		inputs = append(inputs, input{n * common, k * common})
+		n = randomOdd(1 << 27)
+		a = randomNatural(1 << 27)
+		inputs = append(inputs, input{a * common, n * common})
+		// a multiple of n
+		n = randomOdd(1 << 27)
+		a = randomNatural(1 << 26)
+		inputs = append(inputs, input{a * n, n})
+		// n multiple of a
+		n = randomOdd(1 << 27)
+		a = randomNatural(1 << 26)
+		inputs = append(inputs, input{a, n * a})
+		// a is small
+		n = randomOdd(1 << 53)
+		inputs = append(inputs, input{i, n})
+		// n is small
+		a = randomNatural(1 << 53)
+		inputs = append(inputs, input{a, 2*i + 1})
+
 	}
-	for i := 0; i < 4; i++ { // Different residue classes
+	// Different residue classes
+	for i := 0; i < 4; i++ {
 		for j := 1; j < 8; j += 2 {
-			k := randomInClass(1<<53, 8, j)
-			n := randomInClass(k, 4, i)
-			inputs = append(inputs, input{n, k})
+			n := randomInClass(1<<53, 8, j)
+			a := randomInClass(1<<53, 4, i)
+			inputs = append(inputs, input{a, n})
 		}
 	}
 
 	for _, inp := range inputs {
 		tests = append(tests, test{
-			strconv.Itoa(inp.n) + " " + strconv.Itoa(inp.k),
-			strconv.Itoa(big.Jacobi(big.NewInt(int64(inp.n)), big.NewInt(int64(inp.k)))),
+			strconv.Itoa(inp.a) + " " + strconv.Itoa(inp.n),
+			strconv.Itoa(big.Jacobi(big.NewInt(int64(inp.a)), big.NewInt(int64(inp.n)))),
 		})
 	}
 	return outputTests(shuffle(tests))
