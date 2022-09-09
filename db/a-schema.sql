f7c0d8a77e637e049e7245a676057e246223f0a0
James Raspass <jraspass@gmail.com>
2022-09-09 13:16:46 +0100

    Add "Wren" lang

--- db/a-schema.sql
+++ db/a-schema.sql
@@ -50,7 +50,7 @@ CREATE TYPE lang AS ENUM (
     'golfscript', 'haskell', 'hexagony', 'j', 'java', 'javascript', 'julia',
     'k', 'lisp', 'lua', 'nim', 'pascal', 'perl', 'php', 'powershell',
     'prolog', 'python', 'raku', 'ruby', 'rust', 'sed', 'sql', 'swift', 'v',
-    'viml', 'zig'
+    'viml', 'wren', 'zig'
 );
 
 CREATE TYPE medal AS ENUM ('diamond', 'gold', 'silver', 'bronze');
