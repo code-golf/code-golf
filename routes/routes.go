0a8d5cd4e42f6a5010b338273e005d8131187987
Jared Hughes <jahughes241@gmail.com>
2022-09-25 18:46:40 -0700

    Remove extra /rankings

--- routes/routes.go
+++ routes/routes.go
@@ -71,7 +71,7 @@ func Router(db *sql.DB) http.Handler {
 		r.Get("/cheevos", golferCheevosGET)
 		r.Get("/holes", golferHolesGET)
 		r.Get("/holes/{scoring}", golferHolesGET)
-		r.Get("/rankings/{display:rankings|points}/{scope:lang|overall}/{scoring:bytes|chars}", golferRankingsGET)
+		r.Get("/{display:rankings|points}/{scope:lang|overall}/{scoring:bytes|chars}", golferRankingsGET)
 		r.Get("/{hole}/{lang}/{scoring}", golferSolutionGET)
 		// r.Post("/{hole}/{lang}/{scoring}", golferSolutionPOST)
 	})
