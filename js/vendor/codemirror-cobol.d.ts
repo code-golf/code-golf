223a850dbe098e0818109bbd71d1dcda630fc222
Brian Collins <bri.l.collins@gmail.com>
2022-10-07 23:21:11 -0400

    Fixed COBOL syntax highlighter. Restored old Raku syntax highlighter.
    For COBOL, we use the free source format, instead of the fixed source format.
    I merged our old highlighters with the new style for legacy CM modes.
    Also added a couple of Raku operators that weren't listed: %% +^ ~^ +| +^ ~| ~^ ?| ?^ ÷ %% +& +< +> ~& ~< ~>
    Fixes #691.

---
+++ js/vendor/codemirror-cobol.d.ts
@@ -0,0 +1,2 @@
+import {StreamParser} from '@codemirror/language';
+export declare const cobol: StreamParser<unknown>;
---
+++ js/vendor/codemirror-raku.d.ts
@@ -0,0 +1,2 @@
+import {StreamParser} from '@codemirror/language';
+export declare const raku: StreamParser<unknown>;
