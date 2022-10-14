769ab9e4a446bfdceeb74a170abf195834d34b52
Brian Collins <bri.l.collins@gmail.com>
2022-10-14 14:37:52 -0400

    Restore C multiline string support.

--- js/vendor/codemirror-clike.d.ts
+++ js/vendor/codemirror-clike.d.ts
@@ -1,3 +1,4 @@
 import {StreamParser} from '@codemirror/language';
 export declare const c: StreamParser<unknown>;
 export declare const csharp: StreamParser<unknown>;
+export declare const dart: StreamParser<unknown>;
