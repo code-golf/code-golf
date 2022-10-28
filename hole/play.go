3a9655fd76eb10b59b289f81529ec9c0e6d81410
Dan Vargas <danvargas46@gmail.com>
2022-10-27 21:32:17 -0600

    add awk lang

--- hole/play.go
+++ hole/play.go
@@ -197,6 +197,8 @@ func play(ctx context.Context, holeID, langID, code string, score *Scorecard) {
 
 		cmd.Args = []string{"/usr/bin/defasm", "--size-out=3", "-w", "-r"}
 		cmd.ExtraFiles = []*os.File{asmBytesWrite}
+	case "awk":
+		cmd.Args = []string{"/usr/bin/gawk", "-v", "RS=\\0", code}
 	case "bash":
 		cmd.Args = []string{"/usr/bin/bash", "-s", "-"}
 	case "brainfuck":
@@ -263,7 +265,8 @@ func play(ctx context.Context, holeID, langID, code string, score *Scorecard) {
 
 	// Args
 	switch langID {
-	case "brainfuck", "fish":
+	case "awk", "brainfuck", "fish":
+		// Hole args passed through stdin for these langs separated by a null byte
 		args := ""
 		for _, arg := range score.Args {
 			args += arg + "\x00"
@@ -279,8 +282,8 @@ func play(ctx context.Context, holeID, langID, code string, score *Scorecard) {
 
 	// Code
 	switch langID {
-	case "brainfuck", "elixir", "fish", "golfscript", "javascript", "perl", "sed":
-		// For these code is passed as an argument above.
+	case "awk", "brainfuck", "elixir", "fish", "golfscript", "javascript", "perl", "sed":
+		// For these langs, code is passed as an argument above.
 	case "k":
 		code = preprocessKCode(holeID, code)
 		cmd.Stdin = strings.NewReader(code)
