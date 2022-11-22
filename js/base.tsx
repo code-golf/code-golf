3b4bbe9917b23aa49051d881ee1382cf66d91c70
Brian Collins <bri.l.collins@gmail.com>
2022-11-22 17:26:22 -0500

    Allow sorting by points for specific languages on the front page.
    Fixes #502.

--- js/base.tsx
+++ js/base.tsx
@@ -15,6 +15,10 @@ for (const a of $$<HTMLAnchorElement>('.log-in')) {
     a.href = url.href;
 }
 
+// Wire up the language selector on /
+$('#form-config')?.addEventListener('change', (e: Event) =>
+    (e.target as HTMLFormElement).form.submit());
+
 // Wire up mobile form navigation.
 $('#form-nav')?.addEventListener('change',
     (e: Event) => location.href =
