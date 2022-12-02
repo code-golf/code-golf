9c4722a03d301d081d91833acf5b4c7ae65a1898
James Raspass <jraspass@gmail.com>
2022-12-02 23:42:57 +0000

    Show votes on experimental holes
    I'll use this to pick the next hole to go live so get voting!
    
    Closes #803

--- routes/ideas.go
+++ routes/ideas.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"net/http"
+	"sort"
 	"strings"
 
 	"github.com/code-golf/code-golf/config"
@@ -11,14 +12,18 @@ import (
 // GET /ideas
 func ideasGET(w http.ResponseWriter, r *http.Request) {
 	type idea struct {
+		Hole                     *config.Hole
 		ID, ThumbsDown, ThumbsUp int
 		Title                    string
 	}
 
 	data := struct {
-		Holes            []*config.Hole
-		Ideas, Languages []idea
-	}{Holes: config.ExpHoleList}
+		Holes, Ideas, Langs []idea
+	}{Holes: make([]idea, len(config.ExpHoleList))}
+
+	for i, hole := range config.ExpHoleList {
+		data.Holes[i] = idea{Hole: hole, ID: hole.Experiment, Title: hole.Name}
+	}
 
 	rows, err := session.Database(r).Query(
 		"SELECT * FROM ideas ORDER BY thumbs_up - thumbs_down DESC, title")
@@ -27,14 +32,23 @@ func ideasGET(w http.ResponseWriter, r *http.Request) {
 	}
 	defer rows.Close()
 
+rows:
 	for rows.Next() {
 		var i idea
-
 		if err := rows.Scan(&i.ID, &i.ThumbsDown, &i.ThumbsUp, &i.Title); err != nil {
 			panic(err)
 		}
+
+		for j, hole := range data.Holes {
+			if hole.ID == i.ID {
+				data.Holes[j].ThumbsDown = i.ThumbsDown
+				data.Holes[j].ThumbsUp = i.ThumbsUp
+				continue rows
+			}
+		}
+
 		if strings.Contains(strings.ToLower(i.Title), "lang") {
-			data.Languages = append(data.Languages, i)
+			data.Langs = append(data.Langs, i)
 		} else {
 			data.Ideas = append(data.Ideas, i)
 		}
@@ -43,6 +57,11 @@ func ideasGET(w http.ResponseWriter, r *http.Request) {
 	if err := rows.Err(); err != nil {
 		panic(err)
 	}
+
+	sort.SliceStable(data.Holes, func(i, j int) bool {
+		return data.Holes[i].ThumbsUp-data.Holes[i].ThumbsDown >
+			data.Holes[j].ThumbsUp-data.Holes[j].ThumbsDown
+	})
 
 	render(w, r, "ideas", data, "Ideas")
 }
