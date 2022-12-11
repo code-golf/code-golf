b88fb5f8bfa412c8d6c42897a45b4e8d04d30b48
btnlq <31797752+btnlq@users.noreply.github.com>
2022-12-11 13:47:24 +0600

    Refactor QR

--- hole/qr.go
+++ hole/qr.go
@@ -16,36 +16,36 @@ func randStr(len int) string {
 	return string(buf)
 }
 
-type matrix = [][]bool
+type matrix [][]bool
 
-func get(qr matrix, i, j int) byte {
+func (qr matrix) get(i, j int) byte {
 	if qr[i][j] != (((i + j) & 1) == 0) {
 		return 1
 	}
 	return 0
 }
 
-func get2(qr matrix, i, j int) byte {
-	return 2*get(qr, i, j) + get(qr, i, j-1)
+func (qr matrix) get2(i, j int) byte {
+	return 2*qr.get(i, j) + qr.get(i, j-1)
 }
 
-func get4(qr matrix, i, j, dir int) byte {
-	return 4*get2(qr, i, j) + get2(qr, i+dir, j)
+func (qr matrix) get4(i, j, dir int) byte {
+	return 4*qr.get2(i, j) + qr.get2(i+dir, j)
 }
 
-func get8(qr matrix, i, j, dir int) byte {
-	return 16*get4(qr, i, j, dir) + get4(qr, i+2*dir, j, dir)
+func (qr matrix) get8(i, j, dir int) byte {
+	return 16*qr.get4(i, j, dir) + qr.get4(i+2*dir, j, dir)
 }
 
-func getErrorCorrectionBlocks(qr matrix) []byte {
+func (qr matrix) getErrorCorrectionBlocks() []byte {
 	return []byte{
-		get8(qr, 9, 10, 1),
-		get8(qr, 13, 10, 1),
-		get8(qr, 17, 10, 1),
-		get8(qr, 12, 8, -1),
-		get8(qr, 9, 5, 1),
-		get8(qr, 12, 3, -1),
-		get8(qr, 9, 1, 1),
+		qr.get8(9, 10, 1),
+		qr.get8(13, 10, 1),
+		qr.get8(17, 10, 1),
+		qr.get8(12, 8, -1),
+		qr.get8(9, 5, 1),
+		qr.get8(12, 3, -1),
+		qr.get8(9, 1, 1),
 	}
 }
 
@@ -55,16 +55,16 @@ func genQr(content string) matrix {
 	return qr.Bitmap()
 }
 
-func qrIsStandard(qr matrix) bool {
+func (qr matrix) isStandard() bool {
 	if !(qr[8][2] && !qr[8][3] && qr[8][4]) { // mask pattern: (i+j)%2 == 0
 		return false
 	}
 
-	if get4(qr, 20, 20, -1) != 4 { // mode indicator: byte encoding
+	if qr.get4(20, 20, -1) != 4 { // mode indicator: byte encoding
 		return false
 	}
 
-	if get8(qr, 18, 20, -1) != 17 { // message length: 17
+	if qr.get8(18, 20, -1) != 17 { // message length: 17
 		return false
 	}
 
@@ -76,13 +76,13 @@ func getStandardQr() (content string, qr matrix) {
 		content = randStr(17)
 		qr = genQr(content)
 
-		if qrIsStandard(qr) {
+		if qr.isStandard() {
 			return
 		}
 	}
 }
 
-func qrToString(qr matrix, trimRight bool) string {
+func (qr matrix) toString(trimRight bool) string {
 	var buf strings.Builder
 	for i, row := range qr {
 		if i > 0 {
@@ -110,14 +110,14 @@ func qrToString(qr matrix, trimRight bool) string {
 
 func qr(decoder bool) []Scorecard {
 	content, qr := getStandardQr()
-	qrString := qrToString(qr, !decoder)
+	qrString := qr.toString(!decoder)
 
 	if decoder {
 		return []Scorecard{{Args: []string{qrString}, Answer: content}}
 	}
 
 	return []Scorecard{{
-		Args:   []string{content + " " + hex.EncodeToString(getErrorCorrectionBlocks(qr))},
+		Args:   []string{content + " " + hex.EncodeToString(qr.getErrorCorrectionBlocks())},
 		Answer: qrString,
 	}}
 }
