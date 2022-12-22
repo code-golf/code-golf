07131c1620a6babfa5b5f430040c460f260d5c24
James Raspass <jraspass@gmail.com>
2022-12-22 01:41:32 +0000

    Update feeds

--- routes/feed.go
+++ routes/feed.go
@@ -27,6 +27,7 @@ func init() {
 		sha, created, id string
 		hole             bool
 	}{
+		{"70e87ab", "2022-12-22 01:40:23", "game-of-life", true},
 		{"4dad4e3", "2022-12-11 22:52:07", "r", false},
 		{"b574dc5", "2022-11-19 22:40:19", "abundant-numbers-long", true},
 		{"b574dc5", "2022-11-19 22:40:19", "inventory-sequence", true},
