e1101049f50c4f0f5f2fc59f3e6960cdbabbc371
James Raspass <jraspass@gmail.com>
2023-01-21 23:56:56 +0000

    Use sqlx to simplify routes.ideasGET

--- routes/ideas.go
+++ routes/ideas.go
@@ -25,20 +25,16 @@ func ideasGET(w http.ResponseWriter, r *http.Request) {
 		data.Holes[i] = idea{Hole: hole, ID: hole.Experiment, Title: hole.Name}
 	}
 
-	rows, err := session.Database(r).Query(
-		"SELECT * FROM ideas ORDER BY thumbs_up - thumbs_down DESC, title")
-	if err != nil {
+	var ideas []idea
+	if err := session.Database(r).Select(
+		&ideas,
+		"SELECT * FROM ideas ORDER BY thumbs_up - thumbs_down DESC, title",
+	); err != nil {
 		panic(err)
 	}
-	defer rows.Close()
 
 rows:
-	for rows.Next() {
-		var i idea
-		if err := rows.Scan(&i.ID, &i.ThumbsDown, &i.ThumbsUp, &i.Title); err != nil {
-			panic(err)
-		}
-
+	for _, i := range ideas {
 		for j, hole := range data.Holes {
 			if hole.ID == i.ID {
 				data.Holes[j].ThumbsDown = i.ThumbsDown
@@ -54,10 +50,6 @@ rows:
 		} else {
 			data.Ideas = append(data.Ideas, i)
 		}
-	}
-
-	if err := rows.Err(); err != nil {
-		panic(err)
 	}
 
 	sort.SliceStable(data.Holes, func(i, j int) bool {
