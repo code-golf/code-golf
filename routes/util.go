code-golf/code-golf

commit caf617cd19407cd6b030afbf53b3b5a71ca43a21

The great routing refactor
 - Move the routing into routes.
 - Make the routes unexported and include HTTP method in name.
 - Add middleware to inplicitly add pretty bodies to 4xx & 5xx.

routes/util.go
@@ -1,0 +1,40 @@
+package routes
+
+import (
+	"encoding/base64"
+	"math/rand"
+	"net/http"
+	"net/url"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func cookie(r *http.Request, name string) (value string) {
+	if c, _ := r.Cookie(name); c != nil {
+		value = c.Value
+	}
+	return
+}
+
+// Generate a random string for CSP & OAuth state.
+//
+// The generated value SHOULD be at least 128 bits long (before encoding), and
+// SHOULD be generated via a cryptographically secure random number generator.
+// https://w3c.github.io/webappsec-csp/#security-nonces
+func nonce() string {
+	nonce := make([]byte, 16)
+	if _, err := rand.Read(nonce); err != nil {
+		panic(err)
+	}
+
+	return base64.StdEncoding.EncodeToString(nonce)
+}
+
+func param(r *http.Request, key string) string {
+	value, _ := url.QueryUnescape(chi.URLParam(r, key))
+	return value
+}
+
+func redir(url string) http.HandlerFunc {
+	return http.RedirectHandler(url, http.StatusPermanentRedirect).ServeHTTP
+}

main.go
@@ -11,9 +11,7 @@ import (
 
 	"github.com/code-golf/code-golf/discord"
 	"github.com/code-golf/code-golf/github"
-	"github.com/code-golf/code-golf/middleware"
 	"github.com/code-golf/code-golf/routes"
-	"github.com/go-chi/chi/v5"
 	_ "github.com/lib/pq"
 	"golang.org/x/crypto/acme/autocert"
 )
@@ -28,107 +26,6 @@ func main() {
 		panic(err)
 	}
 
-	r := chi.NewRouter()
-
-	r.Use(
-		middleware.Logger,
-		middleware.Recoverer,
-		middleware.RedirectHost,
-		middleware.Static,
-		middleware.RedirectSlashes,
-		middleware.Compress(5),
-		// middleware.Downtime,
-		middleware.Database(db),
-		middleware.Golfer,
-	)
-
-	r.NotFound(routes.NotFound)
-
-	r.Get("/", routes.Index)
-	r.Get("/{hole}", routes.Hole)
-	r.Get("/ng/{hole}", routes.HoleNG)
-	r.Get("/about", routes.About)
-	r.With(middleware.AdminArea).Route("/admin", func(r chi.Router) {
-		r.Get("/", routes.Admin)
-		r.Get("/solutions", routes.AdminSolutions)
-		r.Get("/solutions/run", routes.AdminSolutionsRun)
-	})
-	r.With(middleware.API).Route("/api", func(r chi.Router) {
-		r.NotFound(routes.APINotFound)
-
-		r.Get("/", routes.API)
-		r.Get("/cheevos", routes.APICheevos)
-		r.Get("/cheevos/{cheevo}", routes.APICheevo)
-		r.Get("/langs", routes.APILangs)
-		r.Get("/langs/{lang}", routes.APILang)
-		r.Get("/panic", routes.APIPanic)
-		r.Get("/suggestions/golfers", routes.APISuggestionsGolfers)
-	})
-	r.Get("/callback", routes.Callback)
-	r.Get("/callback/dev", routes.CallbackDev)
-	r.Get("/feeds", routes.Feeds)
-	r.Get("/feeds/{feed:atom|json|rss}", routes.Feed)
-	r.With(middleware.GolferArea).Route("/golfer", func(r chi.Router) {
-		r.Post("/cancel-delete", routes.GolferCancelDelete)
-		r.Get("/connect/{connection}", routes.GolferConnect)
-		r.Post("/delete", routes.GolferDelete)
-		r.Get("/disconnect/{connection}", routes.GolferDisconnect)
-		r.Get("/export", routes.GolferExport)
-		r.Get("/settings", routes.GolferSettings)
-		r.Post("/settings", routes.GolferSettingsPost)
-	})
-	r.With(middleware.GolferInfo).Route("/golfers/{name}", func(r chi.Router) {
-		r.Get("/", routes.GolferWall)
-		r.Post("/{action:follow|unfollow}", routes.GolferAction)
-		r.Get("/cheevos", routes.GolferCheevos)
-		r.Get("/holes", routes.GolferHoles)
-		r.Get("/holes/{scoring}", routes.GolferHoles)
-		r.Get("/{hole}/{lang}/{scoring}", routes.GolferSolution)
-		// r.Post("/{hole}/{lang}/{scoring}", routes.GolferSolutionPost)
-	})
-	r.Get("/healthz", routes.Healthz)
-	r.Get("/ideas", routes.Ideas)
-	r.Get("/log-out", routes.LogOut)
-	r.Get("/random", routes.Random)
-	r.Get("/ng/random", routes.NGRandom)
-	r.Route("/rankings", func(r chi.Router) {
-		// Redirect some old URLs that got out.
-		r.Get("/", redir("/rankings/holes/all/all/bytes"))
-		r.Get("/holes", redir("/rankings/holes/all/all/bytes"))
-		r.Get("/holes/all/all/all", redir("/rankings/holes/all/all/bytes"))
-		r.Get("/langs/bytes", redir("/rankings/langs/all/bytes"))
-		r.Get("/langs/chars", redir("/rankings/langs/all/chars"))
-		r.Get("/medals", redir("/rankings/medals/all/all/all"))
-
-		r.Get("/cheevos", routes.RankingsCheevos)
-		r.Get("/cheevos/all", redir("/rankings/cheevos"))
-		r.Get("/cheevos/{cheevo}", routes.RankingsCheevos)
-
-		r.Get("/holes/{hole}/{lang}/{scoring}", routes.RankingsHoles)
-		r.Get("/recent-holes/{lang}/{scoring}", routes.RankingsHoles)
-
-		r.Get("/medals/{hole}/{lang}/{scoring}", routes.RankingsMedals)
-
-		r.Get("/langs/{lang}/{scoring}", routes.RankingsLangs)
-		r.Get("/solutions", routes.RankingsSolutions)
-	})
-	r.Route("/recent", func(r chi.Router) {
-		r.Get("/", redir("/recent/solutions/all/all/bytes"))
-		r.Get("/{lang}", routes.Recent)
-
-		r.Get("/golfers", routes.RecentGolfers)
-		r.Get("/solutions/{hole}/{lang}/{scoring}", routes.RecentSolutions)
-	})
-	r.Get("/scores/{hole}/{lang}", routes.Scores)
-	r.Get("/scores/{hole}/{lang}/all", routes.ScoresAll)
-	r.Get("/scores/{hole}/{lang}/{scoring}", routes.Scores)
-	r.Get("/scores/{hole}/{lang}/{scoring}/{page}", routes.Scores)
-	r.Get("/scores/{hole}/{lang}/{scoring}/mini", routes.ScoresMini)
-	r.Get("/sitemap.xml", routes.Sitemap)
-	r.Post("/solution", routes.Solution)
-	r.Get("/stats", routes.Stats)
-	r.Get("/users/{name}", routes.User)
-
 	certManager := autocert.Manager{
 		Cache:  autocert.DirCache("certs"),
 		Prompt: autocert.AcceptTOS,
@@ -140,7 +37,7 @@ func main() {
 
 	server := http.Server{
 		Addr:    ":1443",
-		Handler: r,
+		Handler: routes.Router(db),
 		TLSConfig: &tls.Config{
 			CipherSuites: []uint16{
 				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
@@ -239,8 +136,4 @@ func main() {
 
 	// Serve HTTPS.
 	panic(server.ListenAndServeTLS(crt, key))
-}
-
-func redir(url string) http.HandlerFunc {
-	return http.RedirectHandler(url, http.StatusPermanentRedirect).ServeHTTP
 }

middleware/admin_area.go
@@ -3,7 +3,6 @@ package middleware
 import (
 	"net/http"
 
-	"github.com/code-golf/code-golf/routes"
 	"github.com/code-golf/code-golf/session"
 )
 
@@ -13,7 +12,7 @@ func AdminArea(next http.Handler) http.Handler {
 		if golfer := session.Golfer(r); golfer != nil && golfer.Admin {
 			next.ServeHTTP(w, r)
 		} else {
-			routes.Forbidden(w, r)
+			w.WriteHeader(http.StatusForbidden)
 		}
 	})
 }

middleware/golfer_area.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 
 	"github.com/code-golf/code-golf/golfer"
-	"github.com/code-golf/code-golf/routes"
 	"github.com/code-golf/code-golf/session"
 )
 
@@ -15,7 +14,7 @@ func GolferArea(next http.Handler) http.Handler {
 			info := golfer.GetInfo(session.Database(r), g.Name)
 			next.ServeHTTP(w, session.Set(r, "golfer-info", info))
 		} else {
-			routes.Forbidden(w, r)
+			w.WriteHeader(http.StatusForbidden)
 		}
 	})
 }

middleware/golfer_info.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 
 	"github.com/code-golf/code-golf/golfer"
-	"github.com/code-golf/code-golf/routes"
 	"github.com/code-golf/code-golf/session"
 	"github.com/go-chi/chi/v5"
 )
@@ -17,7 +16,7 @@ func GolferInfo(next http.Handler) http.Handler {
 
 		switch {
 		case info == nil:
-			routes.NotFound(w, r)
+			w.WriteHeader(http.StatusNotFound)
 		case info.Name != name:
 			// TODO Handle /holes suffix.
 			http.Redirect(w, r, "/golfers/"+info.Name, http.StatusPermanentRedirect)

middleware/logger.go
@@ -5,8 +5,6 @@ import (
 	"net/http"
 	"os"
 	"time"
-
-	"github.com/go-chi/chi/v5/middleware"
 )
 
 const (
@@ -38,7 +36,7 @@ func Logger(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+		ww := NewWrapResponseWriter(w, r.ProtoMajor)
 
 		defer func() {
 			log.Printf(

middleware/middleware.go
@@ -4,8 +4,9 @@ import "github.com/go-chi/chi/v5/middleware"
 
 // Re-export the upstream middleware we use via this namespace.
 var (
-	Compress        = middleware.Compress
-	Recoverer       = middleware.Recoverer
-	RedirectSlashes = middleware.RedirectSlashes
-	WithValue       = middleware.WithValue
+	Compress              = middleware.Compress
+	NewWrapResponseWriter = middleware.NewWrapResponseWriter
+	Recoverer             = middleware.Recoverer
+	RedirectSlashes       = middleware.RedirectSlashes
+	WithValue             = middleware.WithValue
 )

routes/about.go
@@ -7,8 +7,8 @@ import (
 	"github.com/code-golf/code-golf/session"
 )
 
-// About serves GET /about
-func About(w http.ResponseWriter, r *http.Request) {
+// GET /about
+func aboutGET(w http.ResponseWriter, r *http.Request) {
 	data := struct {
 		Cheevo *config.Cheevo
 		Langs  []*config.Lang

commit 49855e52c5f5256d53d30679840077b19c0a7e1a

nonce should be crypto random!
This is actually a regression from the refactor in caf617c

routes/util.go
@@ -1,8 +1,8 @@
 package routes
 
 import (
+	"crypto/rand"
 	"encoding/base64"
-	"math/rand"
 	"net/http"
 	"net/url"
 	"regexp"