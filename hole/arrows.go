fa3bb537b1d9a4a010c1fa92f53b25362832c625
James Raspass <jraspass@gmail.com>
2023-02-27 07:00:10 +0000

    Arrows: combine the arrow walking code

--- hole/arrows.go
+++ hole/arrows.go
@@ -3,7 +3,6 @@ package hole
 import (
 	"fmt"
 	"math/rand"
-	"strings"
 )
 
 var arrowMap = map[string][2]int8{
@@ -32,7 +31,6 @@ var arrowMapDownAndRight = [...]string{"→", "↓", "↘", "↳", "⇒", "⇓",
 
 func arrows() []Scorecard {
 	args := make([]string, 0, 3*len(arrowMap))
-	pos := [2]int8{}
 
 	// 1-3 of each arrow.
 	for arrow := range arrowMap {
@@ -41,25 +39,11 @@ func arrows() []Scorecard {
 		}
 	}
 
-	// Calculate the outs from the args.
-	outs := make([]string, len(args))
-	for i, arrow := range shuffle(args) {
-		coord := arrowMap[arrow]
-		pos[0] += coord[0]
-		pos[1] += coord[1]
-
-		outs[i] = fmt.Sprint(pos[0], pos[1])
-	}
-
 	// Additional test to force all Cartesian quadrants
 	argsDL := make([]string, 0, 4*len(arrowMapDownAndLeft))
-	posDL := [2]int8{}
 	argsUL := make([]string, 0, 4*len(arrowMapUpAndLeft))
-	posUL := [2]int8{}
 	argsDR := make([]string, 0, 4*len(arrowMapDownAndRight))
-	posDR := [2]int8{}
 	argsUR := make([]string, 0, 4*len(arrowMapUpAndRight))
-	posUR := [2]int8{}
 
 	timesDL := 2 + rand.Intn(2)
 	timesUL := 5 - timesDL
@@ -88,48 +72,20 @@ func arrows() []Scorecard {
 		}
 	}
 
-	// Calculate the outs from the args.
-	outsDL := make([]string, len(argsDL))
-	for i, arrow := range shuffle(argsDL) {
-		coord := arrowMap[arrow]
-		posDL[0] += coord[0]
-		posDL[1] += coord[1]
+	var testRuns [][]test
+	for _, arrows := range [][]string{args, argsDL, argsUL, argsDR, argsUR} {
+		tests := make([]test, len(arrows))
+		testRuns = append(testRuns, tests)
 
-		outsDL[i] = fmt.Sprint(posDL[0], posDL[1])
+		var pos [2]int8
+		for i, arrow := range shuffle(arrows) {
+			coord := arrowMap[arrow]
+			pos[0] += coord[0]
+			pos[1] += coord[1]
+
+			tests[i] = test{arrow, fmt.Sprint(pos[0], pos[1])}
+		}
 	}
 
-	outsUL := make([]string, len(argsUL))
-	for i, arrow := range shuffle(argsUL) {
-		coord := arrowMap[arrow]
-		posUL[0] += coord[0]
-		posUL[1] += coord[1]
-
-		outsUL[i] = fmt.Sprint(posUL[0], posUL[1])
-	}
-
-	outsDR := make([]string, len(argsDR))
-	for i, arrow := range shuffle(argsDR) {
-		coord := arrowMap[arrow]
-		posDR[0] += coord[0]
-		posDR[1] += coord[1]
-
-		outsDR[i] = fmt.Sprint(posDR[0], posDR[1])
-	}
-
-	outsUR := make([]string, len(argsUR))
-	for i, arrow := range shuffle(argsUR) {
-		coord := arrowMap[arrow]
-		posUR[0] += coord[0]
-		posUR[1] += coord[1]
-
-		outsUR[i] = fmt.Sprint(posUR[0], posUR[1])
-	}
-
-	return []Scorecard{
-		{Args: args, Answer: strings.Join(outs, "\n")},
-		{Args: argsDL, Answer: strings.Join(outsDL, "\n")},
-		{Args: argsUL, Answer: strings.Join(outsUL, "\n")},
-		{Args: argsDR, Answer: strings.Join(outsDR, "\n")},
-		{Args: argsUR, Answer: strings.Join(outsUR, "\n")},
-	}
+	return outputTests(testRuns...)
 }
