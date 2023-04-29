ac932fff0f09dde83b08d7f46c49d8f8674067eb
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2023-04-29 16:35:16 +0200

    keep only the first section of FEN in inputs

--- hole/forsyth-edwards-notation.go
+++ hole/forsyth-edwards-notation.go
@@ -9,11 +9,11 @@ var pieceMap = map[rune]rune{
 
 func forsythEdwardsNotation() []Scorecard {
 	args := shuffle([]string{
-		"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-		"rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1",
-		"K7/2bb4/1bbb4/3k4/8/8/8/8 w - - 69 272",
-		"1n1Rkb1r/p4ppp/4q3/4p1B1/4P3/8/PPP2PPP/2K5 b k - 1 17",
-		strings.Join(shuffle([]string{"2k1N3", "7r", "8", "2B5", "3rb3", "4n3", "1R6", "4K3"}), "/") + " - - 0 50",
+		"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR",
+		"rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR",
+		"K7/2bb4/1bbb4/3k4/8/8/8/8",
+		"1n1Rkb1r/p4ppp/4q3/4p1B1/4P3/8/PPP2PPP/2K5",
+		strings.Join(shuffle([]string{"2k1N3", "7r", "8", "2B5", "3rb3", "4n3", "1R6", "4K3"}), "/"),
 	})
 
 	var buf strings.Builder
@@ -22,9 +22,7 @@ func forsythEdwardsNotation() []Scorecard {
 			buf.WriteString("\n\n")
 		}
 		for i, c := range arg {
-			if c == ' ' {
-				break
-			} else if c == '/' {
+			if c == '/' {
 				buf.WriteByte('\n')
 			} else if '1' <= c && c <= '8' {
 				if arg[i+1] >= 'A' {
