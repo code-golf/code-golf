3d12b73b6345b93c9b6656c1828528bf7cd07fa9
KG <kgscience@hotmail.com>
2023-05-31 23:07:20 -0500

    But do it correctly

--- hole/medal-tally.go
+++ hole/medal-tally.go
@@ -11,7 +11,7 @@ func medalTally() []Scorecard {
 
 	for size := 3; size <= 10; size++ {
 		// 3-10 golds
-		for j := 3, j <= 10; j++ {
+		for j := 3, j <= size; j++ {
 			for i := 0; i <= size - j; i++ {
 				tests = append(tests, medalTallyTest(size, j, 0, 0, i))
 			}
