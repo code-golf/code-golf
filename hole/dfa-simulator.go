990fdf73cef264404880e2083d08bf9533d593c6
Stefan Gimmillaro <1554858+stefangimmillaro@users.noreply.github.com>
2023-09-15 07:42:56 -0700

    Add generated cases
    Co-Authored-By: msbranicky <128628609+msbranicky@users.noreply.github.com>

--- hole/dfa-simulator.go
+++ hole/dfa-simulator.go
@@ -2,8 +2,106 @@ package hole
 
 import (
 	"math/rand"
+	"strconv"
 	"strings"
 )
+
+type DFAState struct {
+	name     string
+	accepted bool
+}
+
+type DFAStateTransition struct {
+	input     string
+	stateName string
+}
+
+func solve(g string) string {
+	var startState DFAState
+	stateMap := map[DFAStateTransition]DFAState{}
+	nameToState := map[string]DFAState{}
+
+	data := strings.Split(g, "\n")
+	alphabet := strings.Split(data[0][4:], " ")
+
+	stateRows := data[1 : len(data)-1]
+
+	for _, state := range stateRows {
+		stateName := string(state[2])
+		newState := DFAState{stateName, state[1] == 'F'}
+		nameToState[stateName] = newState
+
+		if state[0] == '>' {
+			startState = newState
+		}
+	}
+
+	for _, state := range stateRows {
+		transitions := strings.Split(state[2:], " ")
+		stateName := transitions[0]
+
+		for i, transition := range transitions[1:] {
+			alphabetWord := string(alphabet[i])
+			newStateTransition := DFAStateTransition{alphabetWord, stateName}
+			stateMap[newStateTransition] = nameToState[transition]
+		}
+	}
+
+	currentState := startState
+	for _, testString := range data[len(data)-1] {
+		if string(testString) != "\"" {
+			currentState = stateMap[DFAStateTransition{string(testString), currentState.name}]
+		}
+	}
+	if currentState.accepted {
+		return currentState.name + " Accept"
+	}
+
+	return currentState.name + " Reject"
+}
+
+func generate() string {
+	alphabet := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	alphabetLength := rand.Intn(36)
+	alphabet = alphabet[:alphabetLength]
+
+	inputDFA := "    " + strings.Join(alphabet, " ") + "\n"
+
+	stateLength := rand.Intn(10) + 1
+	startState := rand.Intn(stateLength)
+	for i := 0; i < stateLength; i++ {
+		stateRow := ""
+
+		if i == startState {
+			stateRow += ">"
+		} else {
+			stateRow += " "
+		}
+
+		if rand.Intn(2) == 0 {
+			stateRow += "F"
+		} else {
+			stateRow += " "
+		}
+
+		stateRow += strconv.Itoa(i) + " "
+
+		transitions := []string{}
+		for j := 0; j < alphabetLength; j++ {
+			transitions = append(transitions, strconv.Itoa(rand.Intn(stateLength)))
+		}
+		stateRow += strings.Join(transitions, " ") + "\n"
+		inputDFA += stateRow
+
+	}
+	inputDFA += "\""
+	for i := 0; i < rand.Intn(2*alphabetLength); i++ {
+		inputDFA += alphabet[rand.Intn(alphabetLength)]
+	}
+	inputDFA += "\""
+
+	return inputDFA
+}
 
 func dfaSimulator() []Run {
 	args := []string{
@@ -23,11 +121,11 @@ func dfaSimulator() []Run {
 		"    a b c d e f g h i j k l m n o p q r s t u v w x y z\n>F0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1\n  1 1 1 1 1 1 1 1 1 1 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0\n\"bananajazz\"",
 		"    a b c d e f g h i j k l m n o p q r s t u v w x y z\n> 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 1\n F1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 1 1 1 1 1 1 1 1 1 1 1 0\n\"panamajazz\"",
 		"    a b c d e f g h i j k l m n o p q r s t u v w x y z\n> 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1\n F1 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0\n\"panamajazz\"",
-		"    0 1 2 3 4 5 6 7 8 9 a b c d e f\n F4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4\n  3 0 0 0 0 0 0 0 0 0 0 0 0 1 0 4 0\n  2 0 0 0 0 0 0 0 0 0 0 0 0 1 3 0 0\n  1 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n> 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 \n\"123456789c0de801f\"",
-		"    0 1 2 3 4 5 6 7 8 9 a b c d e f\n F4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4\n  3 0 0 0 0 0 0 0 0 0 0 0 0 1 0 4 0\n  2 0 0 0 0 0 0 0 0 0 0 0 0 1 3 0 0\n  1 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n> 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 \n\"123456789c0d3801f\"",
-		"    0 1 2 3 4 5 6 7 8 9 a b c d e f\n F4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4\n  3 0 0 0 0 0 0 0 0 0 0 0 0 1 0 4 0\n  2 0 0 0 0 0 0 0 0 0 0 0 0 1 3 0 0\n  1 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n> 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 \n\"123456789801f2c0d\"",
-		"    0 1 2 3 4 5 6 7 8 9 a b c d e f\n F4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4\n  3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 4 3\n  2 2 2 2 2 2 2 2 2 2 2 2 2 2 3 2 2\n  1 2 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1\n> 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 \n\"123456789c0d3801f\"",
-		"    0 1 2 3 4 5 6 7 8 9 a b c d e f\n F4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4\n  3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 4 3\n  2 2 2 2 2 2 2 2 2 2 2 2 2 2 3 2 2\n  1 2 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1\n> 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 \n\"123456789c0d38e1f\"",
+		"    0 1 2 3 4 5 6 7 8 9 a b c d e f\n F4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4\n  3 0 0 0 0 0 0 0 0 0 0 0 0 1 0 4 0\n  2 0 0 0 0 0 0 0 0 0 0 0 0 1 3 0 0\n  1 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n> 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0\n\"123456789c0de801f\"",
+		"    0 1 2 3 4 5 6 7 8 9 a b c d e f\n F4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4\n  3 0 0 0 0 0 0 0 0 0 0 0 0 1 0 4 0\n  2 0 0 0 0 0 0 0 0 0 0 0 0 1 3 0 0\n  1 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n> 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0\n\"123456789c0d3801f\"",
+		"    0 1 2 3 4 5 6 7 8 9 a b c d e f\n F4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4\n  3 0 0 0 0 0 0 0 0 0 0 0 0 1 0 4 0\n  2 0 0 0 0 0 0 0 0 0 0 0 0 1 3 0 0\n  1 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n> 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0\n\"123456789801f2c0d\"",
+		"    0 1 2 3 4 5 6 7 8 9 a b c d e f\n F4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4\n  3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 4 3\n  2 2 2 2 2 2 2 2 2 2 2 2 2 2 3 2 2\n  1 2 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1\n> 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0\n\"123456789c0d3801f\"",
+		"    0 1 2 3 4 5 6 7 8 9 a b c d e f\n F4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4\n  3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 4 3\n  2 2 2 2 2 2 2 2 2 2 2 2 2 2 3 2 2\n  1 2 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1\n> 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0\n\"123456789c0d38e1f\"",
 		"    0 1\n> 0 2 1\n  1 3 1\n  2 2 2\n F3 3 1\n\"01010101\"",
 		"    0 1\n> 0 2 1\n  1 3 1\n F2 2 2\n  3 3 1\n\"01010101\"",
 		"    0 1\n> 0 2 1\n  1 3 1\n  2 2 2\n F3 3 1\n\"10101010\"",
@@ -84,6 +182,12 @@ func dfaSimulator() []Run {
 		"0 Accept",
 		"3 Reject",
 		"3 Reject",
+	}
+
+	for i := 0; i < 100; i++ {
+		generatedTest := generate()
+		args = append(args, generatedTest)
+		results = append(results, solve(generatedTest))
 	}
 
 	rand.Shuffle(len(args), func(i, j int) {
