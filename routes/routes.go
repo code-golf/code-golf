8bb4361cc763feb41baf6c0a09a5ccc90828b231
James Raspass <jraspass@gmail.com>
2023-11-27 02:53:51 +0000

    Skip middleware.Golfer for some routes.
    Some routes don't return HTML and don't care if a golfer is currently
    logged in, skipping the middleware for these saves a fairly hairy SQL
    query per request totalling about 20ms. This will become much more
    important with the next commit.

--- routes/routes.go
+++ routes/routes.go
@@ -24,108 +24,114 @@ func Router(db *sqlx.DB) http.Handler {
 		middleware.Compress(5),
 		// middleware.Downtime,
 		middleware.Database(db),
-		middleware.Golfer,
 	)
 
 	r.NotFound(func(w http.ResponseWriter, _ *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
 	})
 
-	r.Get("/", indexGET)
-	r.Get("/{hole}", holeGET)
-	r.Get("/about", aboutGET)
-	r.With(middleware.AdminArea).Route("/admin", func(r chi.Router) {
-		r.Get("/", adminGET)
-		r.Get("/solutions", adminSolutionsGET)
-		r.Get("/solutions/run", adminSolutionsRunGET)
-	})
-	r.With(middleware.API).Route("/api", func(r chi.Router) {
-		r.Get("/", apiGET)
-		r.Get("/cheevos", apiCheevosGET)
-		r.Get("/cheevos/{cheevo}", apiCheevoGET)
-		r.Get("/golfers/{golfer}", apiGolferGET)
-		r.Get("/holes", apiHolesGET)
-		r.Get("/holes/{hole}", apiHoleGET)
-		r.Get("/langs", apiLangsGET)
-		r.Get("/langs/{lang}", apiLangGET)
-		r.Get(
-			"/mini-rankings/{hole}/{lang}/{scoring:bytes|chars}/{view:top|me|following}",
-			apiMiniRankingsGET,
-		)
-		r.Get("/panic", apiPanicGET)
-		r.Get("/solutions-log", apiSolutionsLogGET)
-		r.Get("/suggestions/golfers", apiSuggestionsGolfersGET)
-	})
-	r.Get("/callback", callbackGET)
+	// Simple routes that don't need middleware.Golfer.
 	r.Get("/callback/dev", callbackDevGET)
-	r.Get("/feeds", feedsGET)
 	r.Get("/feeds/{feed:atom|json|rss}", feedGET)
-	r.With(middleware.GolferArea).Route("/golfer", func(r chi.Router) {
-		r.Post("/cancel-delete", golferCancelDeletePOST)
-		r.Get("/connect/{connection}", golferConnectGET)
-		r.Post("/delete", golferDeletePOST)
-		r.Post("/delete-solution", golferDeleteSolutionPOST)
-		r.Get("/disconnect/{connection}", golferDisconnectGET)
-		r.Get("/export", golferExportGET)
-		r.Get("/settings", golferSettingsGET)
-		r.Post("/settings", golferSettingsPOST)
-	})
-	r.With(middleware.GolferInfo).Route("/golfers/{name}", func(r chi.Router) {
-		r.Get("/", golferGET)
-		r.Post("/{action:follow|unfollow}", golferActionPOST)
-		r.Get("/cheevos", golferCheevosGET)
-		r.Get("/holes", redir("holes/rankings/lang/bytes"))
-		r.Get("/holes/{scoring:bytes|chars}", redir("rankings/lang/{scoring}"))
-		r.Get("/holes/{display:rankings|points}/{scope:lang|overall}/{scoring:bytes|chars}", golferHolesGET)
-		r.Get("/{hole}/{lang}/{scoring}", golferSolutionGET)
-		// r.Post("/{hole}/{lang}/{scoring}", golferSolutionPOST)
-	})
 	r.Get("/healthz", healthzGET)
-	r.Get("/ideas", ideasGET)
 	r.Post("/log-out", logOutPost)
 	r.Get("/random", randomGET)
-	r.Route("/rankings", func(r chi.Router) {
-		// Redirect some old URLs that got out.
-		r.Get("/", redir("/rankings/holes/all/all/bytes"))
-		r.Get("/cheevos", redir("/rankings/cheevos/all"))
-		r.Get("/holes", redir("/rankings/holes/all/all/bytes"))
-		r.Get("/holes/all/all/all", redir("/rankings/holes/all/all/bytes"))
-		r.Get("/langs/bytes", redir("/rankings/langs/all/bytes"))
-		r.Get("/langs/chars", redir("/rankings/langs/all/chars"))
-		r.Get("/medals", redir("/rankings/medals/all/all/all"))
-		r.Get("/solutions", redir("/rankings/misc/solutions"))
-
-		r.Get("/holes/{hole}/{lang}/{scoring}", rankingsHolesGET)
-		r.Get("/recent-holes/{lang}/{scoring}", rankingsHolesGET)
-
-		r.Get("/cheevos/{cheevo}", rankingsCheevosGET)
-		r.Get("/medals/{hole}/{lang}/{scoring}", rankingsMedalsGET)
-		r.Get("/langs/{lang}/{scoring}", rankingsLangsGET)
-		r.Get("/misc/{type}", rankingsMiscGET)
-	})
-	r.Route("/recent", func(r chi.Router) {
-		r.Get("/", redir("/recent/solutions/all/all/bytes"))
-		r.Get("/{lang}", recentGET)
-
-		r.Get("/golfers", recentGolfersGET)
-		r.Get("/solutions/{hole}/{lang}/{scoring}", recentSolutionsGET)
-	})
-	r.Get("/scores/{hole}/{lang}", scoresGET)
-	r.Get("/scores/{hole}/{lang}/all", scoresAllGET)
-	r.Get("/scores/{hole}/{lang}/{scoring}", scoresGET)
-	r.Get("/scores/{hole}/{lang}/{scoring}/{page}", scoresGET)
 	r.Get("/sitemap.xml", sitemapGET)
-	r.Get("/stats", statsGET)
 	r.Get("/users/{name}", userGET)
-	r.Get("/wiki", wikiGET)
-	r.Get("/wiki/*", wikiGET)
 
-	// Rate-limit solutions to avoid running out of FDs. Disable under e2e.
-	if _, e2e := os.LookupEnv("E2E"); e2e {
-		r.Post("/solution", solutionPOST)
-	} else {
-		r.With(httprate.LimitByRealIP(60, time.Minute)).Post("/solution", solutionPOST)
-	}
+	// HTML routes that need middleware.Golfer.
+	r.Group(func(r chi.Router) {
+		r.Use(middleware.Golfer)
+
+		r.Get("/", indexGET)
+		r.Get("/{hole}", holeGET)
+		r.Get("/about", aboutGET)
+		r.With(middleware.AdminArea).Route("/admin", func(r chi.Router) {
+			r.Get("/", adminGET)
+			r.Get("/solutions", adminSolutionsGET)
+			r.Get("/solutions/run", adminSolutionsRunGET)
+		})
+		r.With(middleware.API).Route("/api", func(r chi.Router) {
+			r.Get("/", apiGET)
+			r.Get("/cheevos", apiCheevosGET)
+			r.Get("/cheevos/{cheevo}", apiCheevoGET)
+			r.Get("/golfers/{golfer}", apiGolferGET)
+			r.Get("/holes", apiHolesGET)
+			r.Get("/holes/{hole}", apiHoleGET)
+			r.Get("/langs", apiLangsGET)
+			r.Get("/langs/{lang}", apiLangGET)
+			r.Get(
+				"/mini-rankings/{hole}/{lang}/{scoring:bytes|chars}/{view:top|me|following}",
+				apiMiniRankingsGET,
+			)
+			r.Get("/panic", apiPanicGET)
+			r.Get("/solutions-log", apiSolutionsLogGET)
+			r.Get("/suggestions/golfers", apiSuggestionsGolfersGET)
+		})
+		r.Get("/callback", callbackGET)
+		r.Get("/feeds", feedsGET)
+		r.With(middleware.GolferArea).Route("/golfer", func(r chi.Router) {
+			r.Post("/cancel-delete", golferCancelDeletePOST)
+			r.Get("/connect/{connection}", golferConnectGET)
+			r.Post("/delete", golferDeletePOST)
+			r.Post("/delete-solution", golferDeleteSolutionPOST)
+			r.Get("/disconnect/{connection}", golferDisconnectGET)
+			r.Get("/export", golferExportGET)
+			r.Get("/settings", golferSettingsGET)
+			r.Post("/settings", golferSettingsPOST)
+		})
+		r.With(middleware.GolferInfo).Route("/golfers/{name}", func(r chi.Router) {
+			r.Get("/", golferGET)
+			r.Post("/{action:follow|unfollow}", golferActionPOST)
+			r.Get("/cheevos", golferCheevosGET)
+			r.Get("/holes", redir("holes/rankings/lang/bytes"))
+			r.Get("/holes/{scoring:bytes|chars}", redir("rankings/lang/{scoring}"))
+			r.Get("/holes/{display:rankings|points}/{scope:lang|overall}/{scoring:bytes|chars}", golferHolesGET)
+			r.Get("/{hole}/{lang}/{scoring}", golferSolutionGET)
+			// r.Post("/{hole}/{lang}/{scoring}", golferSolutionPOST)
+		})
+		r.Get("/ideas", ideasGET)
+		r.Route("/rankings", func(r chi.Router) {
+			// Redirect some old URLs that got out.
+			r.Get("/", redir("/rankings/holes/all/all/bytes"))
+			r.Get("/cheevos", redir("/rankings/cheevos/all"))
+			r.Get("/holes", redir("/rankings/holes/all/all/bytes"))
+			r.Get("/holes/all/all/all", redir("/rankings/holes/all/all/bytes"))
+			r.Get("/langs/bytes", redir("/rankings/langs/all/bytes"))
+			r.Get("/langs/chars", redir("/rankings/langs/all/chars"))
+			r.Get("/medals", redir("/rankings/medals/all/all/all"))
+			r.Get("/solutions", redir("/rankings/misc/solutions"))
+
+			r.Get("/holes/{hole}/{lang}/{scoring}", rankingsHolesGET)
+			r.Get("/recent-holes/{lang}/{scoring}", rankingsHolesGET)
+
+			r.Get("/cheevos/{cheevo}", rankingsCheevosGET)
+			r.Get("/medals/{hole}/{lang}/{scoring}", rankingsMedalsGET)
+			r.Get("/langs/{lang}/{scoring}", rankingsLangsGET)
+			r.Get("/misc/{type}", rankingsMiscGET)
+		})
+		r.Route("/recent", func(r chi.Router) {
+			r.Get("/", redir("/recent/solutions/all/all/bytes"))
+			r.Get("/{lang}", recentGET)
+
+			r.Get("/golfers", recentGolfersGET)
+			r.Get("/solutions/{hole}/{lang}/{scoring}", recentSolutionsGET)
+		})
+		r.Get("/scores/{hole}/{lang}", scoresGET)
+		r.Get("/scores/{hole}/{lang}/all", scoresAllGET)
+		r.Get("/scores/{hole}/{lang}/{scoring}", scoresGET)
+		r.Get("/scores/{hole}/{lang}/{scoring}/{page}", scoresGET)
+		r.Get("/stats", statsGET)
+		r.Get("/wiki", wikiGET)
+		r.Get("/wiki/*", wikiGET)
+
+		// Rate-limit solutions to avoid running out of FDs. Disable under e2e.
+		if _, e2e := os.LookupEnv("E2E"); e2e {
+			r.Post("/solution", solutionPOST)
+		} else {
+			r.With(httprate.LimitByRealIP(60, time.Minute)).Post("/solution", solutionPOST)
+		}
+	})
 
 	return r
 }
