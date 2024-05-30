dd4f26f0a8f113244a34440a348fceb63cba4f2c
James Raspass <jraspass@gmail.com>
2024-05-30 01:08:03 +0100

    Drop legacy X-Frame-Options header
    The modern way is CSP frame-ancestors header which we also set.

--- routes/render.go
+++ routes/render.go
@@ -145,7 +145,6 @@ func render(w http.ResponseWriter, r *http.Request, name string, data ...any) {
 	header.Set("Content-Language", "en")
 	header.Set("Content-Type", "text/html; charset=utf-8")
 	header.Set("Referrer-Policy", "no-referrer")
-	header.Set("X-Frame-Options", "DENY")
 	header.Set("Content-Security-Policy",
 		"base-uri 'none';"+
 			"connect-src 'self';"+
