0ff0d0ed3817e7943839e311bdae21f360b81726
James Raspass <jraspass@gmail.com>
2024-06-01 01:05:56 +0100

    Make "Reversi" live
    Closes #1123

--- sql/a-schema.sql
+++ sql/a-schema.sql
@@ -46,7 +46,7 @@ CREATE TYPE hole AS ENUM (
     'pascals-triangle', 'pernicious-numbers', 'pernicious-numbers-long',
     'poker', 'prime-numbers', 'prime-numbers-long', 'proximity-grid',
     'qr-decoder', 'quine', 'recamán', 'repeating-decimals',
-    'reverse-polish-notation', 'rijndael-s-box',
+    'reverse-polish-notation', 'reversi', 'rijndael-s-box',
     'rock-paper-scissors-spock-lizard', 'roman-to-arabic', 'rule-110',
     'seven-segment', 'si-units', 'sierpiński-triangle', 'smith-numbers',
     'spelling-numbers', 'star-wars-opening-crawl', 'sudoku', 'sudoku-v2',
