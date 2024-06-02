c612581a508de079736d0c048201c6edad766cc5
H <76665736+MeWhenI@users.noreply.github.com>
2024-06-02 18:22:05 -0500

    Add random cases to brainfuck (#1175)
    * Add random cases to brainfuck
    
    As per discussion in the Discord, adds 5 semi-randomized test-cases to discourage hardcoding. Keeping in line with the hardcoded testcases, all randomized tests are printable ASCII and end with a newline.
    
    * remove duplicate function
    
    * remove unneeded import
    
    * format
    
    * Fixed case to print all ASCII
    
    * curse you gofumpt
    
    * why
    
    * Restrict ASCII range
    
    Restricts the range of ASCII generated by random cases to preserve legacy solutions that relied on the limited character range.

--- hole/brainfuck.go
+++ hole/brainfuck.go
@@ -1,45 +1,110 @@
 package hole
 
 import (
-	"math/rand/v2"
+	"fmt"
 	"strings"
 )
 
 func brainfuck() []Run {
-	args := []string{
-		"+++++++++++++++[>++>+++>++++>+++++>++++++>+++++++>++++++++<<<<<<<-]+++++++++++++++>>>++++++.>>+++++++.>>-----.<-.<<<<<<-----.",
-		"+++++++++++++++[>++>+++>++++>+++++>++++++>+++++++>++++++++<<<<<<<-]+++++++++++++++>>>>-.>+++++++.>>--.<<.<+++++++++.>++.>>----.<.>--.++++.<<<<<<<-----.",
-		"+++++++++++++++++++++++++[>++>+++>++++>+++++<<<<-]+++++++++++++++++++++++++>>+.>>--------.<---.<<<---------------.",
-		"+++++++++++++++++++++++++[>++>+++>++++>+++++<<<<-]+++++++++++++++++++++++++>>+++++.>+.>-----------.------.<<<<---------------.",
-		"+++++++++++++++[>++>+++>++++>+++++>++++++>+++++++>++++++++<<<<<<<-]+++++++++++++++>>>>+++++.>>----.>------.------.<<<<<<++.>>------.<<<-----.",
-		"+++++++++++++++++++++[>++>+++>++++>+++++>++++++<<<<<-]+++++++++++++++++++++>>>----.--------.++++++++.<<<-----------.",
-		"+++++++++++++++++++++[>++>+++>++++>+++++>++++++<<<<<-]+++++++++++++++++++++>>>----.>>-----.-----.<-.>-----.-.<<<<<-----------.",
-		"+++++++++++++++++++++[>++>+++>++++>+++++>++++++<<<<<-]+++++++++++++++++++++>>>--.>>---------.<-------.>++++.<<<<<-----------.",
-		"++++++++++++++++++[>++>+++>++++>+++++>++++++>+++++++<<<<<<-]++++++++++++++++++>>>-----.>>+++.<++++++++++.+.<<<----.>>++++.>>.---.<+.<<<<--------.",
-		">>>>>>>>>>>>>>>>>>>>>>>>>>>>++++++++++++++++++++++++++[-<<[+<]+[>]>][<<[[-]-----<]>[>]>]<<[++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++<]>[.>]++++++++++.",
-		"+++++[>+++++[>++>++>+++>+++>++++>++++<<<<<<-]<-]+++++[>>[>]<[+.<<]>[++.>>>]<[+.<]>[-.>>]<[-.<<<]>[.>]<[+.<]<-]++++++++++.",
-		"++++++++++[>++++++++++>++++++++++++<<-]>--->++>+++++++++++++[<.-<.+>>-]++++[<<-------------------------.>>-]",
+	var tests []test = fixedBFCases()
+
+	tests = append(tests, randomBFCase(5, 3, 8, -3, 2))
+	tests = append(tests, randomBFCase(10, 5, 1, 1, -3))
+
+	for i := 0; i < 3; i++ {
+		jumpSize := randInt(5, 12)
+		buckets := randInt(2, 8)
+		initialBucketSize := randInt(1, 7)
+		bucketSizeChange := randInt(-1, 2)
+		charShift := randInt(-3, 3)
+		tests = append(tests, randomBFCase(jumpSize, buckets, initialBucketSize, bucketSizeChange, charShift))
 	}
 
-	outs := []string{
-		"Bash",
-		"JavaScript",
-		"Lua",
-		"Perl",
-		"Perl 6",
-		"PHP",
-		"Python",
-		"Ruby",
-		"Code Golf",
-		"abcdefghijklmnopqrstuvwxyz",
-		"eL34NfeOL454KdeJ44JOdefePK55gQ67ShfTL787KegJ77JTeghfUK88iV9:XjgYL:;:KfiJ::JYfijgZK;;k[<=]lh^L=>=KgkJ==J^gklh_K>>m`?@bnicL@A@KhmJ@@JchmnidKAA",
-		"zaybxcwdveuftgshriqjpkolnmU<#",
+	return outputTests(shuffle(tests))
+}
+
+func fixedBFCases() []test {
+	return []test{
+		test{"+++++++++++++++[>++>+++>++++>+++++>++++++>+++++++>++++++++<<<<<<<-]+++++++++++++++>>>++++++.>>+++++++.>>-----.<-.<<<<<<-----.",
+			"Bash"},
+		test{"+++++++++++++++[>++>+++>++++>+++++>++++++>+++++++>++++++++<<<<<<<-]+++++++++++++++>>>>-.>+++++++.>>--.<<.<+++++++++.>++.>>----.<.>--.++++.<<<<<<<-----.",
+			"JavaScript"},
+		test{"+++++++++++++++++++++++++[>++>+++>++++>+++++<<<<-]+++++++++++++++++++++++++>>+.>>--------.<---.<<<---------------.",
+			"Lua"},
+		test{"+++++++++++++++++++++++++[>++>+++>++++>+++++<<<<-]+++++++++++++++++++++++++>>+++++.>+.>-----------.------.<<<<---------------.",
+			"Perl"},
+		test{"+++++++++++++++[>++>+++>++++>+++++>++++++>+++++++>++++++++<<<<<<<-]+++++++++++++++>>>>+++++.>>----.>------.------.<<<<<<++.>>------.<<<-----.",
+			"Perl 6"},
+		test{"+++++++++++++++++++++[>++>+++>++++>+++++>++++++<<<<<-]+++++++++++++++++++++>>>----.--------.++++++++.<<<-----------.",
+			"PHP"},
+		test{"+++++++++++++++++++++[>++>+++>++++>+++++>++++++<<<<<-]+++++++++++++++++++++>>>----.>>-----.-----.<-.>-----.-.<<<<<-----------.",
+			"Python"},
+		test{"+++++++++++++++++++++[>++>+++>++++>+++++>++++++<<<<<-]+++++++++++++++++++++>>>--.>>---------.<-------.>++++.<<<<<-----------.",
+			"Ruby"},
+		test{"++++++++++++++++++[>++>+++>++++>+++++>++++++>+++++++<<<<<<-]++++++++++++++++++>>>-----.>>+++.<++++++++++.+.<<<----.>>++++.>>.---.<+.<<<<--------.",
+			"Code Golf"},
+		test{">>>>>>>>>>>>>>>>>>>>>>>>>>>>++++++++++++++++++++++++++[-<<[+<]+[>]>][<<[[-]-----<]>[>]>]<<[++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++<]>[.>]++++++++++.",
+			"abcdefghijklmnopqrstuvwxyz"},
+		test{"+++++[>+++++[>++>++>+++>+++>++++>++++<<<<<<-]<-]+++++[>>[>]<[+.<<]>[++.>>>]<[+.<]>[-.>>]<[-.<<<]>[.>]<[+.<]<-]++++++++++.",
+			"eL34NfeOL454KdeJ44JOdefePK55gQ67ShfTL787KegJ77JTeghfUK88iV9:XjgYL:;:KfiJ::JYfijgZK;;k[<=]lh^L=>=KgkJ==J^gklh_K>>m`?@bnicL@A@KhmJ@@JchmnidKAA"},
+		test{"++++++++++[>++++++++++>++++++++++++<<-]>--->++>+++++++++++++[<.-<.+>>-]++++[<<-------------------------.>>-]",
+			"zaybxcwdveuftgshriqjpkolnmU<#"},
+		test{"++++++++++[>+++++>+++++++>+<<<-]>+>++[-<.+>]>.",
+			"3456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz"},
+	}
+}
+
+func intToBFString(n int) string {
+	if n < 1 {
+		return strings.Repeat("-", -n)
+	}
+	return strings.Repeat("+", n)
+}
+
+func randomBFCase(jumpSize, buckets, initialBucketSize, bucketSizeChange, charShift int) test {
+	// Only generates ASCII values in a range that was already used by fixed cases before random cases were added, in order to not break legacy solutions.
+	const ASCIIMin = 50
+	const ASCIIMax = 122
+
+	bucketString := ""
+	out := ""
+
+	// Ensure all bucket sizes are strictly positive
+	if bucketSizeChange*buckets+initialBucketSize < 1 {
+		bucketSizeChange = 1
 	}
 
-	rand.Shuffle(len(args), func(i, j int) {
-		args[i], args[j] = args[j], args[i]
-		outs[i], outs[j] = outs[j], outs[i]
-	})
+	// Populate buckets
+	bucketSize := initialBucketSize
+	for i := 0; i < buckets; i++ {
+		// Choose a base for this bucket that ensures all chars remain in the printable ASCII range
+		totalShift := charShift * (bucketSize - 1)
+		minChar := ASCIIMin
+		maxChar := ASCIIMax
+		if totalShift > 0 {
+			maxChar -= totalShift
+		} else {
+			minChar -= totalShift
+		}
+		base := randInt(minChar/jumpSize+1, maxChar/jumpSize)
+		bucketString += ">" + intToBFString(base)
 
-	return []Run{{Args: args, Answer: strings.Join(outs, "\n")}}
+		// Add all the chars in the bucket to the solution
+		char := base * jumpSize
+		for j := 0; j < bucketSize; j++ {
+			out += fmt.Sprintf("%c", char)
+			char += charShift
+		}
+
+		bucketSize += bucketSizeChange
+	}
+
+	in := fmt.Sprintf(">%s[%s[<]>-]<%s>>[<<[>+>.%s<<-]>%s>[-]>[<]<<[>++++++++++.>]>>>]",
+		intToBFString(jumpSize),
+		bucketString,
+		intToBFString(initialBucketSize),
+		intToBFString(charShift),
+		intToBFString(bucketSizeChange))
+
+	return test{in, out}
 }
