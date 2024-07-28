c834debe481c16da95cf850ced11a2041b284402
James Raspass <jraspass@gmail.com>
2024-07-29 00:56:05 +0100

    Add cheevo stats to /stats

--- routes/stats.go
+++ routes/stats.go
@@ -13,13 +13,15 @@ import (
 
 // GET /stats
 func statsGET(w http.ResponseWriter, r *http.Request) {
-	var data struct{ Bytes, Golfers, Solutions int }
+	data := struct {
+		Bytes, Cheevos, CheevosEarned, Golfers, Solutions int
+	}{Cheevos: len(config.CheevoList)}
 
 	db := session.Database(r)
 
 	if err := db.QueryRow(
-		"SELECT COUNT(DISTINCT user_id) FROM trophies",
-	).Scan(&data.Golfers); err != nil {
+		"SELECT COUNT(*), COUNT(DISTINCT user_id) FROM trophies",
+	).Scan(&data.CheevosEarned, &data.Golfers); err != nil {
 		panic(err)
 	}
 
