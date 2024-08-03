0edc8ecdbcb6beb49f6cca6ce2eced04ae5925ef
Yewzir <hichamomari@hotmail.com>
2024-08-03 14:25:47 +0200

    Set NODE_ICU_DATA for Day of Week only

--- hole/play.go
+++ hole/play.go
@@ -356,7 +356,9 @@ func play(
 		cmd.Args = []string{"/usr/bin/tcc", "-run", "-"}
 	case "civet":
 		cmd.Args = []string{"/usr/local/dist/civet", "--comptime", "--no-config", "-"}
-		cmd.Env = []string{"NODE_ICU_DATA=/usr/share/icu"}
+		if hole.ID == "day-of-week" {
+			cmd.Env = []string{"NODE_ICU_DATA=/usr/share/icu"}
+		}
 	case "clojure":
 		// Appending (print) prevents implicit output of the last form, if it is not nil.
 		// This seems to be a quirk of the Babashka interpreter that only occurs when
