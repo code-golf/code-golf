fb48620164c87e8231de58450b3cac4a9e058312
James Raspass <jraspass@gmail.com>
2024-08-09 03:20:07 +0100

    Simplify billiard.go slightly

--- hole/billiard.go
+++ hole/billiard.go
@@ -7,38 +7,36 @@ import (
 
 func billiard() []Run {
 	tests := []test{}
-	addTest := func(height int, width int) {
+	addTest := func(height, width int) {
 		common := gcd(height, width)
 		patternSize := common * 2
 		var answer strings.Builder
 		for y := range height {
 			if y > 0 {
-				answer.WriteString("\n")
+				answer.WriteByte('\n')
 			}
 			for x := range width {
 				if x%patternSize == y%patternSize {
-					answer.WriteString("\\")
+					answer.WriteByte('\\')
 				} else if x%patternSize == patternSize-1-y%patternSize {
-					answer.WriteString("/")
+					answer.WriteByte('/')
 				} else {
-					answer.WriteString(" ")
+					answer.WriteByte(' ')
 				}
 			}
 		}
-		tests = append(tests, test{fmt.Sprint(height, " ", width), answer.String()})
+		tests = append(tests, test{fmt.Sprint(height, width), answer.String()})
 	}
 	for i := range 32 {
 		common := i%5 + 1
-		var height int
-		var width int
 		for {
-			height = common * randInt(1, 2)
-			width = common * randInt(1, 5)
+			height := common * randInt(1, 2)
+			width := common * randInt(1, 5)
 			if gcd(height, width) == common {
+				addTest(height, width)
 				break
 			}
 		}
-		addTest(height, width)
 	}
 	for i := range 32 {
 		height := i%8 + 1
