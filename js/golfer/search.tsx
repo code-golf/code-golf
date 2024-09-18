88c93010d3c65df181aea8e0f5df597008485b11
Michal Maršálek <MichalMarsalek@users.noreply.github.com>
2024-09-18 23:57:23 +0200

    add match preview, ci search

--- js/golfer/search.tsx
+++ js/golfer/search.tsx
@@ -22,7 +22,7 @@ $('#searchInput').onkeyup = onSearch;
 $('#isRegex').onchange = onSearch;
 
 function onSearch() {
-    const search = $<HTMLInputElement>('#searchInput').value;
+    let search = $<HTMLInputElement>('#searchInput').value;
     if (!search) {
         $('#resultsOverview').innerText = '';
         $<HTMLInputElement>('#searchInput').setCustomValidity('');
@@ -30,29 +30,55 @@ function onSearch() {
         return;
     }
     const isRegex = $<HTMLInputElement>('#isRegex').checked;
-    let pattern: RegExp | undefined = undefined;
-    try {
-        pattern = new RegExp(isRegex ? search : search.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), 'g');
-    }
-    catch {
-        $<HTMLInputElement>('#searchInput').setCustomValidity('Invalid Regex');
-        $<HTMLInputElement>('#searchInput').reportValidity();
-    }
-    if (pattern) {
+    search = isRegex ? search : search.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+    
+    // If there is 0 case sensitive matches, we match case-insensitively
+    for (const caseInsensitive of [false, true]) {
+        let pattern: RegExp | undefined = undefined;
+        try {
+            pattern = new RegExp(search, caseInsensitive ? 'gi' : 'g');
+        }
+        catch {
+            $<HTMLInputElement>('#searchInput').setCustomValidity('Invalid Regex');
+            $<HTMLInputElement>('#searchInput').reportValidity();
+        }
+        if (pattern) {
         $<HTMLInputElement>('#searchInput').setCustomValidity('');
         const amount = (n: number, singular: string, plural?: string) => `${n} ${n === 1 ? singular : plural ?? singular + 's'}`;
-        const results = solutions.map(x => ({
-            ...x,
-            matches: [...x.Code.matchAll(pattern)],
-        })).filter(x => x.matches.length > 0);
-        const totalCount = results.map(x => x.matches.length).reduce((a,b)=>a+b);
-        $('#resultsOverview').innerText = results.length === 0
-            ? '0 matches'
-            : `${amount(totalCount, 'match', 'matches')} across ${amount(results.length, 'solution')}`;
-        const resultNodes = results.map(r => (<a href={r.Hole + '#' + r.Lang}>
-            <h2>{holes[r.Hole]} in {langs[r.Lang]}{r.Scoring ? ` (${r.Scoring})` : ''}</h2>
-            <span>{amount(r.matches.length, 'match', 'matches')}</span>
-        </a>));
-        $('#results').replaceChildren(...resultNodes);
+
+            const results = solutions.map(x => {
+                const matches = [...x.Code.matchAll(pattern)]
+                const matchesCount = matches.length
+                let firstMatch = {before: "", match: "", after: ""}
+                if (matchesCount > 0) {
+                    const m = matches[0];
+                    const b = m.index;
+                    let a = b;
+                    while (a-1 > 0 && a > b - 10 && !/\n|\r/.test(x.Code[a-1])) a--;
+                    let c = b;
+                    while (c+1 <= b + m[0].length && !/\n|\r/.test(x.Code[c])) c++;
+                    let d = c;
+                    while (d+1 <= x.Code.length && d < c + 10 && !/\n|\r/.test(x.Code[d])) d++;
+                    firstMatch = {before: x.Code.slice(a,b), match: x.Code.slice(b,c), after: x.Code.slice(c,d)};
+                }
+                return {...x, matchesCount, firstMatch};
+            }).filter(x => x.matchesCount > 0);
+            results.sort((a,b)=> a.matchesCount - b.matchesCount);
+            console.log({results})
+            const totalCount = results.map(x => x.matchesCount).reduce((a,b)=>a+b, 0);
+            const ci = caseInsensitive ? 'case insensitive ' : '';
+            $('#resultsOverview').innerText = results.length === 0
+                ? '0 matches'
+                : `${amount(totalCount, ci + 'match', ci + 'matches')} across ${amount(results.length, 'solution')}`;
+            const resultNodes = results.map(r => (<a href={r.Hole + '#' + r.Lang}>
+                <h2>{holes[r.Hole]} in {langs[r.Lang]}{r.Scoring ? ` (${r.Scoring})` : ''}</h2>
+                <span>
+                    <code>{r.firstMatch.before}<span class='match'>{r.firstMatch.match}</span>{r.firstMatch.after}</code>
+                    {r.matchesCount > 1 ? ' and ' + amount(r.matchesCount-1, 'more match', 'more matches') : ''}
+                </span>
+            </a>));
+            $('#results').replaceChildren(...resultNodes);
+            if (totalCount > 0) break;
+        }
     }
 }
