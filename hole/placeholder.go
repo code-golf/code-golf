b0a719ea3896b03562e0f06ae35b0926a22f6df1
alemelis <alessandro_melis@rocketmail.com>
2024-09-25 09:21:03 +0100

    multiple runs with random number of args, just in case...

--- hole/placeholder.go
+++ hole/placeholder.go
@@ -3,20 +3,24 @@ package hole
 import "strconv"
 
 func placeholder() []Run {
-	var tests []test
+	runs := make([][]test, 5)
 
-	for i := 0; i < 100; i++ {
-		word := randWord()
-		tests = append(tests, test{word, word})
+	for r := range runs {
+		var tests []test
+
+		for i := 0; i < randInt(1, 100); i++ {
+			word := randWord()
+			tests = append(tests, test{word, word})
+		}
+
+		fixedOut := "Hello, World!\n"
+		for i := 0; i < 10; i++ {
+			fixedOut += strconv.Itoa(i) + "\n"
+		}
+		tests[0].out = fixedOut + tests[0].out
+
+		runs[r] = tests
 	}
 
-	firstArgOut := tests[0].out
-
-	fixedOut := "Hello, World!\n"
-	for i := 0; i < 10; i++ {
-		fixedOut += strconv.Itoa(i) + "\n"
-	}
-	tests[0].out = fixedOut + firstArgOut
-
-	return outputTests(tests)
+	return outputTests(runs...)
 }
