be33e6e7338a0fad4082967f01dbf6ef59b52c99
Yewzir <hichamomari@hotmail.com>
2024-10-05 22:23:54 +0200

    Add Piet Lang

--- js/_codemirror.ts
+++ js/_codemirror.ts
@@ -164,6 +164,7 @@ export const extensions : { [key: string]: any } = {
     'pascal':        StreamLanguage.define(pascal),
     'perl':          StreamLanguage.define(perl),
     'php':           php,
+    // TODO piet
     'powershell':    StreamLanguage.define(powerShell),
     'prolog':        prolog(),
     'python':        python,
