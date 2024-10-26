b7032099329f22d07a28a5249fb410d84667b9f4
Yewzir <hichamomari@hotmail.com>
2024-10-26 15:11:36 +0200

    Update `langs/prolog/prolog.c`

--- langs/prolog/prolog.c
+++ langs/prolog/prolog.c
@@ -3,20 +3,22 @@
 #include <unistd.h>
 
 int main (int argc, char *argv[]) {
-    char buffer[4096];
-    ssize_t nbytes;
-    FILE *fp = fopen("/tmp/code.pl", "w");
+    if (argc > 1 && strcmp(argv[1], "--version") != 0) {
+        char buffer[4096];
+        ssize_t nbytes;
+        FILE *fp = fopen("/tmp/code.pl", "w");
 
-    if (fp == 0)
-        return 1;
+        if (fp == 0)
+            return 1;
 
-    while ((nbytes = fread(buffer, sizeof(char), sizeof(buffer), stdin)) > 0)
-        if (fwrite(buffer, sizeof(char), nbytes, fp) != nbytes)
-            return 2;
+        while ((nbytes = fread(buffer, sizeof(char), sizeof(buffer), stdin)) > 0)
+            if (fwrite(buffer, sizeof(char), nbytes, fp) != nbytes)
+                return 2;
 
-    fclose(fp);
+        fclose(fp);
 
-    putenv("LC_ALL=C.UTF-8");
+        putenv("LC_ALL=C.UTF-8");
+    }
 
     execv("/usr/bin/swipl", argv);
     perror("execv");
