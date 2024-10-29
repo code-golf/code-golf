07c8eebefcf55476436dcf3b1e754cc0fa5359dc
Yewzir <hichamomari@hotmail.com>
2024-10-29 08:51:16 +0100

    Change `javascript` to `coffeeScript`

--- js/_codemirror.ts
+++ js/_codemirror.ts
@@ -128,7 +128,7 @@ export const extensions : { [key: string]: any } = {
     'brainfuck':     brainfuck(),
     'c':             StreamLanguage.define(c),
     'c-sharp':       StreamLanguage.define(csharp),
-    'civet':         javascript,
+    'civet':         coffeeScript,
     'clojure':       StreamLanguage.define(clojure),
     'cobol':         StreamLanguage.define(cobol),
     'coconut':       python,
