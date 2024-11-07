abf64b8295886e55d17ab0fb28af158fac141935
Yewzir <hichamomari@hotmail.com>
2024-11-07 09:16:53 +0100

    Almost done

--- langs/piet/piet.c
+++ langs/piet/piet.c
@@ -2,14 +2,19 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+// Due to compiling with `-Werror` have both params been marked as "unused", for now.
 int main(int argc __attribute__((unused)), char* argv[] __attribute__((unused))) {
-    if (chdir("/tmp"))
+    if (chdir("/tmp")) {
+        perror("An error occurred when changing directory.");
         return -1;
+    }
     
     FILE* iof = fopen("code.txt", "w");
 
-    if (!iof)
+    if (!iof) {
+        perror("An error occurred when opening the source file for writing.");
         return -1;
+    }
     
     char buffer[4096];
     ssize_t nbytes;
@@ -18,15 +23,20 @@ int main(int argc __attribute__((unused)), char* argv[] __attribute__((unused)))
         if (fwrite(buffer, sizeof(char), nbytes, iof) != (size_t) nbytes)
             return -1;
 
-    if (fclose(iof))
+    if (fclose(iof)) {
+        perror("An error occurred when closing the source file.");
         return -1;
+    }
 
     if (execl("/usr/bin/python", "python", "/ascii2piet.py", "code.txt", "code.png", NULL)) {
-        perror("An error occurred when generating the PNG image.");
+        perror("An error occurred when saving the output file.");
         return -1;
     }
     
-    execl("/usr/bin/npiet", "npiet", "-d", "code.png", NULL); // npiet takes as input a portable pixmap (a ppm file) and since v0.3a png and gif files too. (Source: https://www.bertnase.de/npiet/)
-    perror("execl");
+    if (execl("/usr/bin/npiet", "npiet", "code.png", NULL)) {
+        perror("An error occurred when reading the input file.");
+        return -1;
+    }
+
     return 0;
 }
