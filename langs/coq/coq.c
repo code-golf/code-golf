de2c972e653e7eae701446091b3ee0510a090dd4
Yewzir <hichamomari@hotmail.com>
2024-11-07 04:05:22 +0100

    Coq environment and extraction are working at this point

--- langs/coq/coq.c
+++ langs/coq/coq.c
@@ -4,24 +4,36 @@
 #include <unistd.h>
 
 int main(int argc, char* argv[]) {
-    if (argc && strcmp(argv[1], "--version")) {
-        FILE* iof = fopen("/tmp/code.v", "w");
-
-        if (!iof)
-            return -1;
-    
-        char buffer[4096];
-        ssize_t nbytes;
-
-        while ((nbytes = fread(buffer, sizeof(char), sizeof(buffer), stdin)))
-            if (fwrite(buffer, sizeof(char), nbytes, iof) != (size_t) nbytes)
-                return -1;
-    
-        if (fclose(iof))
-            return -1;
+    if (argc && !strcmp(argv[1], "--version")) {
+        execv("/usr/bin/coqc", argv);
+        perror("execv");
+        return 0;
     }
+
+    if (chdir("/tmp"))
+        return -1;
+
+    FILE* iof = fopen("code.v", "w");
+
+    if (!iof)
+        return -1;
     
-    execv("/usr/bin/coqc", argv);
-    perror("execv");
+    char buffer[4096];
+    ssize_t nbytes;
+
+    while ((nbytes = fread(buffer, sizeof(char), sizeof(buffer), stdin)))
+        if (fwrite(buffer, sizeof(char), nbytes, iof) != (size_t) nbytes)
+            return -1;
+    
+    if (fclose(iof))
+        return -1;
+    
+    //if (execl("/usr/bin/coqc", "coqc", "-o", "code.vo", "code.v", NULL)) {
+    //    perror("An error occurred when compiling the source file.");
+    //    return -1;
+    //}
+    
+    execl("/usr/bin/coqc", "coqc", "-output-directory", "/tmp", "code.v", NULL);
+    perror("execl");
     return 0;
 }
