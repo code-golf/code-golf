b5f65dfc71bd860bd4426e53397889968e643658
Yewzir <hichamomari@hotmail.com>
2024-11-24 00:23:11 +0100

    Fixed

--- langs/harbour/harbourwrapper.c
+++ langs/harbour/harbourwrapper.c
@@ -5,39 +5,39 @@
 
 #define ERR_AND_EXIT(msg) do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
-const char* hb = "/usr/local/bin/hbmk2";
+const char* hbmk = "/usr/bin/hbmk2";
 
 int main(int argc, char* argv[]) {
     if (!strcmp(argv[1], "--version")) {
-        execv("/usr/local/bin/harbour", argv);
+        execv(hbmk, argv);
         ERR_AND_EXIT("execv");
     }
 
     if (chdir("/tmp"))
         ERR_AND_EXIT("chdir");
-    
+
     FILE* fp;
-    
+
     if (!(fp = fopen("code.hb", "w")))
         ERR_AND_EXIT("fopen");
-    
+
     char buffer[4096];
     ssize_t nbytes;
 
     while ((nbytes = read(STDIN_FILENO, buffer, sizeof(buffer))))
         if (fwrite(buffer, sizeof(char), nbytes, fp) != (size_t) nbytes)
             ERR_AND_EXIT("fwrite");
-    
+
     if (fclose(fp))
         ERR_AND_EXIT("fclose");
-    
+
     int hargc = argc + 1;
     char** hargv = malloc(hargc * sizeof(char*));
-    hargv[0] = (char*) hb;
+    hargv[0] = (char*) hbmk;
     hargv[1] = "code.hb";
     memcpy(&hargv[2], &argv[2], (argc - 2) * sizeof(char*));
     hargv[hargc - 1] = NULL;
 
-    execv(hb, hargv);
+    execv(hbmk, hargv);
     ERR_AND_EXIT("execv");
 }
