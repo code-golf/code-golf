0964120eb86f3cbbe31cad079083a127c46de782
Yewzir <hichamomari@hotmail.com>
2024-12-05 17:58:05 +0100

    Consistency tweaks

--- langs/vyxal/vyxalwrapper.c
+++ langs/vyxal/vyxalwrapper.c
@@ -5,9 +5,11 @@
 
 #define ERR_AND_EXIT(msg) do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
+const char* vyxal = "/usr/bin/vyxal", *code = "code.vy";
+
 int main(int argc, char* argv[]) {
     if (argc && !strcmp(argv[1], "--version")) {
-        execl("/usr/bin/vyxal", "vyxal", "-", "⋎", NULL);
+        execl(vyxal, vyxal, "-", "⋎", NULL);
         ERR_AND_EXIT("execl");
     }
 
@@ -16,7 +18,7 @@ int main(int argc, char* argv[]) {
 
     FILE* fp;
 
-    if (!(fp = fopen("code.vy", "w")))
+    if (!(fp = fopen(code, "w")))
         ERR_AND_EXIT("fopen");
 
     char buffer[4096];
@@ -25,18 +27,18 @@ int main(int argc, char* argv[]) {
     while ((nbytes = read(STDIN_FILENO, buffer, sizeof(buffer))))
         if (fwrite(buffer, sizeof(char), nbytes, fp) != (size_t) nbytes)
             ERR_AND_EXIT("fwrite");
-    
+
     if (fclose(fp))
         ERR_AND_EXIT("fclose");
-    
+
     int vargc = argc + 2;
     char** vargv = malloc(vargc * sizeof(char*));
-    vargv[0] = "/usr/bin/vyxal";
-    vargv[1] = "code.vy";
+    vargv[0] = (char*) vyxal;
+    vargv[1] = (char*) code;
     vargv[2] = "MOa";
     memcpy(&vargv[3], &argv[2], (argc - 2) * sizeof(char*));
     vargv[vargc - 1] = NULL;
-    
-    execv("/usr/bin/vyxal", vargv);
+
+    execv(vyxal, vargv);
     ERR_AND_EXIT("execv");
 }
