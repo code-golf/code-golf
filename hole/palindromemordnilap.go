928b1007b4f36197e57f8b7e077d021c50e536e7
Yewzir <hichamomari@hotmail.com>
2024-12-14 05:17:15 +0100

    Add test case to Palindrome...aP hole
    Co-authored-by: Lukas Potters <lukaspotters@gmail.com>

--- hole/palindromemordnilap.go
+++ hole/palindromemordnilap.go
@@ -28,7 +28,7 @@ func palindromemordnilap() []Run {
 		"123456", "8989a",
 
 		"a", "aA", "aa", "aaaaaaa", "ab", "aba", "abaaaba", "abb", "abc",
-		"abcdc", "abcdcc",
+		"abca", "abcdc", "abcdcc",
 
 		"better", "mississippi", "Palindrome",
 
