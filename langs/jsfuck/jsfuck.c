d38b69089716d03700809db21e8eb087984a879f
Yewzir <hichamomari@hotmail.com>
2024-12-20 14:59:48 +0100

    Alpine Linux 3.21

--- langs/jsfuck/jsfuck.c
+++ langs/jsfuck/jsfuck.c
@@ -5,33 +5,37 @@
 
 #define ERR_AND_EXIT(msg) do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
+const char* jsfuck = "/usr/bin/node", *code = "code.js";
+
 int main(int argc, char* argv[]) {
-    // FIXME Avoid keyboard interruption from never-terminating containers
-    if (argc > 1 && !strcmp(argv[1], "--version"))
+    if (!strcmp(argv[1], "--version"))
         exit(EXIT_SUCCESS);
+
+    if (chdir("/tmp"))
+        ERR_AND_EXIT("chdir");
 
     FILE* fp;
 
-    if (!(fp = fopen("/tmp/code.js", "w")))
+    if (!(fp = fopen(code, "w")))
         ERR_AND_EXIT("fopen");
 
     char buffer[4096];
     ssize_t nbytes;
 
-    while ((nbytes = fread(buffer, sizeof(char), sizeof(buffer), stdin)))
+    while ((nbytes = read(STDIN_FILENO, buffer, sizeof(buffer))))
         if (fwrite(buffer, sizeof(char), nbytes, fp) != (size_t) nbytes)
             ERR_AND_EXIT("fwrite");
-    
+
     if (fclose(fp))
         ERR_AND_EXIT("fclose");
-    
-    int nargc = argc + 1;
-    char** nargv = malloc(nargc * sizeof(char*));
-    nargv[0] = "/usr/bin/node";
-    nargv[1] = "/tmp/code.js";
-    memcpy(&nargv[2], &argv[2], (argc - 2) * sizeof(char*));
-    nargv[nargc - 1] = NULL;
-    
-    execv("/usr/bin/node", nargv);
+
+    int jargc = argc + 1;
+    char** jargv = malloc(jargc * sizeof(char*));
+    jargv[0] = (char*) jsfuck;
+    jargv[1] = (char*) code;
+    memcpy(&jargv[2], &argv[2], (argc - 2) * sizeof(char*));
+    jargv[jargc - 1] = NULL;
+
+    execv(jsfuck, jargv);
     ERR_AND_EXIT("execv");
 }
