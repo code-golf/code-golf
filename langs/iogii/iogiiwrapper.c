55fe79dec459a1cdb5a203da559549df9f6cae7a
Yewzir <hichamomari@hotmail.com>
2024-12-31 13:22:54 +0100

    Revert iogii to 0.1

--- langs/iogii/iogiiwrapper.c
+++ langs/iogii/iogiiwrapper.c
@@ -1,11 +1,13 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #define ERR_AND_EXIT(msg) do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
-const char* iogii = "/usr/bin/iogii", *code = "code.iog";
+const char* iogii = "/usr/bin/iogii", *code = "code.iog", *input = "argv.txt";
 
 int main(int argc, char* argv[]) {
     if (!strcmp(argv[1], "--version")) {
@@ -31,14 +33,39 @@ int main(int argc, char* argv[]) {
     if (fclose(fp))
         ERR_AND_EXIT("fclose");
 
-    int iargc = argc + 2;
-    char** iargv = malloc(iargc * sizeof(char*));
-    iargv[0] = (char*) iogii;
-    iargv[1] = (char*) code;
-    iargv[2] = "--";
-    memcpy(&iargv[3], &argv[2], (argc - 2) * sizeof(char*));
-    iargv[iargc - 1] = NULL;
+    int fd;
 
-    execv(iogii, iargv);
-    ERR_AND_EXIT("execv");
+    if (!(fd = open(input, O_CREAT | O_TRUNC | O_WRONLY, 0644)))
+        ERR_AND_EXIT("open");
+
+    for (int i = 2; i < argc; i++)
+        if (write(fd, argv[i], strlen(argv[i])) < 0 || write(fd, "\n", sizeof(char)) < 0) {
+            if (close(fd))
+                ERR_AND_EXIT("close");
+
+            ERR_AND_EXIT("write");
+        }
+
+    if (close(fd))
+        ERR_AND_EXIT("close");
+
+    pid_t pid;
+
+    if (!(pid = fork())) {
+        if (dup2(open(input, O_RDONLY), STDIN_FILENO))
+            ERR_AND_EXIT("dup2");
+
+        execl(iogii, iogii, code, NULL);
+        ERR_AND_EXIT("execl");
+    }
+
+    int status;
+
+    waitpid(pid, &status, 0);
+
+    if (!WIFEXITED(status))
+        exit(EXIT_FAILURE);
+
+    if (WEXITSTATUS(status))
+        return WEXITSTATUS(status);
 }
