46985d797a770d672e0c3d4c67f57b288361e0b5
Yewzir <hichamomari@hotmail.com>
2025-01-23 11:44:20 +0100

    An expanded dictionary continues to be horse shit

--- hole/boggle.go
+++ hole/boggle.go
@@ -5,19 +5,16 @@ import (
 	"strings"
 )
 
-// TODO Figure out dictionary whats and whatnots
-// TODO 'Qu' implementation
-
 func boggle() []Run {
 	tests := make([]test, 0, 100)
 
 	for range 100 {
 		var argument, expected strings.Builder
 
-		board, words := scramble(), make(map[string]struct{})
+		board, count, words := scramble(), randInt(100, 200), make(map[string]struct{})
 
-		for i := 0; i < 225; {
-			word := strings.ToUpper(randWord())
+		for i := 0; i < count; {
+			word := randWord()
 
 			if _, dupe := words[word]; dupe {
 				continue
@@ -26,20 +23,21 @@ func boggle() []Run {
 			words[word] = struct{}{}
 			i++
 
-			argument.WriteString(word)
+			argument.WriteString(strings.ToLower(word))
 
-			if i < 225 {
+			if i < count {
 				argument.WriteRune(' ')
 			}
 		}
 
 		for _, word := range strings.Fields(argument.String()) {
-			if validate(board, word) {
-				expected.WriteString(word + " ")
+			if validate(board, strings.ToUpper(word)) {
+				expected.WriteString(word)
+				expected.WriteRune(' ')
 			}
 		}
 
-		if expected.String() == "" {
+		if len(expected.String()) == 0 {
 			expected.WriteRune('-')
 		}
 
@@ -73,22 +71,22 @@ func stringify(board [4][4]rune) string {
 }
 
 func scramble() [4][4]rune {
-	var cubes = [16][]rune{
+	var dice = [16][]rune{
 		[]rune("RIFOBX"), []rune("IFEHEY"), []rune("DENOWS"), []rune("UTOKND"),
 		[]rune("HMSRAO"), []rune("LUPETS"), []rune("ACITOA"), []rune("YLGKUE"),
 		[]rune("QBMJOA"), []rune("EHISPN"), []rune("VETIGN"), []rune("BALIYT"),
 		[]rune("EZAVND"), []rune("RALESC"), []rune("UWILRG"), []rune("PACEMD"),
 	}
 
-	for _, cube := range cubes {
+	for _, die := range dice {
 		// Scramble positions.
-		rand.Shuffle(len(cubes), func(i, j int) {
-			cubes[i], cubes[j] = cubes[j], cubes[i]
+		rand.Shuffle(len(dice), func(i, j int) {
+			dice[i], dice[j] = dice[j], dice[i]
 		})
 
 		// Scramble letters.
-		rand.Shuffle(len(cube), func(i, j int) {
-			cube[i], cube[j] = cube[j], cube[i]
+		rand.Shuffle(len(die), func(i, j int) {
+			die[i], die[j] = die[j], die[i]
 		})
 	}
 
@@ -96,7 +94,7 @@ func scramble() [4][4]rune {
 
 	for i := range 4 {
 		for j := range 4 {
-			board[i][j] = cubes[j + i * 4][0] // Index 0 represents the side facing up after scrambling.
+			board[i][j] = dice[j + i * 4][0] // Index 0 represents the side facing up after scrambling.
 		}
 	}
 
@@ -104,15 +102,15 @@ func scramble() [4][4]rune {
 }
 
 func validate(board [4][4]rune, word string) bool {
-	if len(word) < 3 {
+	if len(word) < 3 || len(word) > 16 {
 		return false
 	}
 
-	crossed, letters := [4][4]bool{}, []rune(word)
+	letters, used := []rune(word), [4][4]bool{}
 
 	for i := range 4 {
 		for j := range 4 {
-			if board[i][j] == letters[0] && dfs(board, crossed, letters, 0, i, j) {
+			if board[i][j] == letters[0] && dfs(board, used, letters, 0, i, j) {
 				return true
 			}
 		}
@@ -134,7 +132,7 @@ func dfs(board [4][4]rune, used [4][4]bool, word []rune, index, i, j int) bool {
 
 	for _, direction := range [][]int{
 		{-1, -1}, {-1, +0}, {-1, +1},
-		{+0, -1}, {+0, +0}, {+0, +1},
+		{+0, -1}, {+0, +1},
 		{+1, -1}, {+1, +0}, {+1, +1},
 	} {
 		x, y := i + direction[0], j + direction[1]
