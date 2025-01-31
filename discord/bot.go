fcf2c4ad414bdfaba280fe841eb670c140d2f4d7
James Raspass <jraspass@gmail.com>
2025-01-31 00:00:01 +0000

    Release "Tic-tac-toe"
    Closes #1031

--- discord/bot.go
+++ discord/bot.go
@@ -64,8 +64,8 @@ func init() {
 }
 
 // TODO Make this dynamic based on hole/lang age.
-func channel(_ *config.Hole, lang *config.Lang) string {
-	if lang.ID == "gleam" {
+func channel(hole *config.Hole, lang *config.Lang) string {
+	if hole.ID == "tic-tac-toe" || lang.ID == "gleam" {
 		return chanFreshID
 	}
 	return chanSourID
