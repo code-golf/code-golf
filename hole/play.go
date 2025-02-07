156f5e0b4f3a072321e5a5e58b3c565d0514854d
Hicham Omari <hichamomari@hotmail.com>
2025-02-07 08:18:15 +0100

    Suppress the implicit output on the Quine hole in Racket (#1944)

--- hole/play.go
+++ hole/play.go
@@ -375,6 +375,12 @@ func play(
 		}
 	case "php":
 		code = "<?php " + code + " ;"
+	case "racket":
+		if hole.ID == "quine" {
+			// Inserting `(current-print (λ (x) (void)))` before the code in the editor
+			// suppresses the implicit output of expressions in Racket.
+			code = "(current-print (λ (x) (void)))" + code
+		}
 	case "tex":
 		// Prevent trivial quines. Error out and return early.
 		if hole.ID == "quine" && !strings.Contains(code, `\`) {
