350886ff4aab4e3ed8a7bcf54e5bb627b35b34cd
James Raspass <jraspass@gmail.com>
2025-02-08 15:13:54 +0000

    Whoops, add missing Country.Scan method
    This fixes the 500 introduced by the previous commit on /stats/countries

--- config/sql.go
+++ config/sql.go
@@ -41,9 +41,10 @@ func (n *NullLang) Scan(id any) error {
 	return nil
 }
 
-func (c *Cheevo) Scan(src any) error { return scanID(c, src, CheevoByID) }
-func (h *Hole) Scan(src any) error   { return scanID(h, src, AllHoleByID) }
-func (l *Lang) Scan(src any) error   { return scanID(l, src, AllLangByID) }
+func (c *Cheevo) Scan(src any) error  { return scanID(c, src, CheevoByID) }
+func (c *Country) Scan(src any) error { return scanID(c, src, CountryByID) }
+func (h *Hole) Scan(src any) error    { return scanID(h, src, AllHoleByID) }
+func (l *Lang) Scan(src any) error    { return scanID(l, src, AllLangByID) }
 
 func (c *Cheevos) Scan(src any) error { return scanIDs(c, src, CheevoByID) }
 func (h *Holes) Scan(src any) error   { return scanIDs(h, src, AllHoleByID) }
