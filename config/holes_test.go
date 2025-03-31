768883818d2cea3b78157359e791967343e661c3
James Raspass <jraspass@gmail.com>
2025-03-31 23:19:26 +0100

    Make better use of the new testing API
    It's not everywhere, but it's a good start.

--- config/holes_test.go
+++ config/holes_test.go
@@ -10,12 +10,12 @@ import (
 func TestHoleSynopses(t *testing.T) {
 	for _, hole := range AllHoleList {
 		t.Run(hole.Name, func(t *testing.T) {
-			assert.True(t, hole.Synopsis != "", "Has a synopsis")
+			assert.NotEmpty(t, hole.Synopsis, "Has a synopsis")
 
 			assert.True(t, strings.HasSuffix(hole.Synopsis, "."),
 				"Synopsis ends in a period")
 
-			assert.False(t, strings.ContainsRune(hole.Synopsis, '\n'),
+			assert.NotContains(t, hole.Synopsis, "\n",
 				"Synopsis spans exactly one line")
 
 			assert.False(t, strings.ContainsAny(hole.Synopsis, `"'`),
