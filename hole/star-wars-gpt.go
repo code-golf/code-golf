d7c2be73221fe2d45bf347879b0a03bf28a5c0c6
Stefan Gimmillaro <stefan.c.gimmillaro@gmail.com>
2025-04-16 21:04:39 -0700

    Update star-wars-gpt.go
    refactor to be multiple inputs in a single test run. languages with slow spin up time (kotlin) could not handle all of the parallel test cases.

--- hole/star-wars-gpt.go
+++ hole/star-wars-gpt.go
@@ -1,4 +1,4 @@
-package hole
+package main
 
 import (
 	"slices"
@@ -50,7 +50,7 @@ var _ = answerFunc("star-wars-gpt", func() []Answer {
 		`The dead speak! The galaxy has heard a mysterious broadcast, a threat of REVENGE in the sinister voice of the late EMPEROR PALPATINE. GENERAL LEIA ORGANA dispatches secret agents to gather intelligence, while REY, the last hope of the Jedi, trains for battle against the diabolical FIRST ORDER. Meanwhile, Supreme Leader KYLO REN rages in search of the phantom Emperor, determined to destroy any threat to his power....`,
 	})
 
-	answers := make([]Answer, 10)
+	tests := make([]test, len(inputs))
 	testWordCount := 12
 
 	for i := 0; i < 10; i += 2 {
@@ -90,8 +90,7 @@ var _ = answerFunc("star-wars-gpt", func() []Answer {
 			counter++
 		}
 
-		args := []string{inputSWGPT.String()}
-		answers[i/2] = Answer{Args: args, Answer: outputSWGPT.String()}
+		tests[i/2] = test{inputSWGPT.String(), outputSWGPT.String()}
 	}
 	staticTest1 := "the first order reigns having decimated the peaceful republic supreme leader snoke now deploys his merciless legions to seize military control of the galaxy only general leia organa's band of resistance fighters stand against the rising tyranny certain that jedi master luke skywalker will return and restore a spark of hope to the fight but the resistance has been exposed as the first order speeds toward the rebel base the brave heroes mount a desperate escape turmoil has engulfed the galactic republic the taxation of trade routes to outlying star systems is in dispute hoping to resolve the matter with a blockade of deadly battleships the greedy trade federation has stopped all shipping to the small planet of naboo while the congress of the republic endlessly debates this alarming chain of events the supreme chancellor has secretly dispatched two jedi knights the guardians of peace and justice in the galaxy to settle the conflict\nof\nto\nthe\ndispatched\nfirst\nmilitary\nengulfed\ngalaxy\nspark\ncertain\nevents\nseize"
 	staticResult1 := "the\nthe\nfirst\ntwo\norder\ncontrol\nthe\nonly\nof\nthat\nthe\nmilitary"
@@ -103,11 +102,11 @@ var _ = answerFunc("star-wars-gpt", func() []Answer {
 	staticResult4 := "solar\nleave\nchancellor\nthe\nrepublic\nlord\nknights\njedi\nthis\nsenator\nthe\nthe"
 	staticTest5 := "there is unrest in the galactic senate several thousand solar systems have declared their intentions to leave the republic this separatist movement under the leadership of the mysterious count dooku has made it difficult for the limited number of jedi knights to maintain peace and order in the galaxy senator amidala the former queen of naboo is returning to the galactic senate to vote on the critical issue of creating an army of the republic to assist the overwhelmed jedi luke skywalker has vanished in his absence the sinister first order has risen from the ashes of the empire and will not rest until skywalker the last jedi has been destroyed with the support of the republic general leia organa leads a brave resistance she is desperate to find her brother luke and gain his help in restoring peace and justice to the galaxy leia has sent her most daring pilot on a secret mission to jakku where an old ally has discovered a clue to luke's whereabouts\nand\ngain\nsenate\nbrother\na\npeace\nassist\nqueen\nhis\njedi\npilot\ngalactic"
 	staticResult5 := "order\nhis\nseveral\nluke\nbrave\nand\nthe\nof\nabsence\nknights\non\nsenate"
-	answers[5] = Answer{Args: []string{staticTest1}, Answer: staticResult1}
-	answers[6] = Answer{Args: []string{staticTest2}, Answer: staticResult2}
-	answers[7] = Answer{Args: []string{staticTest3}, Answer: staticResult3}
-	answers[8] = Answer{Args: []string{staticTest4}, Answer: staticResult4}
-	answers[9] = Answer{Args: []string{staticTest5}, Answer: staticResult5}
+	tests[5] = test{staticTest1, staticResult1}
+	tests[6] = test{staticTest2, staticResult2}
+	tests[7] = test{staticTest3, staticResult3}
+	tests[8] = test{staticTest4, staticResult4}
+	tests[9] = test{staticTest5, staticResult5}
 
-	return answers
+	return outputTests(shuffle(tests))
 })
