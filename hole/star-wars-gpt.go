58c65e87fc7a8b0a4f64aaac4f90da46aa0d2a81
Stefan Gimmillaro <stefan.c.gimmillaro@gmail.com>
2025-04-16 21:06:43 -0700

    Update star-wars-gpt.go

--- hole/star-wars-gpt.go
+++ hole/star-wars-gpt.go
@@ -50,7 +50,7 @@ var _ = answerFunc("star-wars-gpt", func() []Answer {
 		`The dead speak! The galaxy has heard a mysterious broadcast, a threat of REVENGE in the sinister voice of the late EMPEROR PALPATINE. GENERAL LEIA ORGANA dispatches secret agents to gather intelligence, while REY, the last hope of the Jedi, trains for battle against the diabolical FIRST ORDER. Meanwhile, Supreme Leader KYLO REN rages in search of the phantom Emperor, determined to destroy any threat to his power....`,
 	})
 
-	tests := make([]test, len(inputs))
+	tests := make([]test, 10)
 	testWordCount := 12
 
 	for i := 0; i < 10; i += 2 {
