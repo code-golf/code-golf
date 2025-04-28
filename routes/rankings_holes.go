779e58f7d6a7473f6b28e7029ad9e8c0aab37f53
James Raspass <jraspass@gmail.com>
2025-04-28 21:32:44 +0100

    Highlight row on ranking deep links
    The links from a golfer's solution page now include the specific row to
    highlight.

--- routes/rankings_holes.go
+++ routes/rankings_holes.go
@@ -25,12 +25,12 @@ func rankingsHolesGET(w http.ResponseWriter, r *http.Request) {
 		Pager                                 *pager.Pager
 		Recent                                bool
 		Rows                                  []struct {
-			Country                             *config.Country
-			Holes, Rank, Points, Strokes, Total int
-			Lang                                *config.Lang
-			Name                                string
-			OtherStrokes                        *int
-			Submitted                           time.Time
+			Country                                  *config.Country
+			Holes, Points, Rank, Row, Strokes, Total int
+			Lang                                     *config.Lang
+			Name                                     string
+			OtherStrokes                             *int
+			Submitted                                time.Time
 		}
 	}{
 		HoleID:  param(r, "hole"),
@@ -133,6 +133,7 @@ func rankingsHolesGET(w http.ResponseWriter, r *http.Request) {
 			          other_strokes    other_strokes,
 			          points           points,
 			          rank_overall     rank,
+			          row_overall      row,
 			          strokes          strokes,
 			          submitted        submitted,
 			          COUNT(*) OVER()  total
@@ -150,6 +151,7 @@ func rankingsHolesGET(w http.ResponseWriter, r *http.Request) {
 			          other_strokes    other_strokes,
 			          points_for_lang  points,
 			          rank             rank,
+			          row              row,
 			          strokes          strokes,
 			          submitted        submitted,
 			          COUNT(*) OVER()  total
