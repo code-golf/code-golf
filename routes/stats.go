6ba61df8f7309e20bdc5ffcdc7a44074d51d4635
James Raspass <jraspass@gmail.com>
2025-05-21 23:27:42 +0100

    Add "Per Day" to /stats/golfers

--- routes/stats.go
+++ routes/stats.go
@@ -118,6 +118,7 @@ func statsGolfersGET(w http.ResponseWriter, r *http.Request) {
 	var data []struct {
 		Count, Sum int
 		Date       time.Time
+		PerDay     string
 	}
 
 	if err := session.Database(r).Select(
@@ -139,9 +140,11 @@ func statsGolfersGET(w http.ResponseWriter, r *http.Request) {
 		           COUNT(*)
 		      FROM earnt_golfers
 		  GROUP BY date
-		) SELECT * FROM first_golfer
+		) SELECT *, 0 per_day FROM first_golfer
 		   UNION ALL
-		  SELECT *, SUM(count) OVER (ORDER BY date) FROM counts`,
+		  SELECT *, SUM(count) OVER (ORDER BY date),
+		         ROUND(count / EXTRACT(doy FROM date), 2) per_day
+		    FROM counts`,
 	); err != nil {
 		panic(err)
 	}
