d877f4325ed2c5076ece4d4305f55046e2984eb4
James Raspass <jraspass@gmail.com>
2025-05-26 21:47:37 +0100

    Scrambled Alpha: inline alphabetizeWords()
    And add a couple of comments to describing the two steps.
    
    Keep alphabet a slice of bytes as that slightly simplifies the
    construction of the order map.

--- hole/scrambled-alphabetization.go
+++ hole/scrambled-alphabetization.go
@@ -6,40 +6,37 @@ import (
 	"strings"
 )
 
-func alphabetizeWords(alphabet string, words []string) string {
-	order := map[byte]int{}
-	for i := range alphabet {
-		order[alphabet[i]] = i
-	}
-
-	slices.SortFunc(words, func(a, b string) int {
-		for i := range min(len(a), len(b)) {
-			if c := cmp.Compare(order[a[i]], order[b[i]]); c != 0 {
-				return c
-			}
-		}
-
-		return cmp.Compare(len(a), len(b))
-	})
-
-	return strings.Join(words, " ")
-}
-
 var _ = answerFunc("scrambled-alphabetization", func() []Answer {
 	tests := make([]test, 100)
 
 	for i := range tests {
-		alphabet := string(shuffle([]byte("abcdefghijklmnopqrstuvwxyz")))
+		// Generate a random alphabet and words.
+		alphabet := shuffle([]byte("abcdefghijklmnopqrstuvwxyz"))
 
 		words := make([]string, randInt(3, 9))
 		for i := range words {
 			words[i] = randWord()
 		}
 
-		tests[i] = test{
-			in:  alphabet + " " + strings.Join(words, " "),
-			out: alphabetizeWords(alphabet, words),
+		tests[i].in = string(alphabet) + " " + strings.Join(words, " ")
+
+		// Sort words according to the random alphabet.
+		order := map[byte]int{}
+		for i, b := range alphabet {
+			order[b] = i
 		}
+
+		slices.SortFunc(words, func(a, b string) int {
+			for i := range min(len(a), len(b)) {
+				if c := cmp.Compare(order[a[i]], order[b[i]]); c != 0 {
+					return c
+				}
+			}
+
+			return cmp.Compare(len(a), len(b))
+		})
+
+		tests[i].out = strings.Join(words, " ")
 	}
 
 	return outputTests(tests)
