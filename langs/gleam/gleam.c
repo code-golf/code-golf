d982d3a78ca3768b599ba79c576df2fbc85105ae
Yewzir <hichamomari@hotmail.com>
2025-06-19 14:08:31 +0200

    Now just remove those stupid version numbers

--- langs/gleam/gleam.c
+++ langs/gleam/gleam.c
@@ -11,7 +11,7 @@
 void copy_file(const char* src_file, const char* dst_file);
 void copy_folder(const char* src_dir, const char* dst_dir);
 
-const char* gleam = "/usr/bin/gleam", *code = "src/main.gleam";
+const char* gleam = "/usr/local/bin/gleam", *code = "src/main.gleam";
 
 int main(int argc, char* argv[]) {
     if (!strcmp(argv[1], "--version")) {
@@ -19,7 +19,7 @@ int main(int argc, char* argv[]) {
         ERR_AND_EXIT("execv");
     }
 
-    copy_folder("/gleam", "/tmp");
+    copy_folder("/usr/local", "/tmp");
 
     if (chdir("/tmp"))
         ERR_AND_EXIT("chdir");
@@ -42,7 +42,6 @@ int main(int argc, char* argv[]) {
     pid_t pid;
 
     if (!(pid = fork())) {
-        // Print errors and/or warnings on STDERR.
         if (!dup2(STDERR_FILENO, STDOUT_FILENO))
             ERR_AND_EXIT("dup2");
 
