66a499434f70fb8988151b4a3790733de30df6b3
GolfingSuccess <19519588+GolfingSuccess@users.noreply.github.com>
2025-07-09 22:24:36 +0300

    make things work

--- hole/topological-sort.go
+++ hole/topological-sort.go
@@ -13,8 +13,10 @@ func tsort(in string) string {
 	children, in_degrees := make(map[string][]string), make(map[string]int)
 	for pair := range slices.Chunk(strings.Fields(in), 2) {
 		children[pair[0]] = append(children[pair[0]], pair[1])
-		in_degrees[pair[0]] = in_degrees[pair[0]]
-		in_degrees[pair[1]]++
+		in_degrees[pair[0]] = in_degrees[pair[0]] // include zero-in-degree nodes
+		if pair[0] != pair[1] {
+			in_degrees[pair[1]]++
+		}
 	}
 	var stack []string
 	for node, deg := range in_degrees {
@@ -44,18 +46,16 @@ func generateTsortTest(length, additional int) string {
 		seq  []int
 		rows int
 	)
-	for i, x := range init[:length-1] {
-		seq = append(seq, x, init[i+1])
-		rows++
-	}
-	arc_choices := rand.Perm((length - 2) * (length - 1) / 2)
-	for i, x := range init[:length-2] {
-		for _, y := range init[i+2:] {
-			if arc_choices[len(arc_choices)-1] < additional {
+	arc_choices := rand.Perm((length-2)*(length-1)/2 + length)
+	for i, x := range init {
+		for j, y := range init[i:] {
+			if j == 1 || arc_choices[len(arc_choices)-1] < additional {
 				seq = append(seq, x, y)
 				rows++
 			}
-			arc_choices = slices.Delete(arc_choices, len(arc_choices)-1, len(arc_choices))
+			if j != 1 {
+				arc_choices = slices.Delete(arc_choices, len(arc_choices)-1, len(arc_choices))
+			}
 		}
 	}
 	var test string
@@ -70,13 +70,15 @@ func generateTsortTest(length, additional int) string {
 
 // single-digit numbers
 var _ = answerFunc("topological-sort", func() []Answer {
-        var tests []test
-        for n := range 10 - 2 + 1 {
-                n += 2
-                for a := range (n - 2) * (n - 1) / 2 + 1 {
-                        tcase := generateTsortTest(n, a)
-                        tests = append(tests, test{tcase, tsort(tcase)})
-                }
-        }
-        return outputTests(shuffle(tests))
+	var tests []test
+	for n := range 10 - 2 + 1 {
+		n += 2
+		for a := range (n-2)*(n-1)/2 + n + 1 {
+			for range 3 {
+				tcase := generateTsortTest(n, a)
+				tests = append(tests, test{tcase, tsort(tcase)})
+			}
+		}
+	}
+	return outputTests(shuffle(tests))
 })
