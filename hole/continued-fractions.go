ed2704244a1a50107f38d58d67ed92acf24abb2e
_hyacinth <148560588+albanian-laundromat@users.noreply.github.com>
2025-11-18 18:33:28 -0600

    Add Continued Fractions Hole (attempt 2) (#2502)
    * Add files via upload
    
    * Update continued-fractions.go
    
    * Update holes.toml
    
    * Update a-schema.sql
    
    * Update continued-fractions.go
    
    * Update continued-fractions.go

---
+++ hole/continued-fractions.go
@@ -0,0 +1,33 @@
+package hole
+
+import (
+	"fmt"
+	"math/rand/v2"
+)
+
+var _ = answerFunc("continued-fractions", func() []Answer {
+	tests := make([]test, 100)
+
+	for i := range 100 {
+		tests[i] = continuedFractionsTest(rand.IntN(256), 1+rand.IntN(255))
+	}
+
+	return outputTests(shuffle(tests))
+})
+
+func continuedFractionsTest(n, d int) test {
+	in := fmt.Sprint(n, "/", d)
+	out := fmt.Sprint("[", n/d)
+	n %= d
+	if n > 0 {
+		out = fmt.Sprint(out, ";")
+		for n%d > 0 {
+			out = fmt.Sprint(out, n/d, ",")
+			n %= d
+			n, d = d, n
+		}
+		out = out[:len(out)-1]
+	}
+	out = fmt.Sprint(out, "]")
+	return test{in, out}
+}
