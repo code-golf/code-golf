a3acfd59814368db30ff197651f9c6cc45a43d26
_hyacinth <148560588+albanian-laundromat@users.noreply.github.com>
2025-11-24 13:04:25 -0600

    Update smooth-numbers.go

--- hole/smooth-numbers.go
+++ hole/smooth-numbers.go
@@ -16,7 +16,6 @@ var _ = answerFunc("smooth-numbers", func() []Answer {
 
 	for j, test := range hardcodedTests {
 		n, k := test.n, test.k
-
 		answers[j] = Answer{
 			Args:   []string{fmt.Sprint(n), fmt.Sprint(k)},
 			Answer: smoothOutput(n, k),
@@ -25,36 +24,29 @@ var _ = answerFunc("smooth-numbers", func() []Answer {
 
 	for j := len(hardcodedTests); j < len(answers); j++ {
 		n, k := randInt(1, math.MaxInt8), randInt(1, math.MaxInt16)
-
 		answers[j] = Answer{
 			Args:   []string{fmt.Sprint(n), fmt.Sprint(k)},
 			Answer: smoothOutput(n, k),
 		}
 	}
-
 	return shuffle(answers)
 })
 
 func smoothOutput(n, k int) string {
 	var primes []int
-
 	for i := range n + 1 {
 		if big.NewInt(int64(i)).ProbablyPrime(0) {
 			primes = append(primes, i)
 		}
 	}
-
 	var expected strings.Builder
-
 	for i := 1; i <= k; i++ {
 		x := i
-
 		for _, p := range primes {
 			for x%p == 0 {
 				x /= p
 			}
 		}
-
 		if x == 1 {
 			if i != 1 {
 				expected.WriteByte('\n')
@@ -62,6 +54,5 @@ func smoothOutput(n, k int) string {
 			fmt.Fprint(&expected, i)
 		}
 	}
-	
 	return expected.String()
 }
