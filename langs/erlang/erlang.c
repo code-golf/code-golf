b0fece14df1d8323aa96c397baf2edd26c049896
Yewzir <hichamomari@hotmail.com>
2025-12-07 22:05:59 +0100

    Invoke Erlang in an alternative way

--- langs/erlang/erlang.c
+++ langs/erlang/erlang.c
@@ -6,7 +6,7 @@
 
 #define ERR_AND_EXIT(msg) do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
-const char* erlang = "/usr/bin/escript", *code = "code.escript";
+const char* erlang = "/usr/bin/erl", *code = "main.erl";
 
 int main(int argc, char* argv[]) {
     if (!strcmp(argv[1], "--version"))
@@ -20,7 +20,7 @@ int main(int argc, char* argv[]) {
     if (!(fp = fopen(code, "w")))
         ERR_AND_EXIT("fopen");
 
-    if (!fputc('\n', fp))
+    if (!fputs("-module(main).\n-export([main/0,main/1]).\nmain()->main([]).\n", fp))
         ERR_AND_EXIT("fputs");
 
     char buffer[4096];
@@ -39,7 +39,7 @@ int main(int argc, char* argv[]) {
         if (!dup2(STDERR_FILENO, STDOUT_FILENO))
             ERR_AND_EXIT("dup2");
 
-        execl(erlang, erlang, "-s", code, NULL);
+        execl(erlang, erlang, "-compile", code, NULL);
         ERR_AND_EXIT("execl");
     }
 
@@ -53,12 +53,18 @@ int main(int argc, char* argv[]) {
     if (WEXITSTATUS(status))
         return WEXITSTATUS(status);
 
-    int eargc = argc + 2;
+    if (remove(code))
+        ERR_AND_EXIT("remove");
+
+    int eargc = argc + 6;
     char** eargv = malloc(eargc * sizeof(char*));
     eargv[0] = (char*) erlang;
-    eargv[1] = "-i";
-    eargv[2] = (char*) code;
-    memcpy(&eargv[3], &argv[2], (argc - 2) * sizeof(char*));
+    eargv[1] = "-s";
+    eargv[2] = "init";
+    eargv[3] = "stop";
+    eargv[4] = "-S";
+    eargv[5] = eargv[6] = "main";
+    memcpy(&eargv[7], &argv[2], (argc - 2) * sizeof(char*));
     eargv[eargc - 1] = NULL;
 
     execv(erlang, eargv);
