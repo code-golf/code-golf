fede0ee87ede12f154e5b2ac65df49d4b6e2f293
Yewzir <hichamomari@hotmail.com>
2025-12-18 11:35:22 +0100

    Checkpoint

--- hole/boggle.go
+++ hole/boggle.go
@@ -2,6 +2,8 @@ package hole
 
 import "strings"
 
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
 var dice = []string{
 	"AACIOT", "ABILTY", "ABJMOQ", "ACDEMP",
 	"ACELRS", "ADENVZ", "AHMORS", "BIFORX",
@@ -15,55 +17,86 @@ var _ = answerFunc("boggle", func() []Answer {
 	for i := range answers {
 		var board [4][4]byte
 
+		// Separate the letters and words as arguments.
 		args := make([]strings.Builder, 2)
 
 		shuffle(dice)
 
 		for j, row := range board {
 			for k := range row {
-				die := dice[k + j * len(row)]
+				die := dice[k+j*len(row)]
 
 				board[j][k] = die[randInt(0, 5)]
 
 				args[0].WriteByte(board[j][k])
 
-				if k < len(row) - 1 {
+				if k < len(row)-1 {
 					args[0].WriteByte(' ')
 				}
 			}
 
-			if j < len(row) - 1 {
+			if j < len(row)-1 {
 				args[0].WriteByte('\n')
 			}
 		}
 
-		var answer strings.Builder
+		var wordList []string
 
+		// Determine all the valid words.
 		for _, word := range shuffle(words) {
 			if validate(board, strings.ToUpper(word)) {
-				if answer.String() != "" {
-					args[1].WriteByte(' ')
-				}
-
-				answer.WriteString(word)
-				answer.WriteByte('\n')
-
-				args[1].WriteString(word)
+				wordList = append(wordList, word)
 			}
 		}
 
-		// Expand args[1] with random:
-		// - invalid words less than 3 characters long
-		// - invalid words because of all the rest
-		// - valid but duplicate words (because no duplicate prints)
-		//
+		// Duplicate a number of valid words.
+		for range 10 {
+			wordList = append(wordList, randChoice(wordList))
+		}
+
+		// Generate a number of words that are too short.
+		for j := range 10 {
+			wordList = append(wordList, strings.Repeat(string(randChoice([]byte(alphabet))), j%2+1))
+		}
+
+		// Append a number of dictionary words. *** This needs more shizzle ***
+		for j := 0; j < 25; {
+			word := randWord()
+
+			if len(word) < len(board)-1 || len(word) > len(board)*2-1 {
+				continue
+			}
+
+			wordList = append(wordList, word)
+			j++
+		}
+
+		var answer strings.Builder
+
+		unique := make(map[string]bool)
+
+		// Build the argument and the answer.
+		for _, word := range shuffle(wordList) {
+			if args[1].String() != "" {
+				args[1].WriteByte(' ')
+			}
+
+			if !unique[word] && validate(board, strings.ToUpper(word)) {
+				answer.WriteString(word)
+				answer.WriteByte('\n')
+
+				unique[word] = true
+			}
+
+			args[1].WriteString(word)
+		}
+
 		// Enforce no valid words for one of the runs if it didn't already generate.
 		// If more than one such run occurs, regenerate the run so there's only one. But, unlikely.
 		//
-		// Implement the 'Q' thing.
-		//
-		// Update more description.
+		// Implement case 'Q' and explain in description.
 
+		// No valid words.
 		if answer.String() == "" {
 			answer.WriteByte('-')
 		}
@@ -75,7 +108,7 @@ var _ = answerFunc("boggle", func() []Answer {
 })
 
 func validate(b [4][4]byte, s string) bool {
-	if l := len(b); len(s) < l - 1 || len(s) > l * l {
+	if l := len(b); len(s) < l-1 || len(s) > l*l {
 		return false
 	}
 
@@ -105,7 +138,7 @@ func dfs(b [4][4]byte, used [4][4]bool, s string, i, j, k int) bool {
 
 	for _, direction := range [][2]int{
 		{-1, -1}, {-1, +0}, {-1, +1},
-		{+0, -1},           {+0, +1},
+		{+0, -1}, {+0, +1},
 		{+1, -1}, {+1, +0}, {+1, +1},
 	} {
 		if dfs(b, used, s, i+1, j+direction[0], k+direction[1]) {
