d5d65b9f7641d8a3695da0338bd032d882e35eb6
Yewzir <hichamomari@hotmail.com>
2025-12-26 17:32:36 +0100

    Build Nibbles with the latest GHC, 9.14.1, bootstrapped with 9.10.3

--- langs/nibbles/nibbles.c
+++ langs/nibbles/nibbles.c
@@ -12,13 +12,11 @@
 void copy_file(const char* src_file, const char* dst_file);
 void copy_folder(const char* src_dir, const char* dst_dir);
 
-const char* nibbles = "/usr/local/bin/nibbles", *code = "code.nbl", *input = "argv.txt";
+const char* nibbles = "/usr/local/bin/nibbles", *haskell = "/usr/local/bin/ghc", *code[] = {"code.nbl", "out.hs"}, *input = "argv.txt";
 
 int main(int argc, char* argv[]) {
-    if (!strcmp(argv[1], "--version")) {
-        execl(nibbles, nibbles, "-v", NULL);
-        ERR_AND_EXIT("execl");
-    }
+    if (!strcmp(argv[1], "--version"))
+        exit(EXIT_SUCCESS);
 
     copy_folder("/usr/local/bin", "/tmp");
 
@@ -27,7 +25,7 @@ int main(int argc, char* argv[]) {
 
     FILE* fp;
 
-    if (!(fp = fopen(code, "w")))
+    if (!(fp = fopen(code[0], "w")))
         ERR_AND_EXIT("fopen");
 
     char buffer[4096];
@@ -39,6 +37,26 @@ int main(int argc, char* argv[]) {
 
     if (fclose(fp))
         ERR_AND_EXIT("fclose");
+
+    pid_t pid;
+
+    if (!(pid = fork())) {
+        execl(nibbles, nibbles, "-hs", "-lit", code[0], NULL);
+        ERR_AND_EXIT("execl");
+    }
+
+    int status;
+
+    waitpid(pid, &status, 0);
+
+    if (!WIFEXITED(status))
+        exit(EXIT_FAILURE);
+
+    if (WEXITSTATUS(status))
+        return WEXITSTATUS(status);
+
+    if (remove(code[0]))
+        ERR_AND_EXIT("remove");
 
     int fd;
 
@@ -56,17 +74,13 @@ int main(int argc, char* argv[]) {
     if (close(fd))
         ERR_AND_EXIT("close");
 
-    pid_t pid;
-
     if (!(pid = fork())) {
         if (dup2(open(input, O_RDONLY), STDIN_FILENO))
             ERR_AND_EXIT("dup2");
 
-        execl(nibbles, nibbles, "-lit", code, NULL);
+        execl(haskell, haskell, "--run", code[1], "--", NULL);
         ERR_AND_EXIT("execl");
     }
-
-    int status;
 
     waitpid(pid, &status, 0);
 
@@ -75,6 +89,9 @@ int main(int argc, char* argv[]) {
 
     if (WEXITSTATUS(status))
         return WEXITSTATUS(status);
+
+    if (remove(code[1]))
+        ERR_AND_EXIT("remove");
 }
 
 void copy_file(const char* src_file, const char* dst_file) {
