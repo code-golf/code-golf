code-golf/code-golf

commit ff4d414e8de7b1935c0d321fe0a672511766368e

Simplify passing hole settings to the frontend
We can just serialise the entire map as JSON in page footer.

Show whitespace needs to remain a class as that's toggled from CSS.

js/_hole-common.tsx
@@ -134,7 +134,9 @@ export const hole         = decodeURI(location.pathname.slice(1));
 const scorings     = ['Bytes', 'Chars'];
 const solutions    = JSON.parse($('#solutions').innerText);
 
-const vimMode = JSON.parse($('#keymap').innerText) === 'vim';
+const settings = JSON.parse($('#settings').innerText);
+
+const vimMode = settings.keymap === 'vim';
 const vimModeExtensions = vimMode ? [extensions.vim] : [];
 
 const baseExtensions = [...vimModeExtensions, ...extensions.base, ...extensions.editor];
@@ -251,8 +253,8 @@ function updateLangPicker() {
 
     // Hybrid language selector: make it easy to see your existing solutions and their lengths.
     const picker = $('#picker');
-    const icon   = picker.dataset.style?.includes('icon')  ?? true;
-    const label  = picker.dataset.style?.includes('label') ?? true;
+    const icon   = settings['lang-picker-style'].includes('icon');
+    const label  = settings['lang-picker-style'].includes('label');
     picker.replaceChildren(...sortedLangs.map(l => {
         const tab = <a href={l.id == lang ? null : '#'+l.id} title={l.name}></a>;
 
@@ -749,8 +751,7 @@ export function setCodeForLangAndSolution(editor: any) {
     let code = localStorage.getItem(getAutoSaveKey(lang, solution)) ||
         getSolutionCode(lang, solution);
 
-    if ($('#editor').classList.contains('show-example-code'))
-        code ||= currentLang.example;
+    if (settings['show-example-code']) code ||= currentLang.example;
 
     setState(code, editor);
 

views/html/hole-footer.html
@@ -22,8 +22,8 @@
 
 <div id=popups></div>
 
-<script id=keymap type=application/json>
-    {{ index .SettingValues "hole" "editor-keymap" }}
+<script id=settings type=application/json>
+    {{ index .SettingValues "hole" }}
 </script>
 
 <script id=case-fold type=application/json>{{ .Data.Hole.CaseFold }}</script>

views/html/hole-tabs.html
@@ -2,9 +2,8 @@
 {{ template "hole-header" . }}
 
 {{ $showWhitespace := index .SettingValues "hole" "show-whitespace" }}
-{{ $showExampleCode := index .SettingValues "hole" "show-example-code" }}
 <main id="hole-{{ .Data.Hole.ID }}" {{ if $showWhitespace }} class=show-whitespace {{ end }}>
-    <nav class=tabs id=picker data-style='{{ index .SettingValues "hole" "lang-picker-style" }}'></nav>
+    <nav class=tabs id=picker></nav>
     {{ template "hole-info" . }}
     <div id="picker-status-row">
         <nav class="tabs hide" id=solutionPicker></nav>
@@ -24,7 +23,7 @@
             </div>
         </div>
     </div>
-    <div id=editor {{ if $showExampleCode }} class=show-example-code {{ end }}></div>
+    <div id=editor></div>
     <div id=golden-container></div>
 </main>
 

views/html/hole.html
@@ -2,16 +2,15 @@
 {{ template "hole-header" . }}
 
 {{ $showWhitespace := index .SettingValues "hole" "show-whitespace" }}
-{{ $showExampleCode := index .SettingValues "hole" "show-example-code" }}
 <main id="hole-{{ .Data.Hole.ID }}" {{ if $showWhitespace }} class=show-whitespace {{ end }}>
     <details id=details {{ if not .Data.HideDetails }}open{{ end }}>
         <summary>Details</summary>
         {{ template "hole-details" . }}
     </details>
-    <nav class=tabs id=picker data-style='{{ index .SettingValues "hole" "lang-picker-style" }}'></nav>
+    <nav class=tabs id=picker></nav>
     <nav class=tabs id=solutionPicker></nav>
     <div id=editor-and-rankings>
-        <div id=editor {{ if $showExampleCode }} class=show-example-code {{ end }}>
+        <div id=editor>
             <header>
                 <div id=strokes>0 bytes, 0 chars</div>
                 <a class=hide href id=restoreLink>Restore solution</a>

commit da7ed3af2d8eb1db3b33eef2d7bc61d506e9ba48

Fix editor keymap settings name
This was a regression in ff4d414e8de7b1935c0d321fe0a672511766368e

js/_hole-common.tsx
@@ -136,7 +136,7 @@ const solutions    = JSON.parse($('#solutions').innerText);
 
 const settings = JSON.parse($('#settings').innerText);
 
-const vimMode = settings.keymap === 'vim';
+const vimMode = settings['editor-keymap'] === 'vim';
 const vimModeExtensions = vimMode ? [extensions.vim] : [];
 
 const baseExtensions = [...vimModeExtensions, ...extensions.base, ...extensions.editor];