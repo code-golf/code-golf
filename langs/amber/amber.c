92009e9ebff379c581ba4267db3dd628c10efe09
Yewzir <hichamomari@hotmail.com>
2026-05-02 02:39:17 +0200

    Introduce a runner with a separate compilation and execution
    With this, the escape problem is already a thing of the past, but there
    are other execution methods.

---
+++ langs/amber/amber.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define ERR_AND_EXIT(msg) do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+const char* amber = "/usr/local/bin/amber", *code = "code.ab";
+
+int main(int argc, char* argv[]) {
+    if (!strcmp(argv[1], "--version")) {
+        execv(amber, argv);
+        ERR_AND_EXIT("execv");
+    }
+
+    if (chdir("/tmp"))
+        ERR_AND_EXIT("chdir");
+
+    FILE* fp;
+
+    if (!(fp = fopen(code, "w")))
+        ERR_AND_EXIT("fopen");
+
+    char buffer[4096];
+    ssize_t nbytes;
+
+    while ((nbytes = read(STDIN_FILENO, buffer, sizeof(buffer))))
+        if (fwrite(buffer, sizeof(char), nbytes, fp) != (size_t) nbytes)
+            ERR_AND_EXIT("fwrite");
+
+    if (fclose(fp))
+        ERR_AND_EXIT("fclose");
+
+    pid_t pid;
+
+    if (!(pid = fork())) {
+        execl(amber, amber, "build", code, "code", NULL);
+        ERR_AND_EXIT("execl");
+    }
+
+    int status;
+
+    waitpid(pid, &status, 0);
+
+    if (!WIFEXITED(status))
+        exit(EXIT_FAILURE);
+
+    if (WEXITSTATUS(status))
+        return WEXITSTATUS(status);
+
+    if (remove(code))
+        ERR_AND_EXIT("remove");
+
+    int aargc = argc;
+    char** aargv = malloc(aargc * sizeof(char*));
+    aargv[0] = "code";
+    memcpy(&aargv[1], &argv[2], (argc - 2) * sizeof(char*));
+    aargv[aargc - 1] = NULL;
+
+    execv("code", aargv);
+    ERR_AND_EXIT("execv");
+}
