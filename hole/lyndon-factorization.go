fc18cbc241cc3d9c685d18dc229384776da03a07
_hyacinth <148560588+albanian-laundromat@users.noreply.github.com>
2026-08-14 01:29:08 -0500

    Update lyndon-factorization.go

--- hole/lyndon-factorization.go
+++ hole/lyndon-factorization.go
@@ -1,62 +1,66 @@
 package hole
 
 import (
-	  "math/rand/v2"
-	  "strings"
-	)
+	"math/rand/v2"
+	"strings"
+)
 
 var fixedInputs = []string{
-	  "lyndon", "factorization", "codegolf",
-    "abcdefghijklmnopqrstuvwxyzyxwvutsrqponmlkjihgfedcbabcdefghijklmnopqrstuvwxyzyxwvutsrqponmlkjihgfedcba",
-    "vdsxqiytqmptjqintoblcromtgjpujvhrwjoydwgbwezpfxwlvye",
-    "rxexxecsudldbuosnzkvzvhlyofxczphartivkiehefdaffazlle",
-    "dntkwgzapobnhyduatjqmkcfidgsdcnastyosbasaolycziwfphq",
-    "aaaaaaaaaa",
+	"lyndon", "factorization", "codegolf",
+	"abcdefghijklmnopqrstuvwxyzyxwvutsrqponmlkjihgfedcbabcdefghijklmnopqrstuvwxyzyxwvutsrqponmlkjihgfedcba",
+	"vdsxqiytqmptjqintoblcromtgjpujvhrwjoydwgbwezpfxwlvye",
+	"rxexxecsudldbuosnzkvzvhlyofxczphartivkiehefdaffazlle",
+	"dntkwgzapobnhyduatjqmkcfidgsdcnastyosbasaolycziwfphq",
+	"aaaaaaaaaa",
 }
 
 var _ = answerFunc("lyndon-factorization", func() []Answer {
-    const alphabet = "qwertzuiopasdfghjklyxcvbnm"
-  
+	const alphabet = "qwertzuiopasdfghjklyxcvbnm"
+
 	tests := make([]test, 100)
 
 	for i, input := range fixedInputs {
-	    tests[i] = lyndonFactorizationTest(input)
+		tests[i] = lyndonFactorizationTest(input)
 	}
 
 	for i := len(fixedInputs); i < len(tests); i++ {
-	testLength := 1+rand.IntN(30)
-        input := ""
-        for range baseLength {
-			      j := randInt(0, 25)
-			      input += alphabet[j : j+1]
-		    }
-		    tests[i] = lyndonFactorizationTest(input)
+		testLength := 1 + rand.IntN(30)
+		input := ""
+		for range baseLength {
+			j := randInt(0, 25)
+			input += alphabet[j : j+1]
+		}
+		tests[i] = lyndonFactorizationTest(input)
 	}
 	return outputTests(shuffle(tests))
 })
 
 func lyndonFactorizationTest(input string) test {
 	in := input
-	
-  	var out strings.Builder
-  	i := 0
-  
-	for i < len(input) {
-      		j := i + 1
-      		k := i
 
-      for j < len(input) && input[k] <= input[j] {
-          if input[k] < input[j] { k = i } else { k += 1 }
-          j += 1
-      }
-    
-      for i <= k {
-          out.WriteString(input[i:i + j - k])
-          out.WriteByte(' ')
-          i += j - k
+	var out strings.Builder
+	i := 0
+
+	for i < len(input) {
+		j := i + 1
+		k := i
+
+		for j < len(input) && input[k] <= input[j] {
+			if input[k] < input[j] {
+				k = i
+			} else {
+				k += 1
+			}
+			j += 1
 		}
-    
-  	}
+
+		for i <= k {
+			out.WriteString(input[i : i+j-k])
+			out.WriteByte(' ')
+			i += j - k
+		}
+
+	}
 
 	return test{in, strings.TrimSpace(out.String())}
 }
