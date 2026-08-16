7a34d689742a5cea901098ffc7713a2b4a440a68
Bubbler-4 <bubbler@bubbler.blue>
2026-08-16 14:33:17 +0900

    Fix and improve wrapper
    - Avoid segfault from strcmp when argv[1] is null
    - Compile and run in three steps to isolate warnings being printed to stdout, and move compilation warnings to stderr

--- langs/lean/leanwrapper.c
+++ langs/lean/leanwrapper.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #define ERR_AND_EXIT(msg) do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
-const char* lean = "/usr/local/lean/bin/lean", *code = "code.lean";
+const char* lean = "/usr/local/lean/bin/lean", *leanc = "/usr/local/lean/bin/leanc",
+    *code = "code.lean", *codec = "code.c", *executable = "code";
 
 int main(int argc, char* argv[]) {
-    if (!strcmp(argv[1], "--version")) {
+    // guard against argv[1] being NULL
+    if (argc > 1 && !strcmp(argv[1], "--version")) {
         execv(lean, argv);
         ERR_AND_EXIT("execv");
     }
 
     if (chdir("/tmp"))
         ERR_AND_EXIT("chdir");
+
+    // 1. write stdin to code.lean
 
     FILE* fp;
 
@@ -31,14 +36,62 @@ int main(int argc, char* argv[]) {
     if (fclose(fp))
         ERR_AND_EXIT("fclose");
 
-    int iargc = argc + 3;
+    pid_t pid;
+    int status;
+
+    // 2. lean -c code.c code.lean
+
+    if (!(pid = fork())) {
+        // Redirect stdout (1) to point to stderr (2)
+        if (dup2(STDERR_FILENO, STDOUT_FILENO) < 0) {
+            ERR_AND_EXIT("dup2");
+        }
+        execl(
+            lean, lean, "-c", codec, code, NULL
+        );
+        ERR_AND_EXIT("execl");
+    }
+
+    waitpid(pid, &status, 0);
+
+    if (!WIFEXITED(status))
+        exit(EXIT_FAILURE);
+
+    if (WEXITSTATUS(status))
+        return WEXITSTATUS(status);
+
+    // 3. leanc -o code code.c
+
+    if (!(pid = fork())) {
+        execl(
+            leanc, leanc, "-o", executable, codec, NULL
+        );
+        ERR_AND_EXIT("execl");
+    }
+
+    waitpid(pid, &status, 0);
+
+    if (!WIFEXITED(status))
+        exit(EXIT_FAILURE);
+
+    if (WEXITSTATUS(status))
+        return WEXITSTATUS(status);
+
+    // 4. remove code.lean and code.c
+
+    if (remove(code))
+        ERR_AND_EXIT("remove");
+
+    if (remove(codec))
+        ERR_AND_EXIT("remove");
+
+    // 5. run executable with suitable argv
+    int iargc = argc + 1;
     char** iargv = malloc(iargc * sizeof(char*));
-    iargv[0] = (char*) lean;
-    iargv[1] = "--run";
-    iargv[2] = (char*) code;
-    memcpy(&iargv[3], &argv[1], (argc - 1) * sizeof(char*));
+    iargv[0] = (char *) executable;
+    memcpy(&iargv[1], &argv[1], (argc - 1) * sizeof(char*));
     iargv[iargc - 1] = NULL;
 
-    execv(lean, iargv);
+    execv(executable, iargv);
     ERR_AND_EXIT("execv");
 }
