35e891b906c8eb51275705a734b1d9e15889f247
Bubbler-4 <bubbler@bubbler.blue>
2026-08-16 16:30:10 +0900

    Revert codemirror change for Lean

--- assets/js/_codemirror.ts
+++ assets/js/_codemirror.ts
@@ -49,7 +49,6 @@ import { jq }                                       from 'codemirror-lang-jq';
 import { julia }                                    from '@codemirror/legacy-modes/mode/julia';
 import { k }                                        from 'codemirror-lang-k';
 import { knight }                                   from 'codemirror-lang-knight';
-//import { leanFallbackHighlightStyle, leanFallbackLanguageSupport } from "codemirror-lean4-lsp";
 import { lua }                                      from '@codemirror/legacy-modes/mode/lua';
 import { nim }                                      from 'nim-codemirror-mode';
 import { pascal }                                   from '@codemirror/legacy-modes/mode/pascal';
@@ -192,7 +191,7 @@ export const extensions : { [key: string]: any } = {
     'k':             k(),
     'knight':        knight(),
     'kotlin':        StreamLanguage.define(kotlin),
-    // 'lean':       leanFallbackLanguageSupport({ highlightStyle: leanFallbackHighlightStyle }),
+    // TODO lean
     // TODO lily
     'lua':           StreamLanguage.define(lua),
     'luau':          StreamLanguage.define(lua),
