7e98c615b04ef3c8b3fbc536357c771af4aacfb3
AlephSquirrel <33697577+AlephSquirrel@users.noreply.github.com>
2026-08-23 12:29:57 -0500

    Change spaces to tabs

--- hole/floyd-steinberg-dithering.go
+++ hole/floyd-steinberg-dithering.go
@@ -1,10 +1,10 @@
 package hole
 
 var _ = answerFunc("floyd-steinberg-dithering", func() []Answer {
-    tests := fixedTests("floyd-steinberg-dithering")
-    runs := make([]Answer, len(tests))
-    for i, test := range tests {
-        runs[i] = Answer{Args: []string{test.in}, Answer: test.out}
-    }
-    return shuffle(runs)
+	tests := fixedTests("floyd-steinberg-dithering")
+	runs := make([]Answer, len(tests))
+	for i, test := range tests {
+		runs[i] = Answer{Args: []string{test.in}, Answer: test.out}
+	}
+	return shuffle(runs)
 })
