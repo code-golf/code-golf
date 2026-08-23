8f40a1afd4aeb74ee4c0ccd76318ceab1727cdcd
AlephSquirrel <33697577+AlephSquirrel@users.noreply.github.com>
2026-08-23 12:21:41 -0500

    Update floyd-steinberg-dithering.go

--- hole/floyd-steinberg-dithering.go
+++ hole/floyd-steinberg-dithering.go
@@ -2,9 +2,9 @@ package hole
 
 var _ = answerFunc("floyd-steinberg-dithering", func() []Answer {
     tests := fixedTests("floyd-steinberg-dithering")
-    runs := make([]Run, len(tests))
+    runs := make([]Answer, len(tests))
     for i, test := range tests {
-        runs[i] = Run{Args: []string{test.in}, Answer: test.out}
+        runs[i] = Answer{Args: []string{test.in}, Answer: test.out}
     }
     return shuffle(runs)
 })
